b6c00a23817341b0b9ef68602415d70380ce2e94
Andreas-Joachim Peters <Andreas.Joachim.Peters@cern.ch>
2010-06-08 23:18:50 +0000

    - added 'rm -r' to console

--- Console/ConsoleMain.cc
+++ Console/ConsoleMain.cc
@@ -971,9 +971,19 @@ com_rm (char* arg1) {
   // split subcommands
   XrdOucTokenizer subtokenizer(arg1);
   subtokenizer.GetLine();
-  XrdOucString path = subtokenizer.GetToken();
-  XrdOucString selection = subtokenizer.GetToken();
+  XrdOucString s1 = subtokenizer.GetToken();
+  XrdOucString s2 = subtokenizer.GetToken();
+  XrdOucString path;
+  XrdOucString option;
 
+  if (s1 == "-r") {
+    option ="r";
+    path = s2;
+  } else {
+    option ="";
+    path = s1;
+  }
+  
   XrdOucString in = "mgm.cmd=rm&"; 
   if (!path.length()) {
     goto com_rm_usage;
@@ -981,15 +991,16 @@ com_rm (char* arg1) {
   } else {
     in += "mgm.path=";
     in += path;
-    
+    in += "&mgm.option=";
+    in += option;
+
     global_retc = output_result(client_user_command(in));
     return (0);
   }
 
  com_rm_usage:
-  printf("usage: rm <path>                                                      :  remote file <path>\n");
+  printf("usage: rm [-r] <path>                                                  :  remove file <path>\n");
   return (0);
-
 }
 
 
