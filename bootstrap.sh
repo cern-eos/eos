4519fa33a995cd20485a2d8a199322c8b1ddce07
Dennis Waldron <Dennis.Waldron@cern.ch>
2011-01-28 14:10:14 +0000

    Do not include empty m4 directory

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-aclocal -I m4 --force || exit 1
+aclocal --force || exit 1
 if libtoolize --version 1 >/dev/null 2>/dev/null; then
   libtoolize --copy --automake || exit 1
 elif glibtoolize --version 1 >/dev/null 2>/dev/null; then
