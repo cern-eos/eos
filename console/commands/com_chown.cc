014bb1dd4b94efad56809b54b31a98192e93c84b
Andreas Peters <Andreas.Joachim.Peters@cern.ch>
2011-03-30 16:44:41 +0200

    Adding 'chown' shell command

---
+++ console/commands/com_chown.cc
@@ -0,0 +1,39 @@
+/*----------------------------------------------------------------------------*/
+#include "console/ConsoleMain.hh"
+/*----------------------------------------------------------------------------*/
+
+/* Owner Interface */
+int 
+com_chown (char* arg1) {
+  XrdOucTokenizer subtokenizer(arg1);
+  subtokenizer.GetLine();
+  XrdOucString owner = subtokenizer.GetToken();
+  XrdOucString option="";
+  XrdOucString in = "mgm.cmd=chown";
+  XrdOucString arg = "";
+
+  if (owner.beginswith("-")) {
+    option = owner;
+    option.erase(0,1);
+    owner = subtokenizer.GetToken();
+    in += "&mgm.chown.option=";
+    in += option;
+  }
+
+  XrdOucString path = subtokenizer.GetToken();
+
+  if ( !path.length() || !owner.length() ) 
+    goto com_chown_usage;
+  in += "&mgm.path="; in += path;
+  in += "&mgm.chown.owner="; in += owner;
+
+  global_retc = output_result(client_admin_command(in));
+  return (0);
+
+ com_chown_usage:
+  printf("usage: chown [-r] <owner>[:<group>] <path>                             : set owner for <path> (-r recursive)\n");  
+  printf("                 <owner> has to be a virtual user id\n");
+  printf("                 <group> has to be a virtual group id\n");
+  printf("                  -r : recursive\n");
+  return (0);
+}
