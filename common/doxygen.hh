35b29a3bd18c0476876a5bfbcf78d6c08c3cd71e
Andreas Peters <Andreas.Joachim.Peters@cern.ch>
2012-02-08 17:20:51 +0100

    DOC: adding doxygen main page

---
+++ common/doxygen.hh
@@ -0,0 +1,16 @@
+/**
+ * @mainpage EOS Source Code Documentation
+ * @section home Project Homepage
+ * Information about EOS can be found under http://eos.cern.ch.
+ * The project homepage links to a GIT repository and release information.
+ * @section license License
+ * EOS is released under GNU GPL license. More information see the source code or license information on the project homepage.
+ */
+
+/**
+ * @file   doxygen.hh
+ * 
+ * @brief  Doxygen Main Page Definition.
+ * 
+ * 
+ */
