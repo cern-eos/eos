12388535d1780204a3121718c217e6c5bc6d6399
Elvin Sindrilaru <esindril@cern.ch>
2012-12-11 16:41:47 +0100

    FST: Split the implementation of XrdFstOss and XrdFstOssFile in different files.

--- fst/XrdFstOssFile.hh
+++ fst/XrdFstOssFile.hh
@@ -1,7 +1,7 @@
 //------------------------------------------------------------------------------
-//! @file XrdFstOss.hh
+//! @file XrdFstOssFile.hh
 //! @author Elvin-Alin Sindrilaru - CERN
-//! @brief Oss plugin for EOS doing block checksumming for files
+//! @brief Oss plugin for EOS dealing with files and adding block checksuming
 //------------------------------------------------------------------------------
 
 /************************************************************************
@@ -22,8 +22,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
  ************************************************************************/
 
-#ifndef __EOSFST_FSTOSS_HH__
-#define __EOSFST_FSTOSS_HH__
+#ifndef __EOSFST_FSTOSSFILE_HH__
+#define __EOSFST_FSTOSSFILE_HH__
 
 /*----------------------------------------------------------------------------*/
 #include <map>
@@ -31,9 +31,7 @@
 /*----------------------------------------------------------------------------*/
 #include "fst/Namespace.hh"
 #include "fst/checksum/CheckSum.hh"
-#include "fst/XrdFstOssFile.hh"
 #include "common/Logging.hh"
-#include "common/Namespace.hh"
 /*----------------------------------------------------------------------------*/
 #include "XrdOss/XrdOssApi.hh"
 #include "XrdSys/XrdSysPthread.hh"
@@ -42,119 +40,98 @@
 EOSFSTNAMESPACE_BEGIN
 
 //------------------------------------------------------------------------------
-//! Class XrdFstOss
+//! Class XrdFstOssFile built on top of XrdOssFile by adding blockxs information
 //------------------------------------------------------------------------------
-class XrdFstOss: public XrdOssSys, public eos::common::LogId
+class XrdFstOssFile : public XrdOssFile, public eos::common::LogId
 {
   public:
-
+  
     //--------------------------------------------------------------------------
     //! Constuctor
+    //!
+    //! @param tid
+    //!
     //--------------------------------------------------------------------------
-    XrdFstOss();
+    XrdFstOssFile( const char* tid );
 
 
     //--------------------------------------------------------------------------
     //! Destructor
     //--------------------------------------------------------------------------
-    virtual ~XrdFstOss();
+    virtual ~XrdFstOssFile();
 
 
     //--------------------------------------------------------------------------
-    //! Init function
+    //! Open function
     //!
-    //! @param lp system logger
-    //! @param configfn configuration file
-    //!
-    //! @return 0 upon success, -errno otherwise
-    //!
-    //--------------------------------------------------------------------------
-    int Init( XrdSysLogger* lp, const char* configfn );
-
-
-    //--------------------------------------------------------------------------
-    //! Unlink a file
-    //!
-    //! @param path fully qualified name of the file to be removed
-    //! @param opts extra options
-    //! @param ep enviroment information
+    //! @param path file path
+    //! @param flags open flags
+    //! @param mode open mode
+    //! @param env env variables passed to the function
     //!
     //! @return XrdOssOK upon success, -errno otherwise
     //!
     //--------------------------------------------------------------------------
-    virtual int Unlink( const char* path, int opts = 0, XrdOucEnv* ep = 0 );
+    virtual int Open( const char* path, int flags, mode_t mode, XrdOucEnv& env );
 
 
     //--------------------------------------------------------------------------
-    //! New file
+    //! Read
     //!
-    //! @param tident
+    //! @param buffer data container for read operation
+    //! @param offset file offet
+    //! @param length read length
     //!
-    //! @return new oss file object
+    //! @return number of bytes read
     //!
     //--------------------------------------------------------------------------
-    virtual XrdOssDF* newFile( const char* tident );
+    ssize_t Read( void* buffer, off_t offset, size_t length );
 
 
     //--------------------------------------------------------------------------
-    //! New directory
+    //! Read raw
     //!
-    //! @param tident
+    //! @param buffer data container for read operation
+    //! @param offset file offet
+    //! @param length read length
     //!
-    //! @return new oss directory object
+    //! @return number of bytes read
     //!
     //--------------------------------------------------------------------------
-    virtual XrdOssDF* newDir( const char* tident );
+    ssize_t ReadRaw( void* buffer, off_t offset, size_t length );
 
 
     //--------------------------------------------------------------------------
-    //! Add new entry to file <-> block checksum map
+    //! Write
     //!
-    //! @param fileName name of the file added to the mapping
-    //! @param blockXs the blockxs object
-    //! @param isRW tell if file is opened in read/write mode
+    //! @param buffer data container for write operation
+    //! @param offset file offet
+    //! @param length write length
     //!
-    //! @return mutex for accessing the blockxs object
+    //! @return number of byes written
     //!
     //--------------------------------------------------------------------------
-    XrdSysRWLock* AddMapping( const std::string& fileName,
-                              CheckSum*&         blockXs,
-                              bool               isRW );
+    ssize_t Write( const void* buffer, off_t offset, size_t length );
 
 
     //--------------------------------------------------------------------------
-    //! Get block checksum object for a file name
+    //! Close function
     //!
-    //! @param fileName file name for which we search for a xs obj
-    //! @param isRW mark if file is opened in read/write mode
+    //! @param retsz
     //!
-    //! @return pair containing the the boockxs obj and its corresponding mutex
-    //!
-    //--------------------------------------------------------------------------
-    std::pair<XrdSysRWLock*, CheckSum*> GetXsObj( const std::string& fileName,
-                                                  bool               isRW );
-
-
-    //--------------------------------------------------------------------------
-    //! Drop block checksum object for a filname
-    //!
-    //! @param fileName file name entry to be dropped from the map
-    //! @param force mark if removal is to be forced
+    //! @return XrdOssOK upon success, -1 otherwise
     //!
     //--------------------------------------------------------------------------
-    void DropXs( const std::string& fileName, bool force = false );
+    virtual int Close( long long* retsz = 0 );
 
-  
   private:
 
-    XrdSysRWLock mRWMap;     ///< rw lock for the file <-> xs map
-
-    //! map between file names and block xs objects
-    std::map< std::string, std::pair<XrdSysRWLock*, CheckSum*> > mMapFileXs;
-
+    XrdOucString        mPath;      ///< path of the file
+    bool                mIsRW;      ///< mark if opened for rw operations
+    XrdSysRWLock*       mRWLockXs;  ///< rw lock for the block xs
+    CheckSum*           mBlockXs;   ///< block xs object
 };
 
 EOSFSTNAMESPACE_END
 
-#endif // __EOSFST_FSTOSS_HH__
-
+#endif // __EOSFST_FSTOSSFILE_HH__
