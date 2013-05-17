f26338ef3bba1389638c1a1fefe0da881a6a4886
Andreas Peters <Andreas.Joachim.Peters@cern.ch>
2013-05-17 13:27:01 +0200

    ONSOLE: fix bug using eoscp if /eos is mounted

--- console/commands/com_cp.cc
+++ console/commands/com_cp.cc
@@ -610,7 +610,7 @@ com_cp (char* argin)
       }
     }
 
-    if ((source_list[nfile].find(":/") == STR_NPOS))
+    if ( (source_list[nfile].find(":/") == STR_NPOS) && (!source_list[nfile].beginswith("/eos")) )
     {
       // ------------------------------------------
       // local file
