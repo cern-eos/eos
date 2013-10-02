b19413881dcf85c41a5c3507c64b67f2b6f319cc
Joaquim Rocha <joaquim.rocha@cern.ch>
2013-10-02 10:48:52 +0200

    CONSOLE: Reimplement the 'chown' command using ConsoleCliCommand

--- console/commands/com_chown.cc
+++ console/commands/com_chown.cc
@@ -1,6 +1,7 @@
 // ----------------------------------------------------------------------
 // File: com_chown.cc
 // Author: Andreas-Joachim Peters - CERN
+// Author: Joaquim Rocha - CERN
 // ----------------------------------------------------------------------
 
 /************************************************************************
@@ -23,53 +24,50 @@
 
 /*----------------------------------------------------------------------------*/
 #include "console/ConsoleMain.hh"
+#include "console/ConsoleCliCommand.hh"
 /*----------------------------------------------------------------------------*/
 
 /* Owner Interface */
 int
 com_chown (char* arg1)
 {
-  XrdOucTokenizer subtokenizer(arg1);
-  subtokenizer.GetLine();
-  XrdOucString owner = subtokenizer.GetToken();
-  XrdOucString option = "";
   XrdOucString in = "mgm.cmd=chown";
-  XrdOucString arg = "";
-  
 
-  if (owner.beginswith("-"))
+  CliOption helpOption("help", "print help", "-h,--help");
+  helpOption.setHidden(true);
+
+  ConsoleCliCommand chownCmd("chown", "provides the change owner "
+                             "interface of EOS");
+  chownCmd.addOption(helpOption);
+  chownCmd.addOption({"recursive", "change mode recursively", "-r"});
+  chownCmd.addOptions({{"owner-group", "<owner> has to be a user id or user "
+                        "name;\n<group> is optional and has to be a group id "
+                        "or group name", 1, 1, "<owner>[:<group>]", true},
+                       {"path", "the file/directory to modify", 2, 1,
+                        "<path>", true}
+                      });
+
+  chownCmd.parse(arg1);
+
+  if (chownCmd.hasValue("help"))
   {
-    option = owner;
-    option.erase(0, 1);
-    owner = subtokenizer.GetToken();
-    in += "&mgm.chown.option=";
-    in += option;
+    chownCmd.printUsage();
+    return 0;
+  }
+  else if (chownCmd.hasErrors())
+  {
+    chownCmd.printErrors();
+    chownCmd.printUsage();
+    return 0;
   }
 
-  XrdOucString path = subtokenizer.GetToken();
+  if (chownCmd.hasValue("recursive"))
+    in += "&mgm.chown.option=r";
 
-  if (wants_help(arg1))
-    goto com_chown_usage;
-
-  if (!path.length() || !owner.length())
-    goto com_chown_usage;
-
-  path = abspath(path.c_str());
-
-  in += "&mgm.path=";
-  in += path;
+  in += "&mgm.path=" + cleanPath(chownCmd.getValue("path"));
   in += "&mgm.chown.owner=";
-  in += owner;
+  in += chownCmd.getValue("owner-group").c_str();
 
   global_retc = output_result(client_user_command(in));
-  return (0);
-
-com_chown_usage:
-  fprintf(stdout, "Usage: chown [-r] <owner>[:<group>] <path>\n");
-  fprintf(stdout, "'[eos] chown ..' provides the change owner interface of EOS.\n");
-  fprintf(stdout, "<path> is the file/directory to modify, <owner> has to be a user id or user name. <group> is optional and has to be a group id or group name.\n");
-  fprintf(stdout, "Remark: EOS does access control on directory level - the '-r' option only applies to directories! It is not possible to set uid!=0 and gid=0!\n\n");
-  fprintf(stdout, "Options:\n");
-  fprintf(stdout, "                  -r : recursive\n");
   return (0);
 }
