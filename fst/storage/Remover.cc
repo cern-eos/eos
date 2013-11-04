3009ff28b58587eab209d25f7939641ea830754c
Andreas Peters <Andreas.Joachim.Peters@cern.ch>
2013-11-04 05:47:25 +0100

    FST: add 'error' object to Manager call to avoid SEGV in case of an returned error

--- fst/storage/Remover.cc
+++ fst/storage/Remover.cc
@@ -64,7 +64,7 @@ Storage::Remover ()
         // delete the file
         XrdOucString hexstring = "";
         eos::common::FileId::Fid2Hex(todelete.fIdVector[j], hexstring);
-        XrdOucErrInfo error;
+        static XrdOucErrInfo error;
 
         XrdOucString capOpaqueString = "/?mgm.pcmd=drop";
         XrdOucString OpaqueString = "";
@@ -83,7 +83,7 @@ Storage::Remover ()
         }
 
         // update the manager
-        int rc = gOFS.CallManager(0, 0, todelete.managerId.c_str(), capOpaqueString);
+        int rc = gOFS.CallManager(&error, 0, todelete.managerId.c_str(), capOpaqueString);
         if (rc)
         {
           eos_static_err("unable to drop file id %s fsid %u at manager %s", hexstring.c_str(), todelete.fsId, todelete.managerId.c_str());
@@ -115,7 +115,6 @@ Storage::Remover ()
 
       lastAskedForDeletions = now;
       eos_static_debug("asking for new deletions");
-      XrdOucErrInfo error;
       XrdOucString managerQuery = "/?";
       managerQuery += "mgm.pcmd=schedule2delete";
       managerQuery += "&mgm.target.nodename=";
