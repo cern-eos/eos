fbfde33543fe2b9dabd07a1d956b67aa546b17d5
Andreas Peters <Andreas.Joachim.Peters@cern.ch>
2014-06-19 09:31:48 +0200

    MGM: (Http) IDE formatting

--- mgm/http/HttpHandler.cc
+++ mgm/http/HttpHandler.cc
@@ -55,43 +55,43 @@ HttpHandler::HandleRequest (eos::common::HttpRequest *request)
 {
   eos_static_debug("handling http request");
   eos::common::HttpResponse *response = 0;
-  
+
   request->AddEosApp();
 
   int meth = ParseMethodString(request->GetMethod());
   switch (meth)
   {
-  case GET:
-    response = Get(request);
-    break;
-  case HEAD:
-    response = Head(request);
-    break;
-  case POST:
-    response = Post(request);
-    break;
-  case PUT:
-    response = Put(request);
-    break;
-  case DELETE:
-    response = Delete(request);
-    break;
-  case TRACE:
-    response = Trace(request);
-    break;
-  case OPTIONS:
-    response = Options(request);
-    break;
-  case CONNECT:
-    response = Connect(request);
-    break;
-  case PATCH:
-    response = Patch(request);
-    break;
-  default:
-    response = new eos::common::PlainHttpResponse();
-    response->SetResponseCode(eos::common::HttpResponse::BAD_REQUEST);
-    response->SetBody("No such method");
+    case GET:
+      response = Get(request);
+      break;
+    case HEAD:
+      response = Head(request);
+      break;
+    case POST:
+      response = Post(request);
+      break;
+    case PUT:
+      response = Put(request);
+      break;
+    case DELETE:
+      response = Delete(request);
+      break;
+    case TRACE:
+      response = Trace(request);
+      break;
+    case OPTIONS:
+      response = Options(request);
+      break;
+    case CONNECT:
+      response = Connect(request);
+      break;
+    case PATCH:
+      response = Patch(request);
+      break;
+    default:
+      response = new eos::common::PlainHttpResponse();
+      response->SetResponseCode(eos::common::HttpResponse::BAD_REQUEST);
+      response->SetBody("No such method");
   }
 
   mHttpResponse = response;
@@ -102,7 +102,7 @@ eos::common::HttpResponse*
 HttpHandler::Get (eos::common::HttpRequest *request, bool isHEAD)
 {
   XrdSecEntity client(mVirtualIdentity->prot.c_str());
-  client.name = const_cast<char*> (mVirtualIdentity->name.c_str());
+  client.name = const_cast<char*> (mVirtualIdentity->uid_string.c_str());
   client.host = const_cast<char*> (mVirtualIdentity->host.c_str());
   client.tident = const_cast<char*> (mVirtualIdentity->tident.c_str());
 
@@ -116,36 +116,37 @@ HttpHandler::Get (eos::common::HttpRequest *request, bool isHEAD)
   XrdOucString spath = request->GetUrl().c_str();
 
   // redirect '/' to '/eos/<instance>/'
-  if (spath=="/") {
+  if (spath == "/")
+  {
     XrdOucString instance = gOFS->MgmOfsInstanceName;
     if (instance.beginswith("eos"))
     {
       instance.replace("eos", "");
     }
-    response = HttpServer::HttpRedirect(url + "eos/" +  instance.c_str(),
-					gOFS->HostName,
-					8000, false);
+    response = HttpServer::HttpRedirect(url + "eos/" + instance.c_str(),
+                                        gOFS->HostName,
+                                        8000, false);
     return response;
   }
 
-  std::string etag="undef";
+  std::string etag = "undef";
   // ------------------------------------------------------------------------------
   // owncloud protocol emulator
   // ------------------------------------------------------------------------------
   if (spath.find("/status.php") != STR_NPOS)
   {
     XrdOucErrInfo error(mVirtualIdentity->tident.c_str());
-    spath.replace("/status.php","");
+    spath.replace("/status.php", "");
     XrdOucString val;
-    if (gOFS->attr_get(spath.c_str(),error, &client, "","sys.allow.oc.sync", val))
+    if (gOFS->attr_get(spath.c_str(), error, &client, "", "sys.allow.oc.sync", val))
     {
       response = HttpServer::HttpError("No sync allowed in this tree",
-				       response->METHOD_NOT_ALLOWED);
+                                       response->METHOD_NOT_ALLOWED);
       return response;
     }
     else
     {
-      std::string data ="{\"installed\":\"true\",\"version\":\"5.0.28\",\"versionstring\":\"5.0.14a\",\"edition\":\"Enterprise\"}";
+      std::string data = "{\"installed\":\"true\",\"version\":\"5.0.28\",\"versionstring\":\"5.0.14a\",\"edition\":\"Enterprise\"}";
       response = HttpServer::HttpData(data.c_str(), data.length());
       return response;
     }
@@ -153,7 +154,7 @@ HttpHandler::Get (eos::common::HttpRequest *request, bool isHEAD)
 
   if (spath.find("/remote.php/webdav/") != STR_NPOS)
   {
-    spath.replace("remote.php/webdav/","");
+    spath.replace("remote.php/webdav/", "");
   }
 
   if (!spath.beginswith("/proc/"))
@@ -163,9 +164,9 @@ HttpHandler::Get (eos::common::HttpRequest *request, bool isHEAD)
     if (gOFS->stat(url.c_str(), &buf, error, &etag, &client, ""))
     {
       eos_static_info("method=GET error=ENOENT path=%s",
-		      url.c_str());
+                      url.c_str());
       response = HttpServer::HttpError("No such file or directory",
-				       response->NOT_FOUND);
+                                       response->NOT_FOUND);
       return response;
     }
 
@@ -173,9 +174,9 @@ HttpHandler::Get (eos::common::HttpRequest *request, bool isHEAD)
     {
       // ETag mismatch
       eos_static_info("method=GET error=precondition-failed path=%s etag=%s cond=match r-etag=%s",
-		      url.c_str(), etag.c_str(), request->GetHeaders()["If-Match"].c_str());
+                      url.c_str(), etag.c_str(), request->GetHeaders()["If-Match"].c_str());
       response = HttpServer::HttpError("ETag precondition failed",
-				       response->PRECONDITION_FAILED);
+                                       response->PRECONDITION_FAILED);
       return response;
     }
 
@@ -183,12 +184,12 @@ HttpHandler::Get (eos::common::HttpRequest *request, bool isHEAD)
     {
       // ETag match
       eos_static_info("method=GET error=precondition-failed path=%s etag=%s cond=not-match r-etag=%s",
-		      url.c_str(), etag.c_str(), request->GetHeaders()["If-Not-Match"].c_str());
-      
+                      url.c_str(), etag.c_str(), request->GetHeaders()["If-Not-Match"].c_str());
+
       response = HttpServer::HttpError("ETag is not modified",
-				       response->NOT_MODIFIED);
+                                       response->NOT_MODIFIED);
     }
-    
+
     if (spath.endswith("/"))
     {
       isfile = false;
@@ -208,8 +209,8 @@ HttpHandler::Get (eos::common::HttpRequest *request, bool isHEAD)
                           url.c_str());
           // HEAD requests on files can return from the MGM without redirection
           response = HttpServer::HttpHead(buf.st_size, basename);
-	  response->AddHeader("ETag",etag);
-	  response->AddHeader("Last-Modified", eos::common::Timing::utctime(buf.st_mtime));
+          response->AddHeader("ETag", etag);
+          response->AddHeader("Last-Modified", eos::common::Timing::utctime(buf.st_mtime));
           return response;
         }
       }
@@ -279,7 +280,7 @@ HttpHandler::Get (eos::common::HttpRequest *request, bool isHEAD)
           response = HttpServer::HttpError("Unexpected result from file open",
                                            EOPNOTSUPP);
         }
-	response->AddHeader("ETag",etag);
+        response->AddHeader("ETag", etag);
       }
       else
       {
@@ -305,13 +306,13 @@ HttpHandler::Get (eos::common::HttpRequest *request, bool isHEAD)
 
         response = new eos::common::PlainHttpResponse();
 
-	XrdOucErrInfo error(mVirtualIdentity->tident.c_str());
+        XrdOucErrInfo error(mVirtualIdentity->tident.c_str());
 
-	if (!gOFS->stat(url.c_str(), &buf, error, &etag, &client, ""))
-	{
-	  response->AddHeader("ETag",etag);
-	  response->AddHeader("Last-Modified", eos::common::Timing::utctime(buf.st_mtime));
-	}
+        if (!gOFS->stat(url.c_str(), &buf, error, &etag, &client, ""))
+        {
+          response->AddHeader("ETag", etag);
+          response->AddHeader("Last-Modified", eos::common::Timing::utctime(buf.st_mtime));
+        }
         response->SetBody(result);
       }
       // clean up the object
@@ -355,14 +356,14 @@ HttpHandler::Get (eos::common::HttpRequest *request, bool isHEAD)
       result += R"literal(
         <div id="newlisting" style="display:none"></div>
         <div id="listing">
-        
+
         <table id="dirlist" border:1px solid #aaa !important;>
         <tr>
           <th style="min-width:150px">Path</th> <th style="min-width:20px"></th>  <th style="min-width:150px">Size</th>
           <th style="min-width:150px">Created</th> <th style="min_width:100">Mode</th>
           <th style="min-width:60px">owner</th> <th style="min-width:60px">group</th>
           <th style="min-width:150px">Acl</th>
-        </tr> 
+        </tr>
         )literal";
 
       // -----------------------------------------------------------------------
@@ -409,7 +410,7 @@ HttpHandler::Get (eos::common::HttpRequest *request, bool isHEAD)
 
           }
         }
-       
+
         struct stat buf;
         buf.st_mode = 0;
         XrdOucErrInfo error(mVirtualIdentity->tident.c_str());
@@ -427,18 +428,18 @@ HttpHandler::Get (eos::common::HttpRequest *request, bool isHEAD)
             isFile = false;
           }
         }
-        
+
         if (!isFile)
-          entryname+="/";
-        
+          entryname += "/";
+
         result += linkname.c_str();
         result += "\">";
         result += "<font size=\"2\">";
         result += entryname.c_str();
-        result += "</font>";    
+        result += "</font>";
         result += "       </a>\n";
         result += "<div fullpath=\"";
-        result += entryname.c_str();        
+        result += entryname.c_str();
         result += "\"></div></td>\n";
         // ---------------------------------------------------------------------
         // share link icon
@@ -452,7 +453,7 @@ HttpHandler::Get (eos::common::HttpRequest *request, bool isHEAD)
           result += linkname.c_str();
           result += R"literal(');">)literal";
 
-          result += R"literal(<img alt="" src="data:image/gif;base64,R0lGODlhEAANAJEAAAJ6xv///wAAAAAAACH5BAkAAAEALAAAAAAQAA0AAAg0AAMIHEiwoMGDCBMqFAigIYCFDBsadPgwAMWJBB1axBix4kGPEhN6HDgyI8eTJBFSvEgwIAA7" /> 
+          result += R"literal(<img alt="" src="data:image/gif;base64,R0lGODlhEAANAJEAAAJ6xv///wAAAAAAACH5BAkAAAEALAAAAAAQAA0AAAg0AAMIHEiwoMGDCBMqFAigIYCFDBsadPgwAMWJBB1axBix4kGPEhN6HDgyI8eTJBFSvEgwIAA7" />
             </a>
             )literal";
         }
@@ -595,7 +596,7 @@ HttpHandler::Get (eos::common::HttpRequest *request, bool isHEAD)
       result += "       </html>\n";
       response = new eos::common::PlainHttpResponse();
       response->SetBody(result);
-      response->AddHeader("ETag",etag);
+      response->AddHeader("ETag", etag);
       response->AddHeader("Last-Modified", eos::common::Timing::utctime(buf.st_mtime));
     }
     else
@@ -644,6 +645,10 @@ HttpHandler::Put (eos::common::HttpRequest * request)
                   url.c_str());
   // Classify path to split between directory or file objects
   bool isfile = true;
+  bool isOcChunked = false;
+  std::map<std::string, std::string> ocHeader;
+
+
   eos::common::HttpResponse *response = 0;
 
   XrdOucString spath = request->GetUrl().c_str();
@@ -655,6 +660,17 @@ HttpHandler::Put (eos::common::HttpRequest * request)
     }
   }
 
+  if (eos::common::OwnCloud::isChunkUpload(request))
+  {
+    isOcChunked = true;
+
+    // we have to rewrite the path and add some additional header describing
+    // the chunking which was stored in the name
+    url = eos::common::OwnCloud::prepareChunkUpload(request, &response, &ocHeader);
+    if (response)
+      return response;
+  }
+
   std::string etag;
 
   {
@@ -664,7 +680,7 @@ HttpHandler::Put (eos::common::HttpRequest * request)
 
     if (gOFS->stat(url.c_str(), &buf, error, &etag, &client, ""))
     {
-      etag="undef"; 
+      etag = "undef";
     }
   }
 
@@ -672,9 +688,9 @@ HttpHandler::Put (eos::common::HttpRequest * request)
   {
     // ETag mismatch
     eos_static_info("method=PUT error=precondition-failed path=%s etag=%s cond=match r-etag=%s",
-	             url.c_str(), etag.c_str(), request->GetHeaders()["If-Match"].c_str());
+                    url.c_str(), etag.c_str(), request->GetHeaders()["If-Match"].c_str());
     response = HttpServer::HttpError("ETag precondition failed",
-		                     response->PRECONDITION_FAILED);
+                                     response->PRECONDITION_FAILED);
     return response;
   }
 
@@ -705,25 +721,48 @@ HttpHandler::Put (eos::common::HttpRequest * request)
       std::string query;
       if (request->GetHeaders().count("Content-Length"))
       {
-	query += "eos.bookingsize=";
-	query += request->GetHeaders()["Content-Length"];
+        query += "eos.bookingsize=";
+        // for OC chun    ked uploads we book the full size
+        char* oclength = eos::common::OwnCloud::getContentSize(request);
+        if (oclength)
+          query += oclength;
+        else
+          query += request->GetHeaders()["Content-Length"];
       }
-      else 
+      else
       {
-	query = "eos.bookingsize=0";
+        query = "eos.bookingsize=0";
+      }
+
+      if (isOcChunked)
+      {
+
+        if (query.length())
+          query += "&";
+        query += "oc.chunk=1";
+        query += OcMaxChunks.c_str();
+        query += OcNchunk.c_str();
+        query += OcUploadId.c_str();
       }
 
       // -----------------------------------------------------------
       // OC clients are switched automatically to atomic upload mode
       // -----------------------------------------------------------
-      if (request->GetHeaders().count("OC-Content-Length"))
+      if (request->GetHeaders().count("OC-Content-Length") || isOcChunked)
       {
-	if (query.length())
-	  query += "&";
-	query += "eos.atomic=1";
+        if (query.length())
+          query += "&";
+        query += "eos.atomic=1";
       }
 
-
+      if (isOcChunked)
+      {
+        if (etag != "undef") // file exists already
+        {
+          eos_static_info("removing truncation flag ");
+          open_mode ^= SFS_O_TRUNC;
+        }
+      }
 
       int rc = file->open(url.c_str(), open_mode, create_mode, &client,
                           query.c_str());
@@ -733,6 +772,7 @@ HttpHandler::Put (eos::common::HttpRequest * request)
         {
           // retry as a file creation
           open_mode |= SFS_O_CREAT;
+          open_mode |= SFS_O_TRUNC;
           rc = file->open(url.c_str(), open_mode, create_mode, &client,
                           query.c_str());
         }
@@ -742,20 +782,20 @@ HttpHandler::Put (eos::common::HttpRequest * request)
       {
         if (rc == SFS_REDIRECT)
         {
-	  if (file->error.getErrInfo() == 1094)
-	  {
-	    // MGM redirect
-	    response = HttpServer::HttpRedirect(request->GetUrl(),
-						file->error.getErrText(),
-						8000, false);
-	  }
-	  else
-	  {
-	    // FST redirect
-	    response = HttpServer::HttpRedirect(request->GetUrl(),
-						file->error.getErrText(),
-						8001, false);
-	  }
+          if (file->error.getErrInfo() == 1094)
+          {
+            // MGM redirect
+            response = HttpServer::HttpRedirect(request->GetUrl(),
+                                                file->error.getErrText(),
+                                                8000, false);
+          }
+          else
+          {
+            // FST redirect
+            response = HttpServer::HttpRedirect(request->GetUrl(),
+                                                file->error.getErrText(),
+                                                8001, false);
+          }
         }
         else if (rc == SFS_ERROR)
         {
@@ -785,13 +825,13 @@ HttpHandler::Put (eos::common::HttpRequest * request)
       }
 
       std::string rurl = file->error.getErrText();
-      rurl.erase(0, rurl.find("?")+1);
+      rurl.erase(0, rurl.find("?") + 1);
       XrdOucEnv env(rurl.c_str());
       char* etag = env.Get("mgm.etag");
       if (etag)
       {
-	// add the ETag into the header
-	response->AddHeader("ETag",etag);
+        // add the ETag into the header
+        response->AddHeader("ETag", etag);
       }
 
       // clean up the object
@@ -801,6 +841,7 @@ HttpHandler::Put (eos::common::HttpRequest * request)
   else
   {
     // DIR requests
+
     response = HttpServer::HttpError("Not Implemented", EOPNOTSUPP);
   }
 
@@ -847,6 +888,7 @@ HttpHandler::Delete (eos::common::HttpRequest * request)
   }
   else
   {
+
     response = new eos::common::PlainHttpResponse();
     response->SetResponseCode(response->NO_CONTENT);
   }
@@ -858,6 +900,7 @@ HttpHandler::Delete (eos::common::HttpRequest * request)
 eos::common::HttpResponse *
 HttpHandler::Trace (eos::common::HttpRequest * request)
 {
+
   using namespace eos::common;
   std::string url = request->GetUrl();
   eos_static_info("method=TRACE error=NOTIMPLEMENTED path=%s",
@@ -871,6 +914,7 @@ HttpHandler::Trace (eos::common::HttpRequest * request)
 eos::common::HttpResponse *
 HttpHandler::Options (eos::common::HttpRequest * request)
 {
+
   eos::common::HttpResponse *response = new eos::common::PlainHttpResponse();
   response->AddHeader("DAV", "1,2");
   response->AddHeader("Allow", "OPTIONS,GET,HEAD,POST,DELETE,TRACE,"\
@@ -884,6 +928,7 @@ HttpHandler::Options (eos::common::HttpRequest * request)
 eos::common::HttpResponse *
 HttpHandler::Connect (eos::common::HttpRequest * request)
 {
+
   using namespace eos::common;
   std::string url = request->GetUrl();
   eos_static_info("method=CONNECT error=NOTIMPLEMENTED path=%s",
