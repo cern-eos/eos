f454a87dc3fb880f10977e5c03b4f7a4c10a8c87
Elvin Sindrilaru <esindril@cern.ch>
2014-07-29 09:20:58 +0200

    ARCHIVE: Refactor the asynchandler module to make it easily extensible when adding new types          of handler objects.

--- archive/archivefile/asynchandler.py
+++ archive/archivefile/asynchandler.py
@@ -25,67 +25,98 @@ import json
 from threading import Condition
 
 
-class MetaHandler(object):
-  """ Meta handler for different types of async requests.
-
-  Attributes:
-    cond: Condition variable used for synchronization.
-    logger: Logger object.
-    mkdir_failed: List of directories failed to create.
-    mkdir_status: Status of mkdir requests, logical and between individual
-      mkdir commands.
-    mkdir_num: Number of mkdir commands waiting for reply.
-  """
-  def __init__(self):
-    self.mkdir_num = 0
-    self.mkdir_status = True
-    self.mkdir_failed = []
-    self.cond = Condition()
-    self.logger = logging.getLogger("ArchiveFile." + type(self).__name__)
-
-  def register_mkdir(self, path):
-    self.cond.acquire()
-    self.mkdir_num += 1
-    self.cond.release()
-    return self._MkDirHandler(path, self)
-
-  def handle_mkdir(self, status, path):
-    self.cond.acquire()
-    self.mkdir_status = self.mkdir_status and status.ok
-    self.mkdir_num -= 1
-
-    if not status.ok:
-      self.mkdir_failed.append(path)
-
-    if self.mkdir_num == 0:
-      self.cond.notifyAll()
-
-    self.cond.release()
-
-  def wait_mkdir(self):
-    self.cond.acquire()
-
-    while self.mkdir_num != 0:
-      self.cond.wait()
-
-    if self.mkdir_failed:
-      self.logger.error("List of failed mkdir paths is: {0}".format(self.mkdir_failed))
-    else:
-      self.logger.debug("All mkdir requests were successful")
-
-    self.cond.release()
-    return self.mkdir_status
-
-  class _MkDirHandler(object):
+class _MkDirHandler(object):
     """ Async mkdir handler which reports to MetaHandler.
 
     Attributes:
-      path (string): Directory path for which the handler is created.
-      meta_handler (MetaHandler): Meta handler object.
+        path (string): Directory path for which the handler is created.
+        meta_handler (MetaHandler): Meta handler object.
     """
     def __init__(self, path, meta_handler):
-      self.path = path
-      self.meta_handler = meta_handler
+        self.type = 'mkdir'
+        self.path = path
+        self.meta_handler = meta_handler
 
     def __call__(self, status, response, hostlist):
-      self.meta_handler.handle_mkdir(status, self.path)
+        self.meta_handler.handle(self.type, status, self.path)
+
+
+class _PrepareHandler(object):
+    """ Async prepare handler which reports to MetaHandler.
+
+    Attributes:
+        path (string): Directory path for which the handler is created.
+        meta_handler (MetaHandler): Meta handler object.
+    """
+    def __init__(self, path, meta_handler):
+        self.type = 'prepare'
+        self.path = path
+        self.meta_handler = meta_handler
+
+    def __call__(self, status, response, hostlist):
+        self.meta_handler.handle(self.type, status, self.path)
+
+
+class MetaHandler(object):
+    """ Meta handler for different types of async requests.
+
+    Attributes:
+        cond: Condition variable used for synchronization.
+        logger: Logger object.
+        mkdir_failed: List of directories failed to create.
+        mkdir_status: Status of mkdir requests, logical and between individual
+            mkdir commands.
+        mkdir_num: Number of mkdir commands waiting for reply.
+    """
+    def __init__(self):
+        list_op = ['mkdir', 'prepare']
+        self.num, self.status, self.failed = {}, {}, {}
+        self.handlers = {'mkdir': _MkDirHandler,
+                         'prepare': _PrepareHandler}
+
+        for op in list_op:
+            self.num[op] = 0
+            self.status[op] = True
+            self.failed[op] = []
+
+        self.cond = Condition()
+        self.logger = logging.getLogger("archivefile.transfer")
+
+    def register(self, op, path):
+        """ Register handler for operation.
+        """
+        self.cond.acquire()
+        self.num[op] += 1
+        self.cond.release()
+        return self.handlers[op](path, self)
+
+    def handle(self, op, status, path):
+        """Handle incoming response.
+        """
+        self.cond.acquire()
+        self.status[op] = self.status[op] and status.ok
+        self.num[op] -= 1
+
+        if not status.ok:
+            self.failed[op].append(path)
+
+        if self.num[op] == 0:
+            self.cond.notifyAll()
+
+            self.cond.release()
+
+    def wait(self, op):
+        """Wait for all responses to arrive.
+        """
+        self.cond.acquire()
+
+        while self.num[op] != 0:
+            self.cond.wait()
+
+        if self.failed[op]:
+            self.logger.error("List of failed {0} paths is: {0}".format(op, self.failed[op]))
+        else:
+            self.logger.debug("All {0} requests were successful".format(op))
+
+        self.cond.release()
+        return self.status[op]
