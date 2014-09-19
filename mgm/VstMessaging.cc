e98b87286faf02c78fdd8b56594cb2a815f801a4
Andreas Peters <Andreas.Joachim.Peters@cern.ch>
2014-09-19 09:54:56 +0200

    MGM: fix re-entrant lock in VstMessaging

--- mgm/VstMessaging.cc
+++ mgm/VstMessaging.cc
@@ -277,24 +277,24 @@ VstMessaging::PublishVst ()
     eos::common::RWMutexReadLock vlock(FsView::gFsView.ViewMutex);
     if (FsView::gFsView.mSpaceView.count("default"))
     {
-      freebytes = FsView::gFsView.mSpaceView["default"]->SumLongLong("stat.statfs.freebytes?configstatus@rw");
-      freefiles = FsView::gFsView.mSpaceView["default"]->SumLongLong("stat.statfs.ffree?configstatus@rw");
+      freebytes = FsView::gFsView.mSpaceView["default"]->SumLongLong("stat.statfs.freebytes?configstatus@rw",false);
+      freefiles = FsView::gFsView.mSpaceView["default"]->SumLongLong("stat.statfs.ffree?configstatus@rw",false);
 
-      maxbytes = FsView::gFsView.mSpaceView["default"]->SumLongLong("stat.statfs.capacity?configstatus@rw");
-      maxfiles = FsView::gFsView.mSpaceView["default"]->SumLongLong("stat.statfs.files?configstatus@rw");
+      maxbytes = FsView::gFsView.mSpaceView["default"]->SumLongLong("stat.statfs.capacity?configstatus@rw",false);
+      maxfiles = FsView::gFsView.mSpaceView["default"]->SumLongLong("stat.statfs.files?configstatus@rw",false);
 
-      ethin = FsView::gFsView.mSpaceView["default"]->SumLongLong("stat.net.inratemib");
-      ethout = FsView::gFsView.mSpaceView["default"]->SumLongLong("stat.net.outratemib");
+      ethin = FsView::gFsView.mSpaceView["default"]->SumLongLong("stat.net.inratemib",false);
+      ethout = FsView::gFsView.mSpaceView["default"]->SumLongLong("stat.net.outratemib",false);
 
-      diskin = FsView::gFsView.mSpaceView["default"]->SumLongLong("stat.disk.readratemb");
-      diskout = FsView::gFsView.mSpaceView["default"]->SumLongLong("stat.disk.writeratemb");
+      diskin = FsView::gFsView.mSpaceView["default"]->SumLongLong("stat.disk.readratemb",false);
+      diskout = FsView::gFsView.mSpaceView["default"]->SumLongLong("stat.disk.writeratemb",false);
 
-      ropen = FsView::gFsView.mSpaceView["default"]->SumLongLong("stat.ropen");
-      wopen = FsView::gFsView.mSpaceView["default"]->SumLongLong("stat.wopen");
+      ropen = FsView::gFsView.mSpaceView["default"]->SumLongLong("stat.ropen",false);
+      wopen = FsView::gFsView.mSpaceView["default"]->SumLongLong("stat.wopen",false);
 
-      nfsrw = FsView::gFsView.mSpaceView["default"]->SumLongLong("<n>?configstatus@rw");
-      iops = FsView::gFsView.mSpaceView["default"]->SumLongLong("stat.disk.iops?configstatus@rw");
-      bw = FsView::gFsView.mSpaceView["default"]->SumLongLong("stat.disk.bw?configstatus@rw");
+      nfsrw = FsView::gFsView.mSpaceView["default"]->SumLongLong("<n>?configstatus@rw",false);
+      iops = FsView::gFsView.mSpaceView["default"]->SumLongLong("stat.disk.iops?configstatus@rw",false);
+      bw = FsView::gFsView.mSpaceView["default"]->SumLongLong("stat.disk.bw?configstatus@rw",false);
     }
   }
   {
