91c0efa62d29939148cf4b41775c1b352bb9ddc6
Andreas Peters <Andreas.Joachim.Peters@cern.ch>
2014-10-06 12:40:10 +0200

    HTTP/FST: disable checksumming after each chunk upload unless it is the last chunk

--- fst/http/HttpHandler.cc
+++ fst/http/HttpHandler.cc
@@ -476,6 +476,12 @@ HttpHandler::Put (eos::common::HttpRequest *request)
         mFile->SetForcedMtime(strtoull(header["x-oc-mtime"].c_str(), 0, 10), 0);
       }
 
+      if (!mLastChunk)
+      {
+	// WARNING: this assumes that chunks are uploaded in order
+	mFile->disableChecksum();
+      }
+
       mCloseCode = mFile->close();
       if (mCloseCode)
       {
