2d16ea67ab141720b1ebeeda1ccad0c9aad1b858
Andreas Peters <Andreas.Joachim.Peters@cern.ch>
2014-12-16 17:26:01 +0100

    COMMON: fix wrong client.name setting after tident mapping

--- common/Mapping.cc
+++ common/Mapping.cc
@@ -640,13 +640,6 @@ Mapping::IdMap (const XrdSecEntity* client, const char* env, const char* tident,
     }
   }
 
-  if (gVirtualUidMap.count(tuid.c_str()) ||
-      gVirtualGidMap.count(tgid.c_str()))
-  {
-    // indicate in the name that this is a mapped tident
-    vid.name = "tident";
-  }
-
   eos_static_debug("suidtident:%s sgidtident:%s", suidtident.c_str(), sgidtident.c_str());
 
   // ---------------------------------------------------------------------------
