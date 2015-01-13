983bf78e703f7689c68f2ef6397ea33d3f745d2e
Elvin Sindrilaru <esindril@cern.ch>
2015-01-13 15:53:41 +0100

    CONSOLE: Add backup tool support for time window option for either ctime or mtime

--- console/commands/com_backup.cc
+++ console/commands/com_backup.cc
@@ -24,6 +24,7 @@
 /*----------------------------------------------------------------------------*/
 #include <sstream>
 #include <memory>
+#include <sys/time.h>
 /*----------------------------------------------------------------------------*/
 #include "console/ConsoleMain.hh"
 #include "XrdCl/XrdClFile.hh"
@@ -32,26 +33,88 @@
 
 int com_backup(char* arg1)
 {
-  XrdOucString in = "";
+  XrdOucString in, token;
   std::ostringstream in_cmd;
   eos::common::StringTokenizer subtokenizer(arg1);
   subtokenizer.GetLine();
+  XrdCl::URL src_url;
+  XrdCl::URL dst_url;
   XrdOucString src_surl = subtokenizer.GetToken();
   XrdOucString dst_surl = subtokenizer.GetToken();
 
+  // Check if minimal input is present
   if (!src_surl.length() || !dst_surl.length())
+    goto com_backup_usage;
+
+  // Check that these are valid XRootD URLs
+  if (!src_url.FromString(src_surl.c_str()) ||
+      !dst_url.FromString(dst_surl.c_str()))
     goto com_backup_usage;
 
   in_cmd << "mgm.cmd=backup&mgm.backup.src=" << src_surl
          << "&mgm.backup.dst=" << dst_surl;
 
+  token = subtokenizer.GetToken();
+
+  if (token.length())
+  {
+    if (!token.beginswith("--"))
+      goto com_backup_usage;
+
+    // Get the type of incremental backup either by mtime or by ctime
+    if (token == "--ctime")
+      in_cmd << "&mgm.backup.ttime=ctime";
+    else if (token == "--mtime")
+      in_cmd << "&mgm.backup.ttime=mtime";
+    else
+      goto com_backup_usage;
+
+    // Get the interval time
+    token = subtokenizer.GetToken();
+
+    if (!token.length())
+      goto com_backup_usage;
+
+    char last = token[token.length() - 1];
+    long int seconds = 0;
+    if (last == 's')
+      seconds = 1; // seconds
+    else if (last == 'm')
+      seconds = 60; //minutes
+    else if (last == 'h')
+      seconds = 3600; // hours
+    else if (last == 'd')
+      seconds = 24 * 3600; // days
+    else
+      goto com_backup_usage;
+
+    // Try to convert the time window to integer value
+    char* p_end = (char*)(token.c_str() + token.length());
+    long int value = strtol(token.c_str(), &p_end, 10);
+
+    if (value == 0L)
+      goto com_backup_usage;
+
+    value *= seconds;
+    struct timeval tv;
+
+    if (gettimeofday(&tv, NULL))
+    {
+      fprintf(stderr, "Error getting current timestamp\n");
+      goto com_backup_usage;
+    }
+
+
+    in_cmd << "&mgm.backup.vtime=" << (tv.tv_sec - value);
+  }
+
   in = in_cmd.str().c_str();
-  global_retc = output_result(client_user_command(in));
+  global_retc = output_result(client_admin_command(in));
   return 0;
 
 com_backup_usage:
   std::ostringstream oss;
-  oss << "usage: backup <src_url> <dst_url>" << std::endl
+  oss << "usage: backup <src_url> <dst_url> [--ctime|mtime <val>s|m|h|d]" << std::endl
       << "              create a backup of the subtree rooted in " << std::endl
       << "              <src_url> and save it at location <dst_url>" << std::endl;
 
