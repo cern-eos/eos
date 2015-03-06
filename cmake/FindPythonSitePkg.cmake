15e859423e8ac592ae5eef9700674b566a05f7c4
Elvin Sindrilaru <esindril@cern.ch>
2015-03-06 16:04:22 +0100

    CMAKE: Fix case spelling for PYTHONSITEPKG_ variables

--- cmake/FindPythonSitePkg.cmake
+++ cmake/FindPythonSitePkg.cmake
@@ -1,22 +1,22 @@
 # Try to find python
 # Once done, this will define
 #
-# PythonSitePkg_FOUND - found python site packages directory
-# PythonSitePkg_PATH  - location where python modules are installed
+# PYTHONSITEPKG_FOUND - found python site packages directory
+# PYTHONSITEPKG_PATH  - location where python modules are installed
 
-if(PythonSitePkg_FIND_REQUIRED)
+if(PYTHONSITEPKG_FIND_REQUIRED)
   find_package(PythonInterp REQUIRED)
 else()
   find_package(PythonInterp)
 endif()
 
 if(NOT PYTHONINTERP_FOUND)
-  set(PythonSitePkg_FOUND FALSE)
+  set(PYTHONSITEPKG_FOUND FALSE)
   return()
 endif()
 
-if(PythonSitePkg_PATH)
- set(PythonSitePkg_FIND_QUIETLY TRUE)
+if(PYTHONSITEPKG_PATH)
+ set(PYTHONSITEPKG_FIND_QUIETLY TRUE)
 else()
   execute_process(
     COMMAND "${PYTHON_EXECUTABLE}" "-c"
@@ -26,11 +26,11 @@ print(sysconfig.get_python_lib(True));
 "
     RESULT_VARIABLE _PYTHON_SUCCESS
     ERROR_VARIABLE _PYTHON_ERROR_VALUE
-    OUTPUT_VARIABLE PythonSitePkg_PATH
+    OUTPUT_VARIABLE PYTHONSITEPKG_PATH
     OUTPUT_STRIP_TRAILING_WHITESPACE)
 
   if(NOT _PYTHON_SUCCESS MATCHES 0)
-    if(PythonSitePkg_FIND_REQUIRED)
+    if(PYTHONSITEPKG_FIND_REQUIRED)
       message(FATAL_ERROR "Python config failure:\n${_PYTHON_ERROR_VALUE}")
     else()
       message(STATUS "PythonSitePkg was not required")
@@ -40,12 +40,12 @@ print(sysconfig.get_python_lib(True));
     return()
   endif()
 
-  string(REGEX REPLACE "\n" "" PythonSitePkg_PATH ${PythonSitePkg_PATH})
+  string(REGEX REPLACE "\n" "" PYTHONSITEPKG_PATH ${PYTHONSITEPKG_PATH})
 
   include (FindPackageHandleStandardArgs)
   find_package_handle_standard_args(
     PythonSitePkg
-    DEFAULT_MSG PythonSitePkg_PATH)
+    DEFAULT_MSG PYTHONSITEPKG_PATH)
 
-  mark_as_advanced(PythonSitePkg_PATH)
+  mark_as_advanced(PythonSitePkg PYTHONSITEPKG_PATH)
 endif()
