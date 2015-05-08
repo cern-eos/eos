709f4e23f294878d9c8585af06ce8689e64960aa
Andreas Peters <Andreas.Joachim.Peters@cern.ch>
2015-05-08 10:33:52 +0200

    UTILS: add script installing all build dependencies on SLC6

---
+++ utils/sl6-packages.sh
@@ -0,0 +1,16 @@
+yum install -y cmake
+yum install -y xrootd-server-devel xrootd-private-devel
+yum install -y sparsehash-devel
+yum install -y ncurses ncurses-devel ncurses-static openssl openssl-devel openssl-static
+yum install -y readline readline-devel
+yum install -y libuuid libuuid-devel
+yum install -y zeromq zeromq-devel
+yum install -y protobuf protobuf-devel leveldb leveldb-devel
+yum install -y cppunit cppunit-devel
+yum install -y fuse-libs fuse fuse-devel
+yum install -y libattr libattr-devel
+yum install -y openldap openldap-devel
+yum install -y libmicrohttp libmicrohttpd-devel
+yum install -y zlib zlib-devel zlib-static
+
+
