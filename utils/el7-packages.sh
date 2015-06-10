baa6daff216ca390ccdfa28e2ad77402c04ae423
root <root@diamondns.cern.ch>
2015-06-10 11:57:23 +0200

    UTILS: add script to prepare a CENTOS7 installation

---
+++ utils/el7-packages.sh
@@ -0,0 +1,18 @@
+yum install -y cmake
+yum install -y xrootd-server-devel xrootd-private-devel
+yum install -y sparsehash-devel
+yum install -y ncurses ncurses-devel ncurses-static openssl openssl-devel openssl-static
+yum install -y readline readline-devel
+yum install -y libuuid libuuid-devel
+yum install -y zeromq3 zeromq3-devel
+yum install -y protobuf protobuf-devel leveldb leveldb-devel
+yum install -y cppunit cppunit-devel
+yum install -y fuse-libs fuse fuse-devel
+yum install -y libattr libattr-devel
+yum install -y openldap openldap-devel
+yum install -y libmicrohttp libmicrohttpd-devel
+yum install -y zlib zlib-devel zlib-static
+yum install -y xfsprogs xfsprogs-devel
+yum install -y e2fsprogs-devel
+( cd /tmp/; git clone  https://github.com/zeromq/cppzmq; cp cppzmq/zmq.hpp /usr/include/ )
+
