348c9de2fb7bb893b92094250c414bf7a3b96729
Andreas Peters <Andreas.Joachim.Peters@cern.ch>
2015-09-01 14:31:44 +0200

    FST: unlink temporary boot meta data dump before reading it to avoid that it lingers around after a crash

--- fst/FmdSqlite.cc
+++ fst/FmdSqlite.cc
@@ -1253,6 +1253,9 @@ FmdSqliteHandler::ResyncAllMgm (eos::common::FileSystem::fsid_t fsid, const char
   std::ifstream inFile(tmpfile);
   std::string dumpentry;
 
+  // unlink the temporary file
+  unlink(tmpfile);
+
   unsigned long long cnt = 0;
   while (std::getline(inFile, dumpentry))
   {
@@ -1304,8 +1307,6 @@ FmdSqliteHandler::ResyncAllMgm (eos::common::FileSystem::fsid_t fsid, const char
 
   isSyncing[fsid] = false;
 
-  // remove the temporary file
-  unlink(tmpfile);
   free(tmpfile);
   return true;
 }
