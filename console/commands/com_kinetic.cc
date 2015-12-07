1e11c16f32cdab509e8761ebf23639cfe763d787
Paul Lensing <paul.lensing@cern.ch>
2015-12-07 12:04:44 +0100

    CONSOLE: Fixed error in CONFIG operation execution.

--- console/commands/com_kinetic.cc
+++ console/commands/com_kinetic.cc
@@ -302,17 +302,26 @@ com_kinetic (char *arg)
     return EXIT_FAILURE;
   }
 
-  setEnvironmentVariables(config);
-
-  std::function<void(int)> callback;
-  if(!config.monitoring)
-    callback = printincremental;
-
   try{
+
+    switch(config.op){
+      case Operation::CONFIG_SHOW:
+      case Operation::CONFIG_PUBLISH:
+        doConfig(config);
+        return EXIT_SUCCESS;
+      default:
+        break;
+    }
+
+    setEnvironmentVariables(config);
     auto ac = kio::KineticIoFactory::makeAdminCluster(
         config.id.c_str(),
         config.target == OperationTarget::DATA ? kio::RedundancyType::ERASURE_CODING : kio::RedundancyType::REPLICATION
     );
+
+    std::function<void(int)> callback;
+    if(!config.monitoring)
+      callback = printincremental;
 
     switch(config.op){
       case Operation::STATUS:{
@@ -353,10 +362,6 @@ com_kinetic (char *arg)
         if(!config.monitoring)
           fprintf(stdout, "Removing keys from cluster: \n");
         printStatistics(ac->reset(config.target, callback, config.numthreads), config);
-        break;
-      case Operation::CONFIG_SHOW:
-      case Operation::CONFIG_PUBLISH:
-        doConfig(config);
         break;
       case Operation::INVALID:
       default:
