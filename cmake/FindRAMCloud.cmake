83edd233b97aa0dc90512969a35586e29e065f85
Elvin Sindrilaru <esindril@cern.ch>
2016-03-31 16:25:40 +0200

    CMAKE: Add find package module for RAMCloud

---
+++ cmake/FindRAMCloud.cmake
@@ -0,0 +1,36 @@
+# Try to find RAMCloud client
+# Once done, this will define
+#
+# RAMCLOUD_FOUND          - system has RAMCloud
+# RAMCLOUD_INCLUDE_DIRS   - RAMCloud include directories
+# RAMCLOUD_LIBRARY        - RAMCloud library directory
+
+include(FindPackageHandleStandardArgs)
+
+if(RAMCLOUD_INCLUDE_DIRS AND RAMCLOUD_LIBRARIES)
+  set(RAMCLOUD_FIND_QUIETLY TRUE)
+else()
+  find_path(
+    RAMCLOUD_INCLUDE_DIR
+    NAMES ramcloud/RamCloud.h
+    HINTS ${RAMCLOUD_ROOT_DIR}
+    PATH_SUFFIXES include)
+
+  find_library(
+    RAMCLOUD_LIBRARY
+    NAMES ramcloud
+    HINTS ${RAMCLOUD_ROOT_DIR}
+    PATH_SUFFIXES ${LIBRARY_PATH_PREFIX})
+
+  set(RAMCLOUD_INCLUDE_DIRS ${RAMCLOUD_INCLUDE_DIR})
+
+  find_package_handle_standard_args(
+    RAMCloud
+    DEFAULT_MSG
+    RAMCLOUD_LIBRARY
+    RAMCLOUD_INCLUDE_DIR)
+
+  mark_as_advanced(
+    RAMCLOUD_LIBRARY
+    RAMCLOUD_INCLUDE_DIR)
+endif()
