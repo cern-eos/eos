ee641cf19bc13a59a81f1bd7468522ef6865cb24
Andreas Peters <Andreas.Joachim.Peters@cern.ch>
2016-04-27 14:26:40 +0200

    FUSE: reduce page size in FuseCache and make it configurable via EOS_FUSE_CACHE_PAGE_SIZE

--- fuse/filesystem.cc
+++ fuse/filesystem.cc
@@ -52,6 +52,7 @@
 #include "XrdCl/XrdClXRootDResponses.hh"
 /*----------------------------------------------------------------------------*/
 
+#include "FuseCache/CacheEntry.hh"
 #include "filesystem.hh"
 
 #ifndef __macos__
@@ -133,6 +134,11 @@ filesystem::log_settings ()
  s = "write-cache-size       := ";
  std::string efcs = getenv ("EOS_FUSE_CACHE_SIZE") ? getenv ("EOS_FUSE_CACHE_SIZE") : "0";
  s += efcs;
+ log ("WARNING", s.c_str ());
+
+ s = "write-cache-page-size  := ";
+ std::string efpcs = getenv ("EOS_FUSE_CACHE_PAGE_SIZE") ? getenv ("EOS_FUSE_CACHE_PAGE_SIZE") : "(default 262144)";
+ s += efpcs;
  log ("WARNING", s.c_str ());
 
  s = "big-writes             := ";
@@ -4502,6 +4508,12 @@ filesystem::init (int argc, char* argv[], void *userdata, std::map<std::string,s
    XFC = FuseWriteCache::GetInstance (static_cast<size_t> (atol (getenv ("EOS_FUSE_CACHE_SIZE"))));
 
    fuse_cache_write = true;
+ }
+
+
+ if ((getenv("EOS_FUSE_CACHE_PAGE_SIZE")))
+ {
+   CacheEntry::SetMaxSize((size_t)strtoul(getenv("EOS_FUSE_CACHE_PAGE_SIZE"), 0, 10));
  }
 
  // Get the number of levels in the top hierarchy protected agains deletions
