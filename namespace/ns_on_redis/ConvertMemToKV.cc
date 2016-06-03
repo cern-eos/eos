f73529d2121e9934596433e476c1df71cec11f01
Elvin Sindrilaru <esindril@cern.ch>
2016-06-03 11:58:34 +0200

    NS: Add tool to convert namespace from in-memory to KV-store representation

---
+++ namespace/ns_on_redis/ConvertMemToKV.cc
@@ -0,0 +1,414 @@
+/************************************************************************
+ * EOS - the CERN Disk Storage System                                   *
+ * Copyright (C) 2016 CERN/Switzerland                                  *
+ *                                                                      *
+ * This program is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ ************************************************************************/
+
+//------------------------------------------------------------------------------
+//! @author Elvin-Alin Sindrilaru <esindril@cern.ch>
+//! @brief Executable used to convert an in-memory namespace representation to
+//! to a KV one.
+//------------------------------------------------------------------------------
+
+#include "namespace/ns_on_redis/ConvertMemToKV.hh"
+#include "namespace/ns_on_redis/RedisClient.hh"
+#include "namespace/ns_on_redis/Constants.hh"
+#include "namespace/utils/StringConvertion.hh"
+#include "namespace/ns_in_memory/FileMD.hh"
+#include "namespace/ns_in_memory/persistency/ChangeLogConstants.hh"
+#include "namespace/Constants.hh"
+#include "common/LayoutId.hh"
+
+// Static global variables
+static redox::Redox* sRedox;
+
+EOSNSNAMESPACE_BEGIN
+
+std::uint64_t ConvertContainerMDSvc::sNumContBuckets = 128 * 1024;
+std::uint64_t ConvertFileMDSvc::sNumFileBuckets = 1024 * 1024;
+
+//------------------------------------------------------------------------------
+//           ************* ConvertContainerMD Class ************
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Constructor
+//------------------------------------------------------------------------------
+ConvertContainerMD::ConvertContainerMD(id_t id, IFileMDSvc* file_svc,
+				       IContainerMDSvc* cont_svc):
+  eos::ContainerMD(id, file_svc, cont_svc)
+{
+  pFilesKey = stringify(id) + constants::sMapFilesSuffix;
+  pDirsKey = stringify(id) + constants::sMapDirsSuffix;
+}
+
+//------------------------------------------------------------------------------
+// Update internal state
+//------------------------------------------------------------------------------
+void
+ConvertContainerMD::updateInternal()
+{
+  pFilesKey = stringify(pId) + constants::sMapFilesSuffix;
+  pDirsKey = stringify(pId) + constants::sMapDirsSuffix;
+}
+
+//------------------------------------------------------------------------------
+// Add container to the KV store
+//------------------------------------------------------------------------------
+void
+ConvertContainerMD::addContainer(eos::IContainerMD* container)
+{
+  try {
+    sRedox->hset(pDirsKey, container->getName(), container->getId());
+  } catch (std::runtime_error& redis_err) {
+    MDException e(EINVAL);
+    e.getMessage() << "Failed to add subcontainer #" << container->getId()
+		   << " or KV-backend connection error";
+    throw e;
+  }
+}
+
+//------------------------------------------------------------------------------
+// Add file to container in the KV store
+//------------------------------------------------------------------------------
+void
+ConvertContainerMD::addFile(eos::IFileMD* file)
+{
+  try {
+    sRedox->hset(pFilesKey, file->getName(), file->getId());
+  } catch (std::runtime_error& redis_err) {
+    MDException e(EINVAL);
+    e.getMessage() << "File #" << file->getId() << " already exists or"
+		   << " KV-backend conntection error";
+    throw e;
+  }
+}
+
+//------------------------------------------------------------------------------
+//         ************* ConvertContainerMDSvc Class ************
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Convert the containers
+//------------------------------------------------------------------------------
+void
+ConvertContainerMDSvc::recreateContainer(IdMap::iterator& it,
+    ContainerList& orphans,
+    ContainerList& nameConflicts)
+{
+  eos::Buffer ebuff;
+  pChangeLog->readRecord(it->second.logOffset, ebuff);
+  std::shared_ptr<IContainerMD> container =
+    std::make_shared<ConvertContainerMD>(IContainerMD::id_t(0), pFileSvc, this);
+  static_cast<ConvertContainerMD*>(container.get())->deserialize(ebuff);
+  static_cast<ConvertContainerMD*>(container.get())->updateInternal();
+  it->second.ptr = container;
+
+  // For non-root containers recreate the parent
+  if (container->getId() != container->getParentId()) {
+    IdMap::iterator parentIt = pIdMap.find(container->getParentId());
+
+    if (parentIt == pIdMap.end()) {
+      orphans.push_back(container);
+      return;
+    }
+
+    if (!(parentIt->second.ptr))
+      recreateContainer(parentIt, orphans, nameConflicts);
+
+    std::shared_ptr<IContainerMD> parent = parentIt->second.ptr;
+    std::shared_ptr<IContainerMD> child  = parent->findContainer(
+	container->getName());
+
+    if (!child) {
+      parent->addContainer(container.get());
+      exportToQuotaView(container.get());
+    } else {
+      nameConflicts.push_back(child);
+      parent->addContainer(container.get());
+    }
+  }
+
+  // Add container to the KV store
+  try {
+    std::string buffer(ebuff.getDataPtr(), ebuff.getSize());
+    std::string sid = stringify(container->getId());
+    std::string bucket_key = getBucketKey(container->getId());
+    sRedox->hset(bucket_key, sid, buffer);
+  } catch (std::runtime_error& redis_err) {
+    MDException e(ENOENT);
+    e.getMessage() << "Container #" << container->getId()
+		   << " failed to contact backend";
+    throw e;
+  }
+}
+
+//------------------------------------------------------------------------------
+// Export container info to the quota view
+//------------------------------------------------------------------------------
+void
+ConvertContainerMDSvc::exportToQuotaView(IContainerMD* cont)
+{
+  if (cont->getFlags() & QUOTA_NODE_FLAG) {
+    sRedox->sadd(quota::sSetQuotaIds, cont->getId());
+  }
+}
+
+//------------------------------------------------------------------------------
+// Get container bucket
+//------------------------------------------------------------------------------
+std::string
+ConvertContainerMDSvc::getBucketKey(IContainerMD::id_t id) const
+{
+  if (id >= sNumContBuckets)
+    id = id & (sNumContBuckets - 1);
+
+  std::string bucket_key = stringify(id);
+  bucket_key += constants::sContKeySuffix;
+  return bucket_key;
+}
+
+//------------------------------------------------------------------------------
+//         ************* ConvertFileMDSvc Class ************
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Get file bucket
+//------------------------------------------------------------------------------
+std::string
+ConvertFileMDSvc::getBucketKey(IContainerMD::id_t id) const
+{
+  if (id >= sNumFileBuckets)
+    id = id & (sNumFileBuckets - 1);
+
+  std::string bucket_key = stringify(id);
+  bucket_key += constants::sFileKeySuffix;
+  return bucket_key;
+}
+
+//------------------------------------------------------------------------
+// Initialize the file service
+//------------------------------------------------------------------------
+void
+ConvertFileMDSvc::initialize()
+{
+  pIdMap.resize(pResSize);
+
+  if (!pContSvc) {
+    MDException e(EINVAL);
+    e.getMessage() << "ConvertFileMDSvc: container service not set";
+    throw e;
+  }
+
+  int logOpenFlags = ChangeLogFile::Create | ChangeLogFile::Append;
+  // Rescan the change log if needed
+  pChangeLog->open(pChangeLogPath, logOpenFlags, FILE_LOG_MAGIC);
+  pFollowStart = pChangeLog->getFirstOffset();
+  FileMDScanner scanner(pIdMap, pSlaveMode);
+  pFollowStart = pChangeLog->scanAllRecords(&scanner);
+  pFirstFreeId = scanner.getLargestId() + 1;
+
+  // Recreate the files
+  for (auto it = pIdMap.begin(); it != pIdMap.end(); ++it) {
+    // Unpack the serialized buffers
+    std::shared_ptr<IFileMD> file = std::make_shared<FileMD>(0, this);
+    static_cast<FileMD*>(file.get())->deserialize(*it->second.buffer);
+
+    // Attach to the hierarchy
+    if (file->getContainerId() == 0)
+      continue;
+
+    // Add file to the KV store
+    try {
+      std::string buffer(it->second.buffer->getDataPtr(),
+			 it->second.buffer->getSize());
+      std::string sid = stringify(file->getId());
+      std::string bucket_key = getBucketKey(file->getId());
+      sRedox->hset(bucket_key, sid, buffer);
+    } catch (std::runtime_error& redis_err) {
+      MDException e(ENOENT);
+      e.getMessage() << "File #" << file->getId()
+		     << " failed to contact backend";
+      throw e;
+    }
+
+    // Free the memory used by the buffer
+    delete it->second.buffer;
+    it->second.buffer = 0;
+    std::shared_ptr<IContainerMD> cont;
+
+    try {
+      cont = pContSvc->getContainerMD(file->getContainerId());
+    } catch (MDException& e) {}
+
+    if (!cont) {
+      attachBroken("orphans", file.get());
+      continue;
+    }
+
+    if (cont->findFile(file->getName())) {
+      attachBroken("name_conflicts", file.get());
+      continue;
+    } else {
+      cont->addFile(file.get());
+      // Populate the FilsSystemView and QuotaView
+      exportToFsView(file.get());
+      exportToQuotaView(file.get());
+    }
+  }
+}
+
+//------------------------------------------------------------------------------
+// Export file info to the file-system view
+//------------------------------------------------------------------------------
+void
+ConvertFileMDSvc::exportToFsView(IFileMD* file)
+{
+  IFileMD::LocationVector::const_iterator it;
+  IFileMD::LocationVector loc_vect = file->getLocations();
+  std::string key, val;
+
+  for (it = loc_vect.begin(); it != loc_vect.end(); ++it) {
+    // Store fsid if it doesn't exist
+    key = fsview::sSetFsIds;
+    val = stringify(*it);
+
+    if (!sRedox->sismember(key, val))
+      sRedox->sadd(key, val);
+
+    // Add file to corresponding fs file set
+    key = val + fsview::sFilesSuffix;
+    sRedox->sadd(key, stringify(file->getId()));
+  }
+
+  IFileMD::LocationVector unlink_vect = file->getUnlinkedLocations();
+
+  for (it = unlink_vect.begin(); it != unlink_vect.end(); ++it) {
+    key = stringify(*it) + fsview::sUnlinkedSuffix;;
+    sRedox->sadd(key, stringify(file->getId()));
+  }
+
+  if (file->getNumLocation() == 0 && file->getNumUnlinkedLocation() == 0)
+    sRedox->sadd(fsview::sNoReplicaPrefix, stringify(file->getId()));
+}
+
+//------------------------------------------------------------------------------
+// Export file info to the quota view
+//------------------------------------------------------------------------------
+void
+ConvertFileMDSvc::exportToQuotaView(IFileMD* file)
+{
+  // Search for a quota node
+  std::shared_ptr<IContainerMD> current =
+    pContSvc->getContainerMD(file->getContainerId());
+
+  while ((current->getId() != 1) &&
+	 ((current->getFlags() & QUOTA_NODE_FLAG) == 0)) {
+    current = pContSvc->getContainerMD(current->getParentId());
+  }
+
+  if ((current->getFlags() & QUOTA_NODE_FLAG) == 0)
+    return;
+
+  // Add current file to the hmap contained in the current quota node
+  std::string quota_uid_key = stringify(current->getId()) + quota::sQuotaUidsSuffix;
+  std::string quota_gid_key = stringify(current->getId()) + quota::sQuotaGidsSuffix;
+  const std::string suid = stringify(file->getCUid());
+  const std::string sgid = stringify(file->getCGid());
+  // Compute physical size
+  eos::IFileMD::layoutId_t lid = file->getLayoutId();
+  const int64_t size = file->getSize() * eos::common::LayoutId::GetSizeFactor(
+			 lid);
+  std::string field = suid + quota::sPhysicalSpaceTag;
+  (void) sRedox->hincrby(quota_uid_key, field, size);
+  field = sgid + quota::sPhysicalSpaceTag;
+  (void) sRedox->hincrby(quota_gid_key, field, size);
+  field = suid + quota::sSpaceTag;
+  (void) sRedox->hincrby(quota_uid_key, field, file->getSize());
+  field = sgid + quota::sSpaceTag;
+  (void) sRedox->hincrby(quota_gid_key, field, file->getSize());
+  field = suid + quota::sFilesTag;
+  (void) sRedox->hincrby(quota_uid_key, field, 1);
+  field = sgid + quota::sFilesTag;
+  (void) sRedox->hincrby(quota_gid_key, field, 1);
+}
+
+EOSNSNAMESPACE_END
+
+//------------------------------------------------------------------------------
+// Print usage information
+//------------------------------------------------------------------------------
+void usage()
+{
+  std::cerr << "Usage:                                            " << std::endl
+	    << "  ./convert_mem_to_kv <file_chlog> <dir_chlog> <redis_host> "
+	    << "<redis_port>" << std::endl
+	    << "    file_chlog - file changelog                   " << std::endl
+	    << "    dir_chlog  - directory changelog              " << std::endl
+	    << "    redis_host - Redis host destination           " << std::endl
+	    << "    redis_port - Redis port destination           " << std::endl;
+}
+
+//------------------------------------------------------------------------------
+// Main function
+//------------------------------------------------------------------------------
+int main(int argc, char* argv[])
+{
+  if (argc != 5) {
+    usage();
+    return 1;
+  }
+
+  std::string file_chlog(argv[1]);
+  std::string dir_chlog(argv[2]);
+  std::string redis_host(argv[3]);
+  std::uint32_t redis_port(std::stoull(argv[4]));
+  sRedox = eos::RedisClient::getInstance(redis_host, redis_port);
+  // Check file and directory changelog files
+  int ret;
+  struct stat info;
+  std::list<std::string> lst_files {file_chlog, dir_chlog};
+
+  for (auto& fn : lst_files) {
+    ret = stat(fn.c_str(), &info);
+
+    if (ret) {
+      std::cerr << "Unable to access file: " << fn << std::endl;
+      return EIO;
+    }
+  }
+
+  std::unique_ptr<eos::IFileMDSvc> file_svc(new eos::ConvertFileMDSvc());
+  std::unique_ptr<eos::IContainerMDSvc> cont_svc(new
+      eos::ConvertContainerMDSvc());
+  std::map<std::string, std::string> config_cont {
+    {"changelog_path", dir_chlog},
+    {"slave_mode", "false"}
+  };
+  std::map<std::string, std::string> config_file {
+    {"changelog_path", file_chlog},
+    {"slave_mode", "false"}
+  };
+  // Initialize the container meta-data service
+  cont_svc->setFileMDService(file_svc.get());
+  cont_svc->configure(config_cont);
+  cont_svc->initialize();
+  // Initialize the file meta-data service
+  file_svc->setContMDService(cont_svc.get());
+  file_svc->configure(config_file);
+  file_svc->initialize();
+  // TODO: save the first free file and container id in the meta_hmap
+  return 0;
+}
