82b4f659a23030f1506c5baa013da4a5acabbc93
Geoffray Adde <geoffray.adde@cern.ch>
2016-06-28 11:13:34 +0200

    BUILD: add eos dependencies repo to the jenkins build to be sure that microhttpd has the right version

--- utils/jenkins-build.sh
+++ utils/jenkins-build.sh
@@ -161,6 +161,8 @@ if [[ ${BRANCH} == 'emerald' || ${BRANCH} == 'danburite' ]]; then
     # Add kineticio and kineticio dependency repos
     echo -e '\n[kio-depend]\nname=kio-depend\nbaseurl=https://dss-ci-repo.web.cern.ch/dss-ci-repo/kinetic/kineticio-depend/'$PLATFORM'-'$ARCHITECTURE'\nenabled=1 \n' >> eos.cfg
     echo -e '\n[kio]\nname=kio\nbaseurl=https://dss-ci-repo.web.cern.ch/dss-ci-repo/kinetic/kineticio/'$PLATFORM'-'$ARCHITECTURE'\nenabled=1 \n' >> eos.cfg
+    # Add eos dependencies repos
+    echo -e '\n[eos-depend]\nname=EOS Dependencies\nbaseurl=http://dss-ci-repo.web.cern.ch/dss-ci-repo/eos/'${BRANCH}'-depend/'$PLATFORM'-'$ARCHITECTURE'/\ngpgcheck=0\nenabled=1 \n' >> eos.cfg
 fi
 echo -e '"""' >> eos.cfg
 
