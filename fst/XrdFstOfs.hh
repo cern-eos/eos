e6f3c0c80a73d552a3b1ee580170b056ed6a4f4f
Andreas Peters <Andreas.Joachim.Peters@cern.ch>
2016-07-01 18:27:56 +0200

    FST: fix after merge

--- fst/XrdFstOfs.hh
+++ fst/XrdFstOfs.hh
@@ -391,8 +391,6 @@ public:
   bool Simulate_XS_write_error; ///< simulate a checksum error on write
   bool Simulate_FMD_open_error; //! simulate a fmd mismatch error on open
 
-  XrdOucString eoscpTransferLog; //! eoscp.log full path
-
   static XrdSysMutex ShutdownMutex; ///< protecting Shutdown variable
   static bool Shutdown; ///< indicating if a shutdown procedure is running
 
