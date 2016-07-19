cern-eos/eos

commit 7accd4205f49d13cbf82d3ac2fb3bf44f9bddc94

BUILD/RPM: move xfsprog and rt to the clients build too

cmake/EosFindLibs.cmake
@@ -46,6 +46,8 @@ if (NOT PACKAGEONLY)
   find_package(ZMQ REQUIRED)
   find_package(krb5 REQUIRED)
   find_package(Sphinx)
+  find_package(xfs REQUIRED)
+  find_package(rt REQUIRED)
 
   if (Linux)
     find_package(glibc REQUIRED)
@@ -55,9 +57,7 @@ if (NOT PACKAGEONLY)
   if (NOT CLIENT)
     # find_package(RAMCloud REQUIRED)
     find_package(SparseHash REQUIRED)
-    find_package(rt REQUIRED)
     find_package(ldap REQUIRED)
-    find_package(xfs REQUIRED)
     find_package(jsoncpp REQUIRED)
     find_package(CPPUnit)
     find_package(microhttpd)

eos.spec.in
@@ -63,10 +63,10 @@ BuildRequires: fuse-devel, fuse >= 2.5
 BuildRequires: redhat-rpm-config
 BuildRequires: protobuf, protobuf-devel, protobuf-compiler
 BuildRequires: leveldb-devel
-BuildRequires: libattr-devel
+BuildRequires: libattr-devel, xfsprogs-devel
 %if ! %{clientsonly}
 BuildRequires: openldap-devel
-BuildRequires: e2fsprogs-devel, xfsprogs-devel
+BuildRequires: e2fsprogs-devel
 BuildRequires: libmicrohttpd, libmicrohttpd-devel >= 0.9.38
 BuildRequires: jsoncpp, jsoncpp-devel
 %endif

commit b73c2267b15d3749e7941b87b34ff9992798f780

CMAKE: fix MAC build broken by 7accd4205f49d13cbf82d3ac2fb3bf44f9bddc94

cmake/EosFindLibs.cmake
@@ -29,7 +29,13 @@ option(CLIENT "Build only client packages" OFF)
 option(ENABLE_REDOX "Enable Redox support" OFF)
 
 if (NOT PACKAGEONLY)
+if( MacOSX )
+else ( MacOSX )
   find_package(attr REQUIRED)
+  find_package(xfs REQUIRED)
+  find_package(rt REQUIRED)
+endif ( MacOSX )
+
   find_package(PythonSitePkg REQUIRED)
   find_package(XRootD REQUIRED)
   find_package(fuse REQUIRED)
@@ -46,8 +52,6 @@ if (NOT PACKAGEONLY)
   find_package(ZMQ REQUIRED)
   find_package(krb5 REQUIRED)
   find_package(Sphinx)
-  find_package(xfs REQUIRED)
-  find_package(rt REQUIRED)
 
   if (Linux)
     find_package(glibc REQUIRED)