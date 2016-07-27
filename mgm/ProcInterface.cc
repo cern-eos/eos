c7d17051fb626a965d23f8ff1f12a90b86e88796
Andreas Peters <Andreas.Joachim.Peters@cern.ch>
2016-07-27 11:01:38 +0200

    MGM: remove unused variable

--- mgm/ProcInterface.cc
+++ mgm/ProcInterface.cc
@@ -879,8 +879,6 @@ ProcCommand::MakeResult ()
 	    continue;
 
 
-	  size_t fpos; 
-
 	  XrdOucString sline = line.c_str();
 	  while (sline.replace("<n>","n")){}
 	  while (sline.replace("?configstatus@rw","_rw")) {}
