20c24c3c672804135551c693a68435883c9f68c1
Andreas Peters <Andreas.Joachim.Peters@cern.ch>
2016-11-02 13:58:18 +0100

    UTILS: update external dev package scripts

--- utils/sl6-packages.sh
+++ utils/sl6-packages.sh
@@ -17,5 +17,9 @@ yum install -y xfsprogs xfsprogs-devel
 yum install -y e2fsprogs-devel
 yum install -y perl-Time-HiRes
 yum install -y jsoncpp jsoncpp-devel
+yum install -y hiredis hiredis-devel
+yum install -y libevent libevent-devel
+yum install -y git
+
 ( cd /tmp/; git clone  https://github.com/zeromq/cppzmq; cp cppzmq/zmq.hpp /usr/include/ )
 
