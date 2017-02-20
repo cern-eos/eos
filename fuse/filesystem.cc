4fafa12bfda267e082586fb23c1a844813dee02e
Andreas Peters <Andreas.Joachim.Peters@cern.ch>
2017-02-20 22:48:28 +0100

    FUSE: set the link count always to 1 for files or symbolic links to make things like gzip work

--- fuse/filesystem.cc
+++ fuse/filesystem.cc
@@ -1752,7 +1752,10 @@ filesystem::stat (const char* path,
    buf->st_dev = (dev_t) sval[0];
    buf->st_ino = (ino_t) sval[1];
    buf->st_mode = (mode_t) sval[2];
-   buf->st_nlink = (nlink_t) sval[3];
+   if (S_ISREG(buf->st_mode) || S_ISLNK(buf->st_mode))
+     buf->st_nlink = 1;
+   else
+     buf->st_nlink = (nlink_t) sval[3];
    buf->st_uid = (uid_t) sval[4];
    buf->st_gid = (gid_t) sval[5];
    buf->st_rdev = (dev_t) sval[6];
@@ -2594,6 +2597,9 @@ filesystem::inodirlist (unsigned long long dirinode,
 
 	  if (S_ISREG (buf.st_mode) && fuse_exec)
             buf.st_mode |= (S_IXUSR | S_IXGRP | S_IXOTH);
+
+	  if (S_ISREG(buf.st_mode) || S_ISLNK(buf.st_mode))
+	    buf.st_nlink = 1;
 
           buf.st_mode &= (~S_ISVTX); // clear the vxt bit
           buf.st_mode &= (~S_ISUID); // clear suid
