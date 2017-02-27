3a6777db26a671a697cfcfdf02422ef4c86d476f
jmakai <jozsef.makai.1992@gmail.com>
2017-02-27 12:05:00 +0100

    Enabling Gitlab CI with build and Docker image build

---
+++ gitlab-ci/export_branch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Gitlab CI enabled branches should be listed here
+
+if [[ "${CI_BUILD_REF_NAME}" == "beryl_aquamarine" ]]; then
+  REFNAME="aquamarine"
+elif [[ "${CI_BUILD_REF_NAME}"  == "beryl_emerald" ]]; then
+  REFNAME="emerald"
+elif [[ "${CI_BUILD_REF_NAME}"  == "master" ]]; then
+  REFNAME="citrine"
+fi
+
+export BRANCH=$REFNAME
