03226148378e55ae938a7f6a7197ad2d12910764
jmakai <jozsef.makai.1992@gmail.com>
2017-03-07 13:28:50 +0100

    CI: fixing OSX artifacts path

---
+++ gitlab-ci/setup_ccache_slc6.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export CCACHE_DIR="`pwd`/ccache"
+export CCACHE_BASEDIR="`rpm --eval %_builddir`/`ls build/SRPMS/*.src.rpm | awk -F '.' 'BEGIN{OFS=".";} {NF-=3;}1' | awk -F '/' '{print $NF}'`"
+export CCACHE_SLOPPINESS=pch_defines
+
+ccache -s
