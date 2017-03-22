2c57baa3eb62dee3460e5bf20f56a98d51c80862
jmakai <jozsef.makai.1992@gmail.com>
2017-03-22 09:22:44 +0100

    CI: using own runners and ccache setup script

--- gitlab-ci/setup_ccache.sh
+++ gitlab-ci/setup_ccache.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-ccache --max-size=1.5G
-
 export CCACHE_DIR="`pwd`/ccache"
-export CCACHE_BASEDIR="`rpm --eval %_builddir`/`ls SRPMS/*.src.rpm | awk -F '.' 'BEGIN{OFS=".";} {NF-=4;}1' | awk -F '/' '{print $NF}'`"
+export CCACHE_BASEDIR="`rpm --eval %_builddir`/`ls build/SRPMS/*.src.rpm | awk -F '.' 'BEGIN{OFS=".";} {NF-=4;}1' | awk -F '/' '{print $NF}'`"
 export CCACHE_SLOPPINESS=pch_defines
+export CCACHE_MAXSIZE=1.5G
 
 ccache -s
 ccache -p
