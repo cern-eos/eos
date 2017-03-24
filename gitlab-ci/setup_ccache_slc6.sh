e8370a5feb9d72cd0ef3fd7ac8892e387f709000
jmakai <jozsef.makai.1992@gmail.com>
2017-03-24 10:59:08 +0100

    CI: fixing build

--- gitlab-ci/setup_ccache_slc6.sh
+++ gitlab-ci/setup_ccache_slc6.sh
@@ -3,7 +3,5 @@
 export CCACHE_DIR="`pwd`/ccache"
 export CCACHE_BASEDIR="`rpm --eval %_builddir`/`ls build/SRPMS/*.src.rpm | awk -F '.' 'BEGIN{OFS=".";} {NF-=3;}1' | awk -F '/' '{print $NF}'`"
 export CCACHE_SLOPPINESS=pch_defines
-export CCACHE_MAXSIZE=1.5G
 
 ccache -s
-ccache -p
