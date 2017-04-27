63f785ea60ae50897e7f26d3630faec33d55c7f1
Jozsef Makai <jmakai@cern.ch>
2017-04-27 10:37:35 +0200

    CI: fixing the xroot preparation for OSX

--- utils/eos-osx-package-prepare.sh
+++ utils/eos-osx-package-prepare.sh
@@ -7,11 +7,11 @@ fi
 
 XROOTD_VERSION=$1
 mkdir xrootd_src
+git clone https://github.com/xrootd/xrootd.git xrootd_src
 cd xrootd_src
-git clone https://github.com/xrootd/xrootd.git
 git checkout $XROOTD_VERSION
 mkdir build; mkdir build_install
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=../build_install/
+cmake .. -DCMAKE_INSTALL_PREFIX=../build_install/ -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl/
 make
 make install
