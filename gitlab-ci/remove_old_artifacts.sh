d08f6b2c5690efb67c06643c5b5ba1fd11a300b1
Jozsef Makai <jmakai@cern.ch>
2017-05-04 08:56:10 +0200

    CI: removing old artifacts

---
+++ gitlab-ci/remove_old_artifacts.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+AGE_DAYS=10
+EOS_DIRS=/eos/project/s/storage-ci/www/eos/*/commit/
+
+for commit_dir in ${EOS_DIRS}; do
+  find ${commit_dir} -name *.rpm -type f -mtime +${AGE_DAYS} -exec rm {} \; || true
+  # Recreate YUM repos
+  yum_repos=$(find ${commit_dir} -path "*/i386" -o -path "*/x86_64" -type d)                                                                                                                                                           
+                                                                                                                                                                                                                                       
+  for repo_dir in ${yum_repos[@]}; 
+  do                                                                                                                                                                                                      
+    echo "Update YUM repo: ${repo_dir}"
+    createrepo --update -q ${repo_dir}
+  done
+done                                                                                                                                                         
+
+
