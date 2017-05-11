9695d481cace5f40b9292319be64287b4780a93b
Jozsef Makai <jmakai@cern.ch>
2017-05-11 16:43:11 +0200

    CI: also removing old stuff from SRPMS repo [ci skip]

--- gitlab-ci/remove_old_artifacts.sh
+++ gitlab-ci/remove_old_artifacts.sh
@@ -6,7 +6,7 @@ EOS_DIRS=/eos/project/s/storage-ci/www/eos/*/commit/
 for commit_dir in ${EOS_DIRS}; do
   find ${commit_dir} -regex '.*\(rpm\|dmg\)$' -type f -mtime +${AGE_DAYS} -exec rm {} \; || true
   # Recreate YUM repos
-  yum_repos=$(find ${commit_dir} -path "*/i386" -o -path "*/x86_64" -type d)                                                                                                                                                           
+  yum_repos=$(find ${commit_dir} -path "*/SRPMS" -o -path "*/x86_64" -type d)                                                                                                                                                           
                                                                                                                                                                                                                                        
   for repo_dir in ${yum_repos[@]}; 
   do                                                                                                                                                                                                      
