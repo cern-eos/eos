fbfeaef2cc830c8fa043470876065c02cc43c4af
Jozsef Makai <jmakai@cern.ch>
2017-06-27 10:41:49 +0200

    MGM: fixing null object in proc_fs_dumpmd

--- mgm/proc/proc_fs.cc
+++ mgm/proc/proc_fs.cc
@@ -156,6 +156,9 @@ proc_fs_dumpmd(std::string& fsidst, XrdOucString& option, XrdOucString& dp,
 
     /* empty list in case of unavailable file lists */
     eos::IFsView::FileList emptyFileList;
+    emptyFileList.set_empty_key(0xffffffffffffffffll);
+    emptyFileList.set_deleted_key(0);
+
     eos::IFsView::FileList& filelist = emptyFileList; // empty by default
     try {
       filelist = gOFS->eosFsView->getFileList(fsid);
@@ -163,11 +166,12 @@ proc_fs_dumpmd(std::string& fsidst, XrdOucString& option, XrdOucString& dp,
       errno = e.getErrno();
       eos_static_err("Couldn't retrieve file list. Error code: %d, message: %s",
                      e.getErrno(), e.getMessage().str().c_str());
+      return e.getErrno();
     }
 
-    for (auto it = filelist.begin(); it != filelist.end(); ++it) {
+    for (auto it : filelist) {
       try {
-        fmd = gOFS->eosFileService->getFileMD(*it);
+        fmd = gOFS->eosFileService->getFileMD(it);
 
         if (fmd) {
           entries++;
@@ -232,7 +236,7 @@ proc_fs_dumpmd(std::string& fsidst, XrdOucString& option, XrdOucString& dp,
       } catch (eos::MDException& e) {
         errno = e.getErrno();
         eos_static_err("Couldn't retrieve meta data for file id: %u. Error code: %d, message: %s",
-                           *it, e.getErrno(), e.getMessage().str().c_str());
+                       it, e.getErrno(), e.getMessage().str().c_str());
       }
     }
 
@@ -245,15 +249,16 @@ proc_fs_dumpmd(std::string& fsidst, XrdOucString& option, XrdOucString& dp,
         errno = e.getErrno();
         eos_static_err("Couldn't retrieve unlinked file list. Error code: %d, message: %s",
                        e.getErrno(), e.getMessage().str().c_str());
+        return e.getErrno();
       }
 
-      for (auto it = unlinked.begin(); it != unlinked.end(); ++it) {
+      for (auto it : unlinked) {
         try {
-          fmd = gOFS->eosFileService->getFileMD(*it);
+          fmd = gOFS->eosFileService->getFileMD(it);
         } catch (eos::MDException& e) {
           errno = e.getErrno();
           eos_static_err("Couldn't retrieve meta data for file id: %u. Error code: %d, message: %s",
-                             *it, e.getErrno(), e.getMessage().str().c_str());
+                         it, e.getErrno(), e.getMessage().str().c_str());
         }
 
         if (fmd) {
