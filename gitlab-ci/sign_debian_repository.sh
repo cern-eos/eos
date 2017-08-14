61e7a5dd90d9fb2936dd7c7a36b8d9821ce5db76
Jozsef Makai <jmakai@cern.ch>
2017-08-14 16:05:18 +0200

    CI: allowing debian store job to fail

--- gitlab-ci/sign_debian_repository.sh
+++ gitlab-ci/sign_debian_repository.sh
@@ -12,8 +12,7 @@ dist=$2
 
 components=$(find $prefix/dists/$dist/ -mindepth 1 -maxdepth 1 -type d -exec basename {} \; | tr '\n' ' ')
 
+rm $prefix/dists/$dist/Release $prefix/dists/$dist/InRelease $prefix/dists/$dist/Release.gpg
 apt-ftparchive -o APT::FTPArchive::Release::Origin=CERN -o APT::FTPArchive::Release::Label=EOS -o APT::FTPArchive::Release::Codename=artful -o APT::FTPArchive::Release::Architectures=amd64 -o APT::FTPArchive::Release::Components="$components" release $prefix/dists/$dist/ > $prefix/dists/$dist/Release
-rm $prefix/dists/$dist/InRelease
-gpg --homedir /home/stci/ --batch --yes --clearsign -o $prefix/dists/$dist/InRelease $prefix/dists/$dist/Release
-rm $prefix/dists/$dist/Release.gpg
-gpg --homedir /home/stci/ --batch --yes -abs -o $prefix/dists/$dist/Release.gpg $prefix/dists/$dist/Release
+gpg --homedir /home/stci/ --clearsign -o $prefix/dists/$dist/InRelease $prefix/dists/$dist/Release
+gpg --homedir /home/stci/ -abs -o $prefix/dists/$dist/Release.gpg $prefix/dists/$dist/Release
