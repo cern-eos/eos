9d6e91dd8dff6a7670acd0280d0709541644bc4e
Andreas Joachim Peters <andreas.joachim.peters@cern.ch>
2017-09-26 11:49:47 +0200

    MGM: add missing LockTracker files

---
+++ mgm/fuse-locks/LockTracker.cc
@@ -0,0 +1,450 @@
+//------------------------------------------------------------------------------
+//! @file LockTracker.cc
+//! @author Gerogios Bitzes
+//! @brief POSIX lock class
+//------------------------------------------------------------------------------
+
+/************************************************************************
+ * EOS - the CERN Disk Storage System                                   *
+ * Copyright (C) 2017 CERN/Switzerland                                  *
+ *                                                                      *
+ * This program is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ ************************************************************************/
+
+
+/*----------------------------------------------------------------------------*/
+#include "LockTracker.hh"
+#include "XrdSys/XrdSysTimer.hh"
+#include <fcntl.h>
+#include <unistd.h>
+
+/*----------------------------------------------------------------------------*/
+
+EOSMGMNAMESPACE_BEGIN
+USE_EOSMGMNAMESPACE
+
+/*----------------------------------------------------------------------------*/
+std::ostream& operator<< (std::ostream &os, const ByteRange &range)
+{
+  os << "[" << range.start() << ", " << range.end() << ")";
+  return os;
+}
+
+/*----------------------------------------------------------------------------*/
+std::ostream& operator<< (std::ostream &os, const Lock &lock)
+{
+  os << lock.range() << " on pid " << lock.pid() << std::endl;
+  return os;
+}
+
+/*----------------------------------------------------------------------------*/
+void
+/*----------------------------------------------------------------------------*/
+LockSet::add(const Lock &l)
+/*----------------------------------------------------------------------------*/
+{
+  Lock newlock(l);
+
+  // Absorb any overlapping ranges, removing the old ones
+  auto it = locks.begin();
+  while (it != locks.end())
+  {
+    if (newlock.absorb(*it))
+    {
+      it = locks.erase(it);
+    }
+    else
+    {
+      it++;
+    }
+  }
+
+  // Append the consolidated superlock
+  locks.push_back(newlock);
+}
+
+/*----------------------------------------------------------------------------*/
+bool
+/*----------------------------------------------------------------------------*/
+LockSet::conflict(const Lock &l) const
+/*----------------------------------------------------------------------------*/
+{
+  auto it = locks.begin();
+  while (it != locks.end())
+  {
+    if (it->pid() != l.pid() && l.range().overlap(it->range()))
+      return true;
+    it++;
+  }
+  return false;
+}
+
+/*----------------------------------------------------------------------------*/
+bool
+/*----------------------------------------------------------------------------*/
+LockSet::getconflict(Lock &l)
+/*----------------------------------------------------------------------------*/
+{
+  auto it = locks.begin();
+  while (it != locks.end())
+  {
+    if (it->pid() != l.pid() && l.range().overlap(it->range()))
+    {
+      l = *it;
+      return true;
+    }
+    it++;
+  }
+  return false;
+}
+
+/*----------------------------------------------------------------------------*/
+bool
+/*----------------------------------------------------------------------------*/
+LockSet::overlap(const Lock &l) const
+/*----------------------------------------------------------------------------*/
+{
+  auto it = locks.begin();
+  while (it != locks.end())
+  {
+    if (l.overlap(*it)) return true;
+    it++;
+  }
+  return false;
+}
+
+/*----------------------------------------------------------------------------*/
+bool
+/*----------------------------------------------------------------------------*/
+LockSet::overlap(const ByteRange &br) const
+/*----------------------------------------------------------------------------*/
+{
+  auto it = locks.begin();
+
+  while (it != locks.end())
+  {
+    if (br.overlap(it->range())) return true;
+    it++;
+  }
+  return false;
+}
+
+/*----------------------------------------------------------------------------*/
+void
+/*----------------------------------------------------------------------------*/
+LockSet::remove(const Lock &l)
+/*----------------------------------------------------------------------------*/
+{
+  std::vector<Lock> queued;
+
+  auto it = locks.begin();
+  while (it != locks.end())
+  {
+    std::vector<Lock> newlocks = it->minus(l);
+
+    if (newlocks.size() == 0)
+    {
+      it = locks.erase(it);
+      continue;
+    }
+
+    if (newlocks.size() >= 1)
+    {
+      *it = newlocks[0];
+    }
+
+    if (newlocks.size() == 2)
+    {
+      queued.push_back(newlocks[1]);
+    }
+
+    it++;
+  }
+
+  // Cannot add new items to vector while iterating, as it invalides the iterators.
+  // Store the items in a queue and add them later.
+  for (size_t i = 0; i < queued.size(); i++)
+  {
+    locks.push_back(queued[i]);
+  }
+}
+
+/*----------------------------------------------------------------------------*/
+size_t
+/*----------------------------------------------------------------------------*/
+LockSet::nlocks()
+/*----------------------------------------------------------------------------*/
+{
+  return locks.size();
+}
+
+/*----------------------------------------------------------------------------*/
+size_t
+/*----------------------------------------------------------------------------*/
+LockSet::nlocks(pid_t pid)
+/*----------------------------------------------------------------------------*/
+{
+  size_t res = 0;
+  auto it = locks.begin();
+  while (it != locks.end())
+  {
+    if (it->pid() == pid) res++;
+    it++;
+  }
+  return res;
+}
+
+/*----------------------------------------------------------------------------*/
+void
+/*----------------------------------------------------------------------------*/
+LockSet::remove(pid_t pid)
+{
+  std::vector<Lock> survivinglocks;
+
+  for (auto it = locks.begin(); it != locks.end(); ++it)
+  {
+    if (it->pid() != pid)
+      survivinglocks.push_back(*it);
+  }
+
+  locks = survivinglocks;
+}
+
+/*----------------------------------------------------------------------------*/
+void
+/*----------------------------------------------------------------------------*/
+LockSet::remove(const std::string& owner)
+{
+  std::vector<Lock> survivinglocks;
+
+  for (auto it = locks.begin(); it != locks.end(); ++it)
+  {
+    if (it->owner() != owner)
+      survivinglocks.push_back(*it);
+  }
+
+  locks = survivinglocks;
+}
+
+/*----------------------------------------------------------------------------*/
+std::set<pid_t>
+/*----------------------------------------------------------------------------*/
+LockSet::lslocks(const std::string& owner)
+/*----------------------------------------------------------------------------*/
+{
+  // return all pids belonging to owner
+  std::set<pid_t> owner_pids;
+
+  for (auto it = locks.begin(); it != locks.end(); ++it)
+  {
+    fprintf(stderr,"lock: owner=%s (%s) pid=%u true=%d\n", it->owner().c_str(), owner.c_str(), it->pid(),
+            (it->owner() == owner));
+    if (it->owner() == owner)
+      owner_pids.insert(it->pid());
+  }
+  return owner_pids;
+}
+
+/*----------------------------------------------------------------------------*/
+int
+/*----------------------------------------------------------------------------*/
+LockTracker::getlk(pid_t pid, struct flock* lock)
+/*----------------------------------------------------------------------------*/
+{
+  std::lock_guard<std::mutex> guard(mtx);
+
+  if (lock->l_type == F_UNLCK)
+  {
+    // TODO signal warning, should not happen (?)
+    return 1;
+  }
+
+  if (canLock(pid, lock))
+  {
+    lock->l_type = F_UNLCK;
+  }
+  else
+  {
+    // canLock filled the blocking lock
+  }
+  return 1;
+}
+
+/*----------------------------------------------------------------------------*/
+int
+/*----------------------------------------------------------------------------*/
+LockTracker::setlk(pid_t pid, struct flock* lock, int sleep, const std::string& owner)
+/*----------------------------------------------------------------------------*/
+{
+  if (!sleep) return addLock(pid, lock, owner);
+
+  size_t cnt=0;
+  while (!addLock(pid, lock, owner))
+  {
+    cnt++;
+    // TODO wait on condition variable?
+    XrdSysTimer sleeper;
+    sleeper.Wait(1);
+
+    if (cnt > 10)
+    {
+      // we give up after 10ms 
+      return 0;
+    }
+  }
+  return 1;
+}
+
+/*----------------------------------------------------------------------------*/
+bool
+/*----------------------------------------------------------------------------*/
+LockTracker::canLock(pid_t pid, struct flock* f_lock)
+/*----------------------------------------------------------------------------*/
+{
+  Lock lock(ByteRange(f_lock->l_start, f_lock->l_len), pid);
+
+  // Can always unlock
+  if (f_lock->l_type == F_UNLCK) return true;
+
+  // Are there any exclusive locks right now?
+  if (wlocks.getconflict(lock)) return false;
+
+  // If this is a read lock, we can lock
+  if (f_lock->l_type == F_RDLCK)
+  {
+    return true;
+  }
+
+  // If this is a write lock, we can lock only if there are no read locks
+  if (f_lock->l_type == F_WRLCK)
+  {
+    return rlocks.getconflict(lock);
+  }
+
+  // TODO raise warning, should never reach this point
+  return false;
+}
+
+/*----------------------------------------------------------------------------*/
+bool
+/*----------------------------------------------------------------------------*/
+LockTracker::addLock(pid_t pid, struct flock* f_lock, const std::string& owner)
+/*----------------------------------------------------------------------------*/
+{
+  std::lock_guard<std::mutex> guard(mtx);
+
+  Lock lock(ByteRange(f_lock->l_start, f_lock->l_len), pid, owner);
+
+  // Unlock?
+  if (f_lock->l_type == F_UNLCK)
+  {
+    rlocks.remove(lock);
+    wlocks.remove(lock);
+    return true;
+  }
+
+  // Exclusive lock?
+  if (f_lock->l_type == F_WRLCK)
+  {
+    // Conflict with any read locks?
+    if (rlocks.conflict(lock)) return false;
+
+    // Conflict with any write locks?
+    if (wlocks.conflict(lock)) return false;
+
+    // Add write lock
+    wlocks.add(lock);
+
+    // It could be that the process is converting a read lock into a write.
+    // Remove any read locks on the same region.
+    rlocks.remove(lock);
+
+    return true;
+  }
+
+  // Read lock?
+  if (f_lock->l_type == F_RDLCK)
+  {
+    // Conflict with any write locks?
+    if (wlocks.conflict(lock)) return false;
+
+    // Add read lock
+    rlocks.add(lock);
+
+    // It could be that the process is converting a write lock into a read.
+    // Remove any write locks on the same region.
+    wlocks.remove(lock);
+
+    return true;
+  }
+
+  // TODO raise warning, should never reach this point
+  std::cerr << "WARNING, something is wrong" << std::endl;
+  return false;
+}
+
+/*----------------------------------------------------------------------------*/
+int
+/*----------------------------------------------------------------------------*/
+LockTracker::removelk(pid_t pid)
+{
+  std::lock_guard<std::mutex> guard(mtx);
+  rlocks.remove(pid);
+  wlocks.remove(pid);
+
+  return 1;
+}
+
+/*----------------------------------------------------------------------------*/
+int
+/*----------------------------------------------------------------------------*/
+LockTracker::removelk(const std::string& owner)
+{
+  std::lock_guard<std::mutex> guard(mtx);
+  rlocks.remove(owner);
+  wlocks.remove(owner);
+
+  return 1;
+}
+
+/*----------------------------------------------------------------------------*/
+bool
+/*----------------------------------------------------------------------------*/
+LockTracker::inuse()
+/*----------------------------------------------------------------------------*/
+{
+  std::lock_guard<std::mutex> guard(mtx);
+  return (rlocks.nlocks() + wlocks.nlocks()) ? true : false;
+}
+
+/*----------------------------------------------------------------------------*/
+std::set<pid_t>
+/*----------------------------------------------------------------------------*/
+LockTracker::getrlks(const std::string& owner)
+/*----------------------------------------------------------------------------*/
+{
+  std::lock_guard<std::mutex> guard(mtx);
+  return rlocks.lslocks(owner);
+}
+
+/*----------------------------------------------------------------------------*/
+std::set<pid_t>
+/*----------------------------------------------------------------------------*/
+LockTracker::getwlks(const std::string& owner)
+/*----------------------------------------------------------------------------*/
+{
+  std::lock_guard<std::mutex> guard(mtx);
+  return wlocks.lslocks(owner);
+}
+EOSMGMNAMESPACE_END
---
+++ mgm/fuse-locks/LockTracker.hh
@@ -0,0 +1,333 @@
+//------------------------------------------------------------------------------
+//! @file LockTracker.hh
+//! @author Gerogios Bitzes
+//! @brief POSIX lock class
+//------------------------------------------------------------------------------
+
+/************************************************************************
+ * EOS - the CERN Disk Storage System                                   *
+ * Copyright (C) 2017 CERN/Switzerland                                  *
+ *                                                                      *
+ * This program is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ ************************************************************************/
+
+
+#ifndef __EOS_MGM_LOCKTRACKER_HH__
+#define __EOS_MGM_LOCKTRACKER_HH__
+
+#include <cstdlib>
+#include <list>
+#include <vector>
+#include <set>
+#include <limits>
+#include <iostream>
+#include <mutex>
+#include <fcntl.h>
+#include "mgm/Namespace.hh"
+
+/*----------------------------------------------------------------------------*/
+
+EOSMGMNAMESPACE_BEGIN
+
+
+/*----------------------------------------------------------------------------*/
+#define DBG(message) std::cerr << __FILE__ << ":" << __LINE__ << " -- " \
+        <<#message << " = " << message << std::endl
+
+template<typename T>
+bool isPointBetween(const T& start, const T& target, const T& end)
+{
+  return target >= start && target < end;
+}
+
+template<typename T>
+bool isPointBetweenOrTouching(const T& start, const T& target, const T& end)
+{
+  return target >= start && target <= end;
+}
+/*----------------------------------------------------------------------------*/
+typedef off_t Offset;
+/*----------------------------------------------------------------------------*/
+class ByteRange;
+class Lock;
+std::ostream& operator<< (std::ostream &os, const ByteRange &range);
+std::ostream& operator<< (std::ostream &os, const Lock &lock);
+
+/*----------------------------------------------------------------------------*/
+class ByteRange
+/*----------------------------------------------------------------------------*/
+{
+public:
+
+  ByteRange(Offset start, Offset len) : start_(start), len_(len)
+  {
+    if (!overlap(*this))
+    {
+      std::cerr << "ByteRange assertion failed: range does not overlap with itself! start: "
+              << start << ", len: " << len << std::endl;
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  bool operator==(const ByteRange& rhs) const
+  {
+    return this->start() == rhs.start() && this->end() == rhs.end();
+  }
+
+  Offset start() const
+  {
+    return start_;
+  }
+
+  Offset len() const
+  {
+    return len_;
+  }
+
+  Offset end() const
+  {
+    if (len_ == -1) return std::numeric_limits<Offset>::max();
+    return start_ + len_;
+  }
+
+  // Absorb the other range if possible, expand myself
+  // to contain both ranges
+
+  bool absorb(const ByteRange &other)
+  {
+    if (!overlapOrTouch(other)) return false;
+
+    Offset myend = end();
+    start_ = std::min(start_, other.start_);
+    updateEnd(std::max(myend, other.end()));
+    return true;
+  }
+
+  bool contains(const ByteRange& other) const
+  {
+    return start()     <= other.start() &&
+            other.end() <= end();
+  }
+
+  // Return what happens when removing the range "other" from "this".
+  // Might return 0, 1 or 2 resulting ranges.
+
+  std::vector<ByteRange> minus(const ByteRange &other) const
+  {
+    // Case 1: "other" fully to the left, no overlap
+    if (other.end() <= this->start()) return { *this};
+
+    // Case 2: "other" fully to the right, no overlap
+    if (this->end() <= other.start()) return { *this};
+
+    // Case 3: "other" eats the entire thing, no output
+    if (other.contains(*this)) return {};
+
+    // Case 4: "other" eats the start, but not the end
+    if (isPointBetween(other.start(),
+                       this->start(), other.end()) && other.end() < this->end())
+    {
+      return { ByteRange(other.end(), this->end() - other.end())};
+    }
+
+    // Case 5: "other" eats the end, but not the start
+    if (isPointBetween(other.start(), this->end() - 1,
+                       other.end()) && this->start() < other.start())
+    {
+      return { ByteRange(this->start(), other.start() - this->start())};
+    }
+
+    // Case 6: "other" eats the middle
+    return { ByteRange(this->start(), other.start() - this->start()),
+      ByteRange(other.end(), this->end() - other.end())};
+  }
+
+  // checks whether the two ranges overlap, or at least touch
+
+  bool overlapOrTouch(const ByteRange& other) const
+  {
+    // case 1: Is other.start between this->start and this->end ?
+    if (isPointBetweenOrTouching(this->start(),
+                                 other.start(), this->end())) return true;
+
+    // case 2: Is this->start between other.start and other.end ?
+    if (isPointBetweenOrTouching(other.start(),
+                                 this->start(), other.end())) return true;
+
+    // case 3: ranges don't overlap or touch
+    return false;
+
+  }
+
+  bool overlap(const ByteRange& other) const
+  {
+    // case 1: 0 ranges at the same offset overlap
+    if ( (this->start() == this->end()) &&
+        (other.start() == other.end()) &&
+        (this->start() == other.start()) )
+      return true;
+
+    // case 2: Is other.start between this->start and this->end ?
+    if (isPointBetween(this->start(), other.start(), this->end())) return true;
+
+    // case 3: Is this->start between other.start and other.end ?
+    if (isPointBetween(other.start(), this->start(), other.end())) return true;
+
+    // case 4: ranges don't overlap
+    return false;
+  }
+
+private:
+
+  void updateEnd(Offset newend)
+  {
+    if (newend <= start())
+    {
+      std::cerr << "ByteRange assertion failed: tried to update end to "
+              << newend << ", while start = " << start() << std::endl;
+      exit(EXIT_FAILURE);
+    }
+
+    if (newend == std::numeric_limits<Offset>::max())
+    {
+      len_ = -1;
+    }
+    else
+    {
+      len_ = newend - start_;
+    }
+  }
+
+  Offset start_;
+  Offset len_;
+} ;
+
+/*----------------------------------------------------------------------------*/
+class Lock
+/*----------------------------------------------------------------------------*/
+{
+public:
+
+  Lock(const ByteRange &range, pid_t pid, const std::string& owner="") : range_(range), pid_(pid), owner_(owner)
+  {
+  }
+
+  pid_t pid() const
+  {
+    return pid_;
+  }
+
+  std::string owner() const
+  {
+    return owner_;
+  }
+
+  const ByteRange& range() const
+  {
+    return range_;
+  }
+
+  bool overlap(const Lock &other) const
+  {
+    if (pid() != other.pid()) return false;
+    return range_.overlap(other.range_);
+  }
+
+  bool contains(const Lock &other) const
+  {
+    if (pid() != other.pid()) return false;
+    return range_.contains(other.range_);
+  }
+
+  bool absorb(const Lock &other)
+  {
+    if (pid() != other.pid()) return false;
+    return range_.absorb(other.range_);
+  }
+
+  std::vector<Lock> minus(const Lock &other)
+  {
+    if (pid() != other.pid()) return { *this};
+
+    std::vector<ByteRange> ranges = range_.minus(other.range_);
+    std::vector<Lock> locks;
+
+    for (size_t i = 0; i < ranges.size(); i++)
+    {
+      locks.emplace_back(ranges[i], pid());
+    }
+    return locks;
+  }
+
+  bool operator==(const Lock &rhs) const
+  {
+    return pid() == rhs.pid() && range() == rhs.range();
+  }
+
+private:
+  ByteRange range_;
+  pid_t pid_;
+  std::string owner_;
+} ;
+
+class LockSet
+{
+public:
+  void add(const Lock &l); // adds lock, merging appropriately any overlaps
+  bool overlap(const Lock &l) const; // check if overlaps with locks from the *same* process
+  bool overlap(const ByteRange &r) const; // check if overlaps with locks from *any* process
+  void remove(const Lock &l); // remove any contained locks, shrink any overlapping
+  void remove(const pid_t pid); // remove all locks for a given pid
+  void remove(const std::string& owner); // remove all locks for a given owner
+
+  // check if there's a conflict between this lock and any other in the set.
+  // If two locks overlap, but have the same PID, this is not a conflict!
+  bool conflict(const Lock &l) const;
+
+  bool getconflict(Lock &l);
+
+  size_t nlocks(); // how many locks there are in total (after coalescing)
+  size_t nlocks(pid_t pid); // how many locks are held by a specific pid (after coalescing)
+
+  std::set<pid_t> lslocks(const std::string& owner); // return all pids belonging to owner
+
+private:
+  std::vector<Lock> locks;
+} ;
+
+class LockTracker
+{
+public:
+  int getlk(pid_t pid, struct flock* lock);
+  int setlk(pid_t pid, struct flock* lock, int sleep, const std::string& owner);
+
+  std::set<pid_t> getrlks(const std::string& owner);
+  std::set<pid_t> getwlks(const std::string& owner);
+
+  int removelk(pid_t pid);
+  int removelk(const std::string& owner);
+  bool inuse();
+
+private:
+  std::mutex mtx;
+  bool addLock(pid_t pid, struct flock* lock, const std::string& owner);
+  bool canLock(pid_t pid, struct flock* lock);
+
+  LockSet rlocks;
+  LockSet wlocks;
+} ;
+
+EOSMGMNAMESPACE_END
+
+#endif
