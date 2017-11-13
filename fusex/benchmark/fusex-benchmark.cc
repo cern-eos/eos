46c700770df8019f86fef2027a25fd885280263f
Andreas Joachim Peters <andreas.joachim.peters@cern.ch>
2017-11-13 11:07:36 +0100

    FUSEX: fix coverity warning about unchecked return value

--- fusex/benchmark/fusex-benchmark.cc
+++ fusex/benchmark/fusex-benchmark.cc
@@ -317,6 +317,13 @@ int main(int argc, char* argv[])
     unlink(name);
 
     int fd = open(name, O_CREAT | O_RDWR, S_IRWXU);
+
+    if (fd < 0)
+    {
+      fprintf(stderr, "[test=%03d] creat failed\n", testno);
+      exit(testno);
+    }
+
     for (int i = 0; i < LOOP_10; i+=2)
     {
       ssize_t nwrite = pwrite(fd, &i, 4, (i * 4) + (2 * 1024 * 1024));
