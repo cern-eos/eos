a1bbe0e86012e2e1b1844f6cfb378cf14bc911df
Andreas Joachim Peters <andreas.joachim.peters@cern.ch>
2017-11-29 12:50:44 +0100

    MGM: print the configuration files used in Master::Activate

--- mgm/Master.cc
+++ mgm/Master.cc
@@ -1247,6 +1247,7 @@ Master::Activate (XrdOucString &stdOut, XrdOucString &stdErr, int transitiontype
     stdOut += "\n";
   }
 
+  MasterLog(eos_static_notice(stdOut.c_str()));
   gOFS->ConfEngine->SetConfigDir(gOFS->MgmConfigDir.c_str());
 
   if (transitiontype != kSlaveToMaster)
