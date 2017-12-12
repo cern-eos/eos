ec66c3cdfbc21e27527c32518a7a89d34ae46abe
Jozsef Makai <jmakai@cern.ch>
2017-12-12 16:58:53 +0100

    CI: fixing aquamarine tag handling

--- gitlab-ci/export_branch.sh
+++ gitlab-ci/export_branch.sh
@@ -2,7 +2,7 @@
 
 #Gitlab CI enabled branches should be listed here
 
-if [[ "${CI_BUILD_REF_NAME}" == "beryl_aquamarine" ]]; then
+if [[ "${CI_BUILD_REF_NAME}" == "beryl_aquamarine" ]] || [[ ${CI_BUILD_REF_NAME} == 0.3* ]] ; then
   REFNAME="aquamarine"
 else
   REFNAME="citrine"
