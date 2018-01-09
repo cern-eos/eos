2018755c8d1dab784f4e7da29a36a5dc346e8856
Andreas Joachim Peters <andreas.joachim.peters@cern.ch>
2018-01-09 10:32:34 +0100

    FUSEX: fix wrong truncation of local startfile cache in case of a write

--- fusex/data/diskcache.cc
+++ fusex/data/diskcache.cc
@@ -294,7 +294,11 @@ diskcache::truncate(off_t offset)
     offset = sMaxSize;
   }
 
-  rc = ::ftruncate(fd, offset);
+  if (size() != 0)
+  {
+    // don't truncate empty files to a larger size
+    rc = ::ftruncate(fd, offset);
+  }
 
   if (!rc) {
     sDirCleaner->get_external_tree().change(detachstat.st_size - attachstat.st_size,
