d10fbe9a057a9ff6f8de05b15175501cb53074d2
Andreas Joachim Peters <andreas.joachim.peters@cern.ch>
2018-01-09 10:39:45 +0100

    MISC: better match /var/eos/fs/* instead of /var/eos/fs*

--- misc/sbin/eos_start_pre.sh
+++ misc/sbin/eos_start_pre.sh
@@ -63,7 +63,7 @@ if [ "$1" = "eos-start-pre" ]; then
     mkdir -p /var/log/eos /var/eos/config/${EOS_MGM_HOST}
     touch /var/eos/config/${EOS_MGM_HOST}/default.eoscf
     chown -R daemon /var/spool/eos /var/spool/eos/admin /var/log/eos 
-    find /var/eos/ -maxdepth 1 -mindepth 1 -not -path "/var/eos/fs*" -not -path "/var/eos/fusex*" -type d -exec chown -Rv daemon {} \;
+    find /var/eos/ -maxdepth 1 -mindepth 1 -not -path "/var/eos/fs/*" -not -path "/var/eos/fusex*" -type d -exec chown -Rv daemon {} \;
     chmod -R 775 /var/spool/eos
     mkdir -p /var/eos/auth /var/eos/stage
     chown daemon /var/eos/auth /var/eos/stage
