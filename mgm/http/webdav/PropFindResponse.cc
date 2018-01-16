a0bb81e84e57c3fb232825c073e1349163c0b52e
Andreas Joachim Peters <andreas.joachim.peters@cern.ch>
2018-01-16 14:17:16 +0100

    Revert "HTTP: conform to access rules when serving PROPFIND requests (banned users could pass etc.)"
    This reverts commit 1faab45f7424fbebcbd2c51b28d33ad7bde400d9.

--- mgm/http/webdav/PropFindResponse.cc
+++ mgm/http/webdav/PropFindResponse.cc
@@ -226,13 +226,9 @@ PropFindResponse::BuildResponse (eos::common::HttpRequest *request)
 	  SetResponseCode(HttpResponse::OK);
         }
       }
-<<<<<<< HEAD
     }
     else
     {
-=======
-    } else {
->>>>>>> 9639089... HTTP: conform to access rules when serving PROPFIND requests (banned users could pass etc.)
       eos_static_warning("msg=\"error opening directory - might be stalled/banned\"");
       SetResponseCode(ResponseCodes::FORBIDDEN);
       return this;
