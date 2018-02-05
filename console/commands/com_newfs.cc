89c3101653c3e012174b947a7632f4e46f2bc794
Jozsef Makai <jmakai@cern.ch>
2018-02-05 08:20:12 +0100

    CONSOLE: add new fs clone to console

--- console/commands/com_newfs.cc
+++ console/commands/com_newfs.cc
@@ -183,6 +183,36 @@ FsHelper::ParseCommand(const char* arg)
         }
       }
     }
+  } else if (cmd == "clone") {
+    auto* clone = fs->mutable_clone();
+
+    if (!(option = tokenizer.GetToken())) {
+      return false;
+    } else {
+      soption = option;
+
+      try {
+        auto sourceid = std::stoull(soption);
+        clone->set_sourceid(sourceid);
+      } catch (const std::exception& e) {
+        std::cerr << "error: fsid needs to be numeric" << std::endl;
+        return false;
+      }
+    }
+
+    if (!(option = tokenizer.GetToken())) {
+      return false;
+    } else {
+      soption = option;
+
+      try {
+        auto targetid = std::stoull(soption);
+        clone->set_targetid(targetid);
+      } catch (const std::exception& e) {
+        std::cerr << "error: fsid needs to be numeric" << std::endl;
+        return false;
+      }
+    }
   } else if (cmd == "compare") {
     auto* compare = fs->mutable_compare();
 
