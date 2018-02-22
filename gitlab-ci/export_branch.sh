7dcf17296b2f490151ae379be2707e48beff69d7
Jozsef Makai <jozsef.makai@cern.ch>
2018-02-22 15:16:18 +0000

    CI: adding branch script

---
+++ gitlab-ci/export_branch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Gitlab CI enabled branches should be listed here
+
+if [[ ${CI_BUILD_REF_NAME} == beryl_aquamarine* ]] || [[ ${CI_BUILD_REF_NAME} == 0.3* ]] ; then
+  REFNAME="aquamarine"
+else
+  REFNAME="citrine"
+fi
+
+export BRANCH=$REFNAME
