3b26fbf88fce89ed6d2cd62bee79ac08a554fc0b
Jozsef Makai <jozsef.makai@cern.ch>
2018-02-27 08:26:23 +0000

    CI: allow removal failure for release data

--- gitlab-ci/sign_debian_repository.sh
+++ gitlab-ci/sign_debian_repository.sh
@@ -12,7 +12,13 @@ dist=$2
 
 components=$(find $prefix/dists/$dist/ -mindepth 1 -maxdepth 1 -type d -exec basename {} \; | tr '\n' ' ')
 
+# This thing can fail for the first time
+set +e
+
 rm $prefix/dists/$dist/Release $prefix/dists/$dist/InRelease $prefix/dists/$dist/Release.gpg
+
+set -e
+
 apt-ftparchive -o APT::FTPArchive::Release::Origin=CERN -o APT::FTPArchive::Release::Label=EOS -o APT::FTPArchive::Release::Codename=artful -o APT::FTPArchive::Release::Architectures=amd64 -o APT::FTPArchive::Release::Components="$components" release $prefix/dists/$dist/ > $prefix/dists/$dist/Release
 gpg --homedir /home/stci/ --clearsign -o $prefix/dists/$dist/InRelease $prefix/dists/$dist/Release
 gpg --homedir /home/stci/ -abs -o $prefix/dists/$dist/Release.gpg $prefix/dists/$dist/Release
