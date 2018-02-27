4f95581b045accf5c5e2ada2a91859928d9086be
Andreas Peters <Andreas.Joachim.Peters@cern.ch>
2018-02-27 11:36:15 +0100

    MGM/FUSEX: add protocol v3 and add broadcast reception time to MD record
    Conflicts:
    	fusex/fusex.proto
    	mgm/FuseServer.cc
    
    FUSEX: fix merge issue

--- mgm/FuseServer.cc
+++ mgm/FuseServer.cc
@@ -129,7 +129,7 @@ FuseServer::Clients::MonitorHeartBeat()
           it->second.set_state(Client::ONLINE);
         }
 
-        if (it->second.heartbeat().protversion() != it->second.heartbeat().PROTOCOLV2) {
+        if (it->second.heartbeat().protversion() < it->second.heartbeat().PROTOCOLV2) {
           // protocol version mismatch, evict this client
           evictversionmap[it->second.heartbeat().uuid()] = it->first;
           it->second.set_state(Client::EVICTED);
@@ -145,7 +145,7 @@ FuseServer::Clients::MonitorHeartBeat()
 
     // delete client ot be evicted because of a version mismatch
     for (auto it = evictversionmap.begin(); it != evictversionmap.end(); ++it) {
-      std::string versionerror = "Server supports only PROTOCOLV2";
+      std::string versionerror = "Server supports PROTOCOLV3 and requires atleast PROTOCOLV2";
       std::string uuid = it->first;
       Evict(uuid, versionerror);
       mMap.erase(it->second);
