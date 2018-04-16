75be63344073afe4555bb191fe3e95814e66c633
Andreas Joachim Peters <andreas.joachim.peters@cern.ch>
2018-04-16 15:14:29 +0200

    MGM: avoid replica aggregation when FUSEX recovers and the FSTs actually work but the client 'just' got a timeout error

--- mgm/XrdMgmOfs/fsctl/Commit.cc
+++ mgm/XrdMgmOfs/fsctl/Commit.cc
@@ -227,6 +227,15 @@
             }
           }
 
+	  // check if we have this replica in the unlink list
+	  if (fusex && fmd->hasUnlinkedLocation((unsigned short) fsid)) {
+	    eos_thread_err("suppressing possible recovery replica for fid=%lu on unlinked fsid=%llu- rejecting replica", fmd->getId(), fsid); 
+	    // this happens when a FUSEX recovery has been triggered, to avoid to reattach replicas, 
+	    // we clean them up here
+	    return Emsg(epname, error, EBADE, "commit replica - file size is wrong [EBADE] - suppressing recovery replica",
+			"");
+	  }
+
           if (eos::common::LayoutId::GetLayoutType(lid) ==
               eos::common::LayoutId::kReplica) {
             // we check filesize and the checksum only for replica layouts
