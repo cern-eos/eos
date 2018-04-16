eb2a7f96104a1aff8c407c62ffc3caa6a7609ba5
Georgios Bitzes <georgios.bitzes@cern.ch>
2018-04-16 11:31:23 +0200

    CMAKE: Require and use -std=c++1y from now on

--- cmake/EosOSDefaults.cmake
+++ cmake/EosOSDefaults.cmake
@@ -76,31 +76,8 @@ endif()
 
 include(CheckCXXCompilerFlag)
 
-check_cxx_compiler_flag(-std=c++11 HAVE_STD_CPP11_FLAG)
-if (HAVE_STD_CPP11_FLAG)
-  # Check if including cmath works with -std=c++11 and -O3.
-  # It may not in MinGW due to bug http://ehc.ac/p/mingw/bugs/2250/.
-  set(CMAKE_REQUIRED_FLAGS "-std=c++11 -O3")
-  check_cxx_source_compiles("
-    #include <cmath>
-    int main() {}" FMT_CPP11_CMATH)
-  # Check if including <unistd.h> works with -std=c++11.
-  # It may not in MinGW due to bug http://sourceforge.net/p/mingw/bugs/2024/.
-  check_cxx_source_compiles("
-    #include <unistd.h>
-    int main() {}" FMT_CPP11_UNISTD_H)
-  if (FMT_CPP11_CMATH AND FMT_CPP11_UNISTD_H)
-    set(CPP11_FLAG -std=c++11)
-  else ()
-    check_cxx_compiler_flag(-std=gnu++11 HAVE_STD_GNUPP11_FLAG)
-    if (HAVE_STD_CPP11_FLAG)
-      set(CPP11_FLAG -std=gnu++11)
-    endif ()
-  endif ()
-  set(CMAKE_REQUIRED_FLAGS )
-else ()
-  check_cxx_compiler_flag(-std=c++0x HAVE_STD_CPP0X_FLAG)
-  if (HAVE_STD_CPP0X_FLAG)
-    set(CPP11_FLAG -std=c++0x)
-  endif ()
+check_cxx_compiler_flag(-std=c++1y HAVE_STD_CPP1Y_FLAG)
+if (NOT HAVE_STD_CPP1Y_FLAG)
+  message(FATAL_ERROR "Compiler with -std=c++1y support required.")
+  set(CPP11_FLAG -std=c++1y)
 endif ()
