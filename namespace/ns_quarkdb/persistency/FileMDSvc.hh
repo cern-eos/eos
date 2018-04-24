739749b4ce3041616b120db6ace955b4c89de41f
Georgios Bitzes <georgios.bitzes@cern.ch>
2018-04-24 10:54:13 +0200

    NS: Re-introduce asynchronous metadata provider
    Just for FileMDs right now.

--- namespace/ns_quarkdb/persistency/FileMDSvc.hh
+++ namespace/ns_quarkdb/persistency/FileMDSvc.hh
@@ -34,6 +34,7 @@ EOSNSNAMESPACE_BEGIN
 
 class IQuotaStats;
 class MetadataFlusher;
+class MetadataProvider;
 
 //------------------------------------------------------------------------------
 //! FileMDSvc based on Redis
@@ -190,8 +191,8 @@ private:
   qclient::QClient* pQcl; ///< QClient object
   qclient::QHash mMetaMap ; ///< Map holding metainfo about the namespace
   NextInodeProvider mInodeProvider; ///< Provides next free inode
-  LRU<IFileMD::id_t, IFileMD> mFileCache; ///< Local cache of file objects
   std::atomic<uint64_t> mNumFiles; ///< Total number of fileso
+  std::unique_ptr<MetadataProvider> mMetadataProvider; ///< Provides metadata from backend
 };
 
 EOSNSNAMESPACE_END
---
+++ namespace/ns_quarkdb/persistency/MetadataProvider.hh
@@ -0,0 +1,96 @@
+/************************************************************************
+ * EOS - the CERN Disk Storage System                                   *
+ * Copyright (C) 2016 CERN/Switzerland                                  *
+ *                                                                      *
+ * This program is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ ************************************************************************/
+
+//------------------------------------------------------------------------------
+//! @author Georgios Bitzes <georgios.bitzes@cern.ch>
+//! @brief Asynchronous metadata retrieval from QDB, with caching support.
+//------------------------------------------------------------------------------
+
+#pragma once
+#include "namespace/interface/IFileMD.hh"
+#include "namespace/interface/IContainerMD.hh"
+#include "namespace/Namespace.hh"
+#include "namespace/ns_quarkdb/LRU.hh"
+#include <qclient/QClient.hh>
+
+#include <folly/futures/Future.h>
+#include <folly/futures/FutureSplitter.h>
+
+namespace folly {
+  class Executor;
+}
+
+EOSNSNAMESPACE_BEGIN
+
+class IContainerMDSvc;
+class IFileMDSvc;
+
+//------------------------------------------------------------------------------
+//! Class MetadataProvider
+//------------------------------------------------------------------------------
+class MetadataProvider
+{
+public:
+  //----------------------------------------------------------------------------
+  //! Constructor
+  //----------------------------------------------------------------------------
+  MetadataProvider(qclient::QClient &qcl, IContainerMDSvc *contsvc, IFileMDSvc *filemvc);
+
+  //----------------------------------------------------------------------------
+  //! Retrieve ContainerMD by ID.
+  //----------------------------------------------------------------------------
+  folly::Future<IContainerMDPtr> retrieveContainerMD(id_t id);
+
+  //----------------------------------------------------------------------------
+  //! Retrieve FileMD by ID.
+  //----------------------------------------------------------------------------
+  folly::Future<IFileMDPtr> retrieveFileMD(id_t id);
+
+  //----------------------------------------------------------------------------
+  //! Insert newly created item into the cache.
+  //----------------------------------------------------------------------------
+  void insertFileMD(id_t id, IFileMDPtr item);
+
+  //----------------------------------------------------------------------------
+  //! Change file cache size.
+  //----------------------------------------------------------------------------
+  void setFileMDCacheSize(uint64_t size);
+
+private:
+  //----------------------------------------------------------------------------
+  //! Turn an incoming FileMDProto into FileMD, removing from the inFlight
+  //! staging area, and inserting into the cache.
+  //----------------------------------------------------------------------------
+  IFileMDPtr processIncomingFileMdProto(id_t id, eos::ns::FileMdProto proto);
+
+  qclient::QClient &mQcl;
+  IContainerMDSvc *mContSvc;
+  IFileMDSvc *mFileSvc;
+
+  std::mutex mMutex;
+  std::map<IContainerMD::id_t, folly::FutureSplitter<IContainerMDPtr>> mInFlightContainers;
+  std::map<IContainerMD::id_t, folly::FutureSplitter<IFileMDPtr>> mInFlightFiles;
+
+  LRU<IContainerMD::id_t, IContainerMD> mContainerCache;
+  LRU<IFileMD::id_t, IFileMD> mFileCache;
+
+  std::unique_ptr<folly::Executor> mExecutor;
+
+};
+
+EOSNSNAMESPACE_END
