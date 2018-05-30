364f775440f62f1359e1a114888d70f9f6aab5f0
Elvin Sindrilaru <esindril@cern.ch>
2018-05-30 18:52:56 +0200

    CI: Set the proper codename for the Ubuntu build

--- gitlab-ci/sign_debian_repository.sh
+++ gitlab-ci/sign_debian_repository.sh
@@ -19,6 +19,6 @@ rm $prefix/dists/$dist/Release $prefix/dists/$dist/InRelease $prefix/dists/$dist
 
 set -e
 
-apt-ftparchive -o APT::FTPArchive::Release::Origin=CERN -o APT::FTPArchive::Release::Label=EOS -o APT::FTPArchive::Release::Codename=artful -o APT::FTPArchive::Release::Architectures=amd64 -o APT::FTPArchive::Release::Components="$components" release $prefix/dists/$dist/ > $prefix/dists/$dist/Release
+apt-ftparchive -o APT::FTPArchive::Release::Origin=CERN -o APT::FTPArchive::Release::Label=EOS -o APT::FTPArchive::Release::Codename=$dist -o APT::FTPArchive::Release::Architectures=amd64 -o APT::FTPArchive::Release::Components="$components" release $prefix/dists/$dist/ > $prefix/dists/$dist/Release
 gpg --homedir /home/stci/ --clearsign -o $prefix/dists/$dist/InRelease $prefix/dists/$dist/Release
 gpg --homedir /home/stci/ -abs -o $prefix/dists/$dist/Release.gpg $prefix/dists/$dist/Release
