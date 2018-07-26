a874f747d704c867f8f74ebae0d6a4f2c4e2a0ae
Georgios Bitzes <georgios.bitzes@cern.ch>
2018-07-26 11:46:34 +0200

    NS: Fix murmur hasher to support all types, remove conversion operator from identifiers

--- common/Murmur3.hh
+++ common/Murmur3.hh
@@ -69,14 +69,10 @@ public:
   };
 
   struct eqstr {
-    bool operator()(const uint64_t s1, const uint64_t s2) const
+    template<typename T>
+    bool operator()(const T& val1, const T& val2) const
     {
-      return (s1 == s2);
-    }
-
-    bool operator()(const std::string& s1, const std::string& s2) const
-    {
-      return (s1 == s2);
+      return (val1 == val2);
     }
   };
 };
