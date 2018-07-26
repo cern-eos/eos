d1a8255e73553e9f61af896bc0957a7ca946b731
Elvin Sindrilaru <esindril@cern.ch>
2018-07-26 17:39:40 +0200

    CI: Rename some of the stages and clean up script [ci skip]

--- gitlab-ci/remove_old_artifacts.sh
+++ gitlab-ci/remove_old_artifacts.sh
@@ -1,18 +1,14 @@
-#! /bin/bash
-
+#!/bin/bash
 AGE_DAYS=10
 EOS_DIRS=/eos/project/s/storage-ci/www/eos/*/commit/
 
 for commit_dir in ${EOS_DIRS}; do
   find ${commit_dir} -regex '.*\(rpm\|dmg\)$' -type f -mtime +${AGE_DAYS} -exec rm {} \; || true
   # Recreate YUM repos
-  yum_repos=$(find ${commit_dir} -path "*/SRPMS" -o -path "*/x86_64" -type d)                                                                                                                                                           
-                                                                                                                                                                                                                                       
-  for repo_dir in ${yum_repos[@]}; 
-  do                                                                                                                                                                                                      
+  yum_repos=$(find ${commit_dir} -path "*/SRPMS" -o -path "*/x86_64" -type d)
+
+  for repo_dir in ${yum_repos[@]}; do
     echo "Update YUM repo: ${repo_dir}"
     createrepo --update -q ${repo_dir}
   done
-done                                                                                                                                                         
-
-
+done
