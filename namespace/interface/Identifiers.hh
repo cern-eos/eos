23fb5cd7f99345aeb6d1cf323c85ccacf9ef31b2
Georgios Bitzes <georgios.bitzes@cern.ch>
2018-07-26 11:24:18 +0200

    NS: Make conversion operator for identifier phantom types explicit
    So that they don't spontaneously and implicitly convert to uint64_t.
    (which is the whole point of the phantom types, instead of using
    uint64_t in the first place)

--- namespace/interface/Identifiers.hh
+++ namespace/interface/Identifiers.hh
@@ -76,7 +76,7 @@ public:
   //----------------------------------------------------------------------------
   //! Overload function operator
   //----------------------------------------------------------------------------
-  operator uint64_t() const
+  explicit operator uint64_t() const
   {
     return val;
   }
@@ -121,7 +121,7 @@ public:
   //----------------------------------------------------------------------------
   //! Overload function operator
   //----------------------------------------------------------------------------
-  operator uint64_t() const
+  explicit operator uint64_t() const
   {
     return val;
   }
