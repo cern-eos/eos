19ecbd62e0013f18a6d4d77bd9447cadbc3c4753
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2018-08-24 17:15:09 +0200

    MAN: Speed-up man page generation script

--- man/create_man.sh
+++ man/create_man.sh
@@ -3,9 +3,6 @@
 ###########################
 # create the EOS man pages
 ###########################
-export EOS_DISABLE_PIPEMODE=1
-rm -rf man1 >& /dev/null
-mkdir man1
 
 # commands excluded from man page generation
 excluded=(
@@ -22,18 +19,35 @@ excluded=(
         "?"
     )
 
+export EOS_DISABLE_PIPEMODE=1
+rm -rf man1 >& /dev/null
+mkdir man1
+
 # create eos command include file
-./create_eos_cmds.pl ${excluded[*]} > eos.cmds
+$(dirname $0)/create_eos_cmds.pl ${excluded[*]} > eos.cmds
 help2man --include eos.cmds --no-discard-stderr --help-option="-h " --no-info eos > man1/eos.1
 gzip man1/eos.1
+unlink eos.cmds
 
+# prepare array of command names
 excluded[${#excluded[@]} - 1]="\?"
 excluded_string=$(IFS=\| ; echo "${excluded[*]}")
+commands=(`eos -b help | awk '{print $1}' | grep -w -v -E "${excluded_string}" | cut -d\  -f1`)
 
-for name in `eos -b help | awk '{print $1}' | grep -w -v -E "${excluded_string}"`; do
-    echo "Processing command ${name} ..."
-    help2man --name "eos $name" --no-discard-stderr --help-option="$name -h " --no-info "eos " > man1/eos-$name.1
-    gzip man1/eos-$name.1
+# generate man page for each command asynchronously
+echo "Generating man pages:"
+for (( i=0; i<${#commands[@]}; i++ )); do
+    name="${commands[$i]}"
+    printf "  [%2s/%2s] Processing command %s\n" $((${i} + 1)) ${#commands[@]} ${name}
+
+    help2man --name "eos $name" --no-discard-stderr --help-option="$name -h " --no-info "eos " > man1/eos-${name}.1 \
+        && gzip man1/eos-${name}.1 &
 done
 
-unlink eos.cmds
+# wait at most 5 seconds for all man pages to be created
+COUNT=0
+while [[ `ls man1/ | wc -l` -ne $((${#commands[@]} + 1)) ]]; do
+    COUNT=$((COUNT + 1))
+    [[ ${COUNT} -eq 6 ]] && break
+    sleep 1
+done
