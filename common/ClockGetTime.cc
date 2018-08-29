fcf0e313210d7423602c46421d93648d59a17de7
Elvin Sindrilaru <esindril@cern.ch>
2018-08-29 10:55:13 +0200

    COMMON: Move clock_gettime implementation into a separate source file which is used only when the system does not provide it by default.

--- common/ClockGetTime.cc
+++ common/ClockGetTime.cc
@@ -1,5 +1,5 @@
 //------------------------------------------------------------------------------
-//! @file ClockGetTime.hh
+//! @file ClockGetTime.cc
 //------------------------------------------------------------------------------
 
 /************************************************************************
@@ -20,21 +20,25 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
  ************************************************************************/
 
-#pragma once
-#include <time.h>
+#include "common/ClockGetTime.hh"
 
 #ifdef __MACH__
-#include <mach/mach_time.h>
-#ifndef CLOCK_REALTIME
-#define CLOCK_REALTIME 0
-#define CLOCK_MONOTONIC 0
-#define CLOCK_REALTIME_COARSE 0
-#define clockid_t int
-#define clock_gettime _clock_gettime
-#endif
-
-int _clock_gettime(clockid_t clk_id, struct timespec* t);
-
-#else
-#define _clock_gettime clock_gettime
+int _clock_gettime(clockid_t clk_id, struct timespec* t)
+{
+  if (&clock_gettime == 0) {
+    mach_timebase_info_data_t timebase;
+    mach_timebase_info(&timebase);
+    uint64_t time;
+    time = mach_absolute_time();
+    double nseconds = ((double)time * (double)timebase.numer) / ((
+                        double)timebase.denom);
+    double seconds = ((double)time * (double)timebase.numer) / ((
+                       double)timebase.denom * 1e9);
+    t->tv_sec = seconds;
+    t->tv_nsec = nseconds;
+    return 0;
+  } else {
+    return clock_gettime(clk_id, t);
+  }
+}
 #endif
