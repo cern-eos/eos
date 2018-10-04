fa086de050a76d3417e31f6b1f50e6843898899a
Elvin Sindrilaru <esindril@cern.ch>
2018-10-04 16:23:48 +0200

    TEST: Add test for draining when there is a long running writer that actually closes the file while the file systems is being drained.

--- test/XrdCpRandom.cc
+++ test/XrdCpRandom.cc
@@ -21,8 +21,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
  ************************************************************************/
 
-/*-----------------------------------------------------------------------------*/
-/*-----------------------------------------------------------------------------*/
 #include <vector>
 #include <map>
 #include <stdio.h>
@@ -30,39 +28,36 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
-/*-----------------------------------------------------------------------------*/
 #include <XrdPosix/XrdPosixXrootd.hh>
 #include <XrdCl/XrdClFileSystem.hh>
 #include <XrdOuc/XrdOucString.hh>
-/*-----------------------------------------------------------------------------*/
 
 XrdPosixXrootd posixXrootd;
 
-int main (int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
   // creates a 100M file written in random order
   XrdOucString urlFile = argv[1];
+
   if (!urlFile.length()) {
-    fprintf(stderr,"usage: xrdcpabort <url>\n");
+    fprintf(stderr, "usage: xrdcpabort <url>\n");
     exit(EINVAL);
   }
 
   int fdWrite = XrdPosixXrootd::Open(urlFile.c_str(),
-				     O_CREAT|O_RDWR|O_TRUNC,
-				     kXR_ur | kXR_uw | kXR_gw | kXR_gr | kXR_or );
-
+                                     O_CREAT | O_RDWR | O_TRUNC,
+                                     kXR_ur | kXR_uw | kXR_gw | kXR_gr | kXR_or);
   std::map<off_t, off_t> lmap;
   std::vector<off_t> voff;
-
   char* buffer = (char*) malloc(100000000);
 
-  for (size_t i=0; i< 100000000; i++) {
-    buffer[i] = i%255;
+  for (size_t i = 0; i < 100000000; i++) {
+    buffer[i] = i % 255;
   }
 
   // create 100 pieces
-  for (size_t i=0; i<100; i++) {
-    off_t offset = (off_t)(100000000.0 * random()/RAND_MAX);
+  for (size_t i = 0; i < 100; i++) {
+    off_t offset = (off_t)(100000000.0 * random() / RAND_MAX);
     voff.push_back(offset);
     lmap[offset] = 0;
     //    fprintf(stderr,"Chunk %u : %llu\n", (unsigned int) i, (unsigned long long)offset);
@@ -70,19 +65,19 @@ int main (int argc, char* argv[]) {
 
   std::map<off_t, off_t>::const_iterator it1;
   std::map<off_t, off_t>::const_iterator it2;
-  it2=lmap.begin();
+  it2 = lmap.begin();
   it2++;
-  for (it1 = lmap.begin(); (it1 != lmap.end())&&(it2 != lmap.end()) ; it1++) {
-    lmap[it1->first] = (it2->first-it1->first);
+
+  for (it1 = lmap.begin(); (it1 != lmap.end()) && (it2 != lmap.end()) ; it1++) {
+    lmap[it1->first] = (it2->first - it1->first);
     //    fprintf(stderr,"%llu %llu\n", it1->first,lmap[it1->first]);
     it2++;
   }
 
-
-  if (fdWrite>=0) {
-    for (size_t i=0; i< 100 ;i++) {
+  if (fdWrite >= 0) {
+    for (size_t i = 0; i < 100 ; i++) {
       //      fprintf(stderr,"Writing %llu %llu\n", voff[i], lmap[voff[i]]);
-      XrdPosixXrootd::Pwrite(fdWrite, buffer+voff[i], lmap[voff[i]],voff[i]);
+      XrdPosixXrootd::Pwrite(fdWrite, buffer + voff[i], lmap[voff[i]], voff[i]);
     }
   } else {
     exit(-1);
---
+++ test/XrdCpSlowWriter.cc
@@ -0,0 +1,107 @@
+//------------------------------------------------------------------------------
+//! @file XrdCpSlowWriter.cc
+//! @author Elvin Sindrilaru - CERN
+//------------------------------------------------------------------------------
+
+/************************************************************************
+ * EOS - the CERN Disk Storage System                                   *
+ * Copyright (C) 2018 CERN/Switzerland                                  *
+ *                                                                      *
+ * This program is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ ************************************************************************/
+
+#include "XrdCl/XrdClFile.hh"
+#include <thread>
+#include <chrono>
+#include <fstream>
+#include <iostream>
+
+//------------------------------------------------------------------------------
+//! This executable simulates a client which keeps the file open for more than
+//! 1 minutes are writes slowly blocks of data.
+//------------------------------------------------------------------------------
+int main(int argc, char* argv[])
+{
+  if (argc < 2) {
+    std::cerr << "Usage: " << argv[0] << " <xrootd_url> [<transfer_time>]"
+              << std::endl
+              << "  <xrootd_url> - full XRootD URL where file is written"
+              << std::endl
+              << "  <transfer_time> - total time in seconds the transfer "
+              << " should take, default 80 seconds"
+              << std::endl;
+    exit(EINVAL);
+  }
+
+  std::string surl = argv[1];
+  XrdCl::URL url(surl);
+
+  if (!url.IsValid()) {
+    std::cerr << "error: given URL is not valid" << std::endl;
+    exit(EINVAL);
+  }
+
+  uint32_t tx_time = 80;
+
+  if (argc == 3) {
+    try {
+      tx_time = std::stoul(argv[2]);
+    } catch (const std::exception& e) {
+      tx_time = 80;
+    }
+  }
+
+  // Allocate a random buffer used for writing
+  // Fill buffer with random characters
+  uint32_t block_size = 1024 * 1024;
+  std::unique_ptr<char> buffer {new char[block_size]};
+  std::ifstream urandom("/dev/urandom", std::ios::in | std::ios::binary);
+  urandom.read(buffer.get(), block_size);
+  urandom.close();
+  // Open file and start writing
+  XrdCl::File file;
+  XrdCl::XRootDStatus status =
+    file.Open(surl, XrdCl::OpenFlags::Delete | XrdCl::OpenFlags::Write,
+              XrdCl::Access::UR | XrdCl::Access::UW);
+
+  if (!status.IsOK()) {
+    std::cerr << "error: unable to open file for writing, errno="
+              << status.errNo << std::endl;
+    exit(status.errNo);
+  }
+
+  int count = 8;
+  uint64_t offset = 0ull;
+  int sleep_sec = (int) tx_time / count;
+
+  while (count) {
+    status = file.Write(offset, block_size, buffer.get());
+    std::cout << "info: slow write at offset=" << offset << std::endl;
+
+    if (!status.IsOK()) {
+      std::cerr << "error: failed write offset=" << offset << ", lenght="
+                << block_size << std::endl;
+      exit(status.errNo);
+    }
+
+    --count;
+    offset += block_size;
+    std::this_thread::sleep_for(std::chrono::seconds(sleep_sec));
+  }
+
+  if (!file.Close().IsOK()) {
+    std::cerr << "error: failed to close file" << std::endl;
+    exit(EIO);
+  }
+}
