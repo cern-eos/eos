905b5a47a1e2987a0343d24a41c4d684e37aeb96
Andreas Joachim Peters <andreas.joachim.peters@cern.ch>
2019-02-11 16:43:13 +0100

    UTILS: add some new missing dependency packages

--- utils/el7-packages.sh
+++ utils/el7-packages.sh
@@ -1,3 +1,5 @@
+yum install -y centos-release-scl
+yum install -y centos-release-scl
 yum install -y cmake3
 yum install --disablerepo=epel -y xrootd-server-devel xrootd-private-devel xrootd-client xrootd-client-devel
 yum install -y sparsehash-devel
@@ -10,7 +12,7 @@ yum install -y cppunit cppunit-devel
 yum install -y fuse-libs fuse fuse-devel
 yum install -y libattr libattr-devel
 yum install -y openldap openldap-devel
-yum install -y libmicrohttp libmicrohttpd-devel
+yum install -y libmicrohttpd libmicrohttpd-devel
 yum install -y zlib zlib-devel zlib-static
 yum install -y xfsprogs xfsprogs-devel
 yum install -y e2fsprogs-devel
@@ -21,4 +23,9 @@ yum install -y libcurl libcurl-devel
 yum install -y hiredis hiredis-devel
 yum install -y libevent libevent-devel
 yum install -y git
+yum install -y bzip2-devel bzip2-libs
+yum install -y jemalloc jemalloc-devel
+yum intsall -y eos-rocksdb
 ( cd /tmp/; git clone  https://github.com/zeromq/cppzmq; cp cppzmq/zmq.hpp /usr/include/ )
+
+echo do \"source /opt/rh/devtoolset-6/enable\"
