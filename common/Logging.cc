1922675e5820e4d4af82e2e01e00966a03b8e6f1
Georgios Bitzes <georgios.bitzes@cern.ch>
2019-02-19 14:48:11 +0100

    COMMON: Drop unnecessary (?) fflush in Logging class to check for improvements in performance

--- common/Logging.cc
+++ common/Logging.cc
@@ -236,7 +236,6 @@ Logging::log(const char* func, const char* file, int line, const char* logid,
       // we do log-message fanout
       if (gLogFanOut.count("*")) {
         fprintf(gLogFanOut["*"], "%s\n", buffer);
-        fflush(gLogFanOut["*"]);
       }
 
       if (gLogFanOut.count(File.c_str())) {
@@ -248,7 +247,6 @@ Logging::log(const char* func, const char* file, int line, const char* logid,
                 EOS_TEXTNORMAL,
                 sourceline,
                 ptr);
-        fflush(gLogFanOut[File.c_str()]);
         buffer[15] = ' ';
       } else {
         if (gLogFanOut.count("#")) {
@@ -264,16 +262,13 @@ Logging::log(const char* func, const char* file, int line, const char* logid,
                   func,
                   ptr
                  );
-          fflush(gLogFanOut["#"]);
           buffer[15] = ' ';
         }
       }
 
       fprintf(stderr, "%s\n", buffer);
-      fflush(stderr);
     } else {
       fprintf(stderr, "%s\n", buffer);
-      fflush(stderr);
     }
   }
 
