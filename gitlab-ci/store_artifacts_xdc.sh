06c3e82aa418ba41a84d6ab2800cd4272dd34eb6
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2019-02-20 10:50:58 +0100

    CI: Publish commit RPMs to XDC repo

---
+++ gitlab-ci/store_artifacts_xdc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ex
+
+BRANCH=$1
+BUILD_TYPE=$2
+PATH_PREFIX=$3
+
+STORAGE_PATH_CC7=${PATH_PREFIX}/${BRANCH}/${BUILD_TYPE}/el-7/x86_64
+mkdir -p $STORAGE_PATH_CC7
+cp cc7_artifacts/RPMS/* $STORAGE_PATH_CC7
+createrepo --update -q $STORAGE_PATH_CC7
+STORAGE_PATH_CC7_SRPM=${PATH_PREFIX}/${BRANCH}/${BUILD_TYPE}/el-7/SRPMS
+mkdir -p $STORAGE_PATH_CC7_SRPM
+cp cc7_artifacts/SRPMS/* $STORAGE_PATH_CC7_SRPM
+createrepo --update -q $STORAGE_PATH_CC7_SRPM
+
+exit 0
