20779c997e1f225f3f218cd2cb3ab07441f0a8bb
Andreas Joachim Peters <andreas.joachim.peters@cern.ch>
2019-03-14 13:54:34 +0100

    FUSEX: declare FUSEPROTOCOLVERSION macro to have a handle to increase the current valid version in a single place

--- fusex/eosfuse.hh
+++ fusex/eosfuse.hh
@@ -52,6 +52,9 @@
 #include <google/dense_hash_set>
 #include <google/dense_hash_map>
 
+// PROTOBUF protocol version announced via heartbeats and attached to URLs by the backend
+#define FUSEPROTOCOLVERSION eos::fusex::heartbeat::PROTOCOLV4
+
 class EosFuse : public llfusexx::FuseBase<EosFuse>
 {
 public:
