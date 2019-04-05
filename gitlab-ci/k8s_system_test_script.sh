51b6753b754268797d68d9023305aa78a17da0f0
Fabio Luchetti <fabio.luchetti@cern.ch>
2019-04-05 17:28:20 +0200

    CI: Use correct IMAGE_TAG for tagged releases in k8s tests

--- gitlab-ci/k8s_system_test_script.sh
+++ gitlab-ci/k8s_system_test_script.sh
@@ -8,7 +8,7 @@ if [[ $1 =~ ^[a-z0-9]([-a-z0-9]*[a-z0-9])?$ ]];
 fi
 
 # get_podname() : Return the name of the Pods tagged with $1. Suppose it return just one result.
-# $1 			: is a label selector with key="app", specifying identifying attributes for a Kubernetes object.
+#            $1 : is a label selector with key="app", specifying identifying attributes for a Kubernetes object.
 # Example of admitted labels are {eos-mgm, eos-mq, eos-fst1, eos-fst2 ... }, mirroring mirrors eos-roles
 # refs :https://kubernetes.io/docs/concepts/overview/working-with-objects/labels/
 function get_podname () { 
@@ -32,7 +32,7 @@ kubectl exec --namespace=${NAMESPACE} $(get_podname eos-cli1) \
 kubectl exec --namespace=${NAMESPACE} $(get_podname eos-cli1) \
 	-- /bin/bash -c 'atd; at now <<< "mkdir /eos1/; mount -t fuse eosxd /eos1/; mkdir /eos2/; mount -t fuse eosxd /eos2/;"'
 kubectl exec --namespace=${NAMESPACE} $(get_podname eos-cli1) \
-	-- /bin/bash -c 'count=0; while [[ $count -le 10 ]] && ( [[ ! -d /eos1/dockertest/ ]] || [[ ! -d /eos2/dockertest/ ]] ); do echo "Wait for mount... $count"; count=$(($count + 1)); sleep 1; done;'
+	-- /bin/bash -c 'count=0; while [[ $count -le 10 ]] && ( [[ ! -d /eos1/dockertest/ ]] || [[ ! -d /eos2/dockertest/ ]] ); do echo "Wait for mount... $count"; (( count++ )); sleep 1; done;'
 kubectl exec --namespace=${NAMESPACE} $(get_podname eos-cli1) \
 	-- su - eos-user -c 'mkdir /eos1/dockertest/fusex_tests/; cd /eos1/dockertest/fusex_tests/; /usr/sbin/fusex-benchmark' # workaround for docker exec '-u' flag
 
@@ -48,9 +48,15 @@ else
 	-- su - eos-user -c 'python /eosclient-tests/run.py --workdir="/eos1/dockertest /eos2/dockertest" ci';
 fi
 
-# if [ "$CI_JOB_NAME" != k8s_ubuntu_system_test ]; then
-# 	kubectl exec --namespace=${NAMESPACE} $(get_podname eos-cli1) \
-# 	-- /bin/bash -c 'eos fuse mount /eos_fuse; eos fuse mount /eos_fuse2;';
-# 	kubectl exec --namespace=${NAMESPACE} $(get_podname eos-cli1) \
-# 	-- python /eosclient-tests/run.py --workdir="/eos_fuse/dockertest /eos_fuse2/dockertest" ci;
-# fi
+if [ "$CI_JOB_NAME" != k8s_ubuntu_system_test ]; then
+	kubectl exec --namespace=${NAMESPACE} $(get_podname eos-cli1) \
+	-- /bin/bash -c 'eos fuse mount /eos_fuse; eos fuse mount /eos_fuse2;';
+	kubectl exec --namespace=${NAMESPACE} $(get_podname eos-cli1) \
+	-- python /eosclient-tests/run.py --workdir="/eos_fuse/dockertest /eos_fuse2/dockertest" ci;
+fi
+
+
+# @todo something like
+# function kexec () {
+# 	kubectl exec -it $(kubectl get pods --no-headers --output custom-columns=":metadata.name" --selector app=$1 --namespace=$NAMESPACE) --namespace=$NAMESPACE ${@:2}
+# }
