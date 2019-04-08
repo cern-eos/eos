e67790aa8e71c01d2494e0cbea40febb2705de61
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2019-04-08 16:06:25 +0200

    CMAKE: Small adjustments to CMake find messages

--- cmake/Findrichacl.cmake
+++ cmake/Findrichacl.cmake
@@ -17,37 +17,22 @@ else()
     PATH_SUFFIXES include)
 
   find_library(
-    RICHACL_LIB
+    RICHACL_LIBRARY
     NAMES richacl
     HINTS ${RICHACL_ROOT_DIR}
     PATH_SUFFIXES ${LIBRARY_PATH_PREFIX})
 
-  if(RICHACL_LIB)
-    message(STATUS "RICHACL_LIB is set to ${RICHACL_LIB}")
-  else()
-    message(STATUS "RICHACL_LIB is not set")
-  endif()
+  set(RICHACL_INCLUDE_DIRS ${RICHACL_INCLUDE_DIR})
+  set(RICHACL_LIBRARIES ${RICHACL_LIBRARY})
 
-  if(RICHACL_LIB)
-    set(RICHACL_LIBRARIES ${RICHACL_LIB})
-  else()
-    set(RICHACL_LIBRARIES "")
-  endif()
-
-  if(RICHACL_INCLUDE_DIR)
-    set(RICHACL_INCLUDE_DIRS ${RICHACL_INCLUDE_DIR})
-  else()
-    set(RICHACL_INCLUDE_DIRS "")
+  if(RICHACL_FOUND)
+    add_definitions(-DRICHACL_FOUND)
   endif()
 
   find_package_handle_standard_args(
     richacl
     DEFAULT_MSG
-    RICHACL_LIB RICHACL_INCLUDE_DIR)
+    RICHACL_LIBRARY RICHACL_INCLUDE_DIR)
 
-  ## mark_as_advanced(RICHACL_LIBRARIES RICHACL_INCLUDE_DIR)
-
-  if(RICHACL_FOUND)
-      add_definitions(-DRICHACL_FOUND)
-  endif()
+  mark_as_advanced(RICHACL_LIBRARY RICHACL_INCLUDE_DIR)
 endif()
