03e495103972bf862ab1b0ea946eaffc419ccd71
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2019-04-26 16:04:15 +0200

    CI: Modify commit artifacts clean-up script to target only citrine branch. Also, recreate repo only if outdated RPMs have been deleted [ci-skip]

--- gitlab-ci/remove_old_artifacts.sh
+++ gitlab-ci/remove_old_artifacts.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+
 AGE_DAYS=10
-EOS_DIRS=/eos/project/s/storage-ci/www/eos/*/commit/
+EOS_DIRS=/eos/project/s/storage-ci/www/eos/citrine/commit
 
-for commit_dir in ${EOS_DIRS}; do
-  find ${commit_dir} -regex '.*\(rpm\|dmg\)$' -type f -mtime +${AGE_DAYS} -exec rm {} \; || true
-  # Recreate YUM repos
-  yum_repos=$(find ${commit_dir} -path "*/SRPMS" -o -path "*/x86_64" -type d)
+for commit_dir in ${EOS_DIRS}/*; do
+  count=$(find ${commit_dir} -regex '.*\(rpm\|dmg\)$' -type f -mtime +${AGE_DAYS} -print -delete | wc -l)
 
-  for repo_dir in ${yum_repos[@]}; do
-    echo "Update YUM repo: ${repo_dir}"
-    createrepo --update -q ${repo_dir}
-  done
+  if [[ ${count} -gt 0 ]]; then
+    echo "${commit_dir}: deleted ${count} RPMs"
+
+    # Recreate YUM repos
+    yum_repos=$(find ${commit_dir} -path "*/SRPMS" -o -path "*/x86_64" -type d)
+
+    for repo_dir in ${yum_repos[@]}; do
+      echo "Updating YUM repo: ${repo_dir}"
+      createrepo --update -q ${repo_dir}
+    done
+  fi
 done
