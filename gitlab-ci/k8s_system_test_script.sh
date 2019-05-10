dc4e3f4edc57b265f0c2e83f4eb40e933e6b5de0
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2019-05-10 16:54:00 +0200

    CI: Don't execute eos-instance-test script on ubuntu system test jobs. The eos-instance-test script must be executed on the MGM machine, whereas the Ubuntu testing jobs should only test the Ubuntu clients.

--- gitlab-ci/k8s_system_test_script.sh
+++ gitlab-ci/k8s_system_test_script.sh
@@ -1,31 +1,62 @@
 #!/bin/bash -ve
 
 
-NAMESPACE=""
-if [[ $1 =~ ^[a-z0-9]([-a-z0-9]*[a-z0-9])?$ ]]; 
-	then NAMESPACE=$1
-	else echo "! Wrong arg $1: arg1 must be a DNS-1123 label and must consist of lower case alphanumeric characters or '-', and must start and end with an alphanumeric character"; exit 1
-fi
+function usage () {
+  filename=$(basename $0)
+  echo "Usage: $filename [--only-client] <namespace>"
+  echo "       --only-client  -- execute only client functionality tests"
+  echo "       <namespace>    -- name of the k8s namespace (DNS-1123 label)"
+}
 
 # get_podname() : Return the name of the Pods tagged with $1. Suppose it return just one result.
 #            $1 : is a label selector with key="app", specifying identifying attributes for a Kubernetes object.
 # Example of admitted labels are {eos-mgm, eos-mq, eos-fst1, eos-fst2 ... }, mirroring mirrors eos-roles
 # refs :https://kubernetes.io/docs/concepts/overview/working-with-objects/labels/
-function get_podname () { 
+function get_podname () {
 	kubectl get pods --namespace=${NAMESPACE} --no-headers -o custom-columns=":metadata.name" -l app=$1
 }
 
+################################################################################
+# Set up variables
+################################################################################
+
+if [[ $1 = "--only-client" ]]; then
+  ONLY_CLIENT=true
+  shift
+fi
+
+if [[ $# -ne 1 ]]; then
+  echo "Invalid number of arguments"
+  usage
+  exit 1
+fi
+
+NAMESPACE=""
+if [[ $1 =~ ^[a-z0-9]([-a-z0-9]*[a-z0-9])?$ ]]; then
+  NAMESPACE=$1
+else
+  echo "! Wrong arg $1: arg1 must be a DNS-1123 label and must consist of lower case alphanumeric characters or '-', and must start and end with an alphanumeric character"
+  exit 1
+fi
+
+################################################################################
+# Execute system tests
+################################################################################
 
 kubectl exec --namespace=${NAMESPACE} $(get_podname eos-mgm) \
 	-- eos chmod 2777 /eos/dockertest/
 kubectl exec --namespace=${NAMESPACE} $(get_podname eos-mgm) \
 	-- eos vid enable krb5
-kubectl exec --namespace=${NAMESPACE} $(get_podname eos-mgm) \
-	-- sed -i 's/eos-mq-test.eoscluster.cern.ch/eos-mq/g' /usr/sbin/eos-instance-test-ci # @todo tmp, then re-code the files
-kubectl exec --namespace=${NAMESPACE} $(get_podname eos-mgm) \
-	-- sed -i "s/eos-fst4-test.eoscluster.cern.ch/eos-fst4.eos-fst4.${NAMESPACE}.svc.cluster.local/g" /usr/sbin/eos-drain-test # @todo tmp, then re-code the files
-kubectl exec --namespace=${NAMESPACE} $(get_podname eos-mgm) \
+
+# Execute full suite of instance tests if ONLY_CLIENT flag is not defined
+if [[ -z $ONLY_CLIENT ]]; then
+  kubectl exec --namespace=${NAMESPACE} $(get_podname eos-mgm) \
+    -- sed -i 's/eos-mq-test.eoscluster.cern.ch/eos-mq/g' /usr/sbin/eos-instance-test-ci # @todo tmp, then re-code the files
+  kubectl exec --namespace=${NAMESPACE} $(get_podname eos-mgm) \
+    -- sed -i "s/eos-fst4-test.eoscluster.cern.ch/eos-fst4.eos-fst4.${NAMESPACE}.svc.cluster.local/g" /usr/sbin/eos-drain-test # @todo tmp, then re-code the files
+  kubectl exec --namespace=${NAMESPACE} $(get_podname eos-mgm) \
 	-- eos-instance-test-ci
+fi
 
 kubectl exec --namespace=${NAMESPACE} $(get_podname eos-cli1) \
 	-- git clone https://gitlab.cern.ch/dss/eosclient-tests.git
