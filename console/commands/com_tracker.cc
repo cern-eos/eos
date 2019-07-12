ee027f2e8082eb795878920c22c253748d735a9a
Andreas Joachim Peters <andreas.joachim.peters@cern.ch>
2019-07-12 10:58:25 +0200

    CONSOLE: add shortened command 'tracker' for 'space tracker'

---
+++ console/commands/com_tracker.cc
@@ -0,0 +1,35 @@
+// ----------------------------------------------------------------------
+// File: com_tracker.cc
+// Author: Andreas-Joachim Peters - CERN
+// ----------------------------------------------------------------------
+
+/************************************************************************
+ * EOS - the CERN Disk Storage System                                   *
+ * Copyright (C) 2019 CERN/Switzerland                                  *
+ *                                                                      *
+ * This program is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ ************************************************************************/
+
+/*----------------------------------------------------------------------------*/
+#include "console/ConsoleMain.hh"
+extern int com_space (char*);
+/*----------------------------------------------------------------------------*/
+
+int
+com_tracker (char *arg1)
+{
+  XrdOucString cmd = "tracker ";
+  cmd += arg1;
+  return com_space((char*)cmd.c_str());
+}
