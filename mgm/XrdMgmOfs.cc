e92f183d267a5e06786618ea0020d45eedeb570c
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2019-07-18 14:13:07 +0200

    MGM: QoS attributes list and get -- low-level API

--- mgm/XrdMgmOfs.cc
+++ mgm/XrdMgmOfs.cc
@@ -565,6 +565,7 @@ XrdMgmOfs::Disc(const XrdSecEntity* client)
 #include "XrdMgmOfs/Merge.cc"
 #include "XrdMgmOfs/Mkdir.cc"
 #include "XrdMgmOfs/PathMap.cc"
+#include "XrdMgmOfs/QoS.cc"
 #include "XrdMgmOfs/Remdir.cc"
 #include "XrdMgmOfs/Rename.cc"
 #include "XrdMgmOfs/Rm.cc"
