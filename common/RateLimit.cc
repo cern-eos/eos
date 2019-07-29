5206f675ef1e5e2a977a0811c232db4494b0edbe
Elvin Sindrilaru <esindril@cern.ch>
2019-07-29 16:39:48 +0200

    COMMON/UNIT_TEST: Add RateLimit interface and RequestRateLimit class to be used in fsck

---
+++ common/RateLimit.cc
@@ -0,0 +1,115 @@
+//------------------------------------------------------------------------------
+//! @file RateLimit.cc
+//! @author Elvin Sindrilaru - CERN
+//------------------------------------------------------------------------------
+
+/************************************************************************
+ * EOS - the CERN Disk Storage System                                   *
+ * Copyright (C) 2019 CERN/Switzerland                                  *
+ *                                                                      *
+ * This program is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ ************************************************************************/
+
+#include "common/RateLimit.hh"
+#include <chrono>
+#include <iostream>
+#include <stdexcept>
+
+using namespace std::chrono;
+
+EOSCOMMONNAMESPACE_BEGIN
+
+//----------------------------------------------------------------------------
+//! Get delay for the current request
+//!
+//! @return delay in microseconds for the current requestor
+//----------------------------------------------------------------------------
+microseconds
+RequestRateLimit::GetDelay()
+{
+  // If no rate limit enforced then no delay
+  if (mRate == 0ull) {
+    return microseconds(0);
+  }
+
+  std::lock_guard<std::mutex> lock(mMutex);
+  // Compute expire timestamp which is one second back in time
+  uint64_t now_us = duration_cast<microseconds>
+                    (mClock.getTime().time_since_epoch()).count();
+  // Round to the beginning of the interval
+  now_us = (now_us / mRateIntervalUs) * mRateIntervalUs;
+  uint64_t expire_us = now_us - 1000000;
+
+  // Try to expire old entries if any present
+  if (!mSetReqTimestamps.empty()) {
+    auto it = mSetReqTimestamps.lower_bound(expire_us);
+
+    if (it != mSetReqTimestamps.end()) {
+      // If current entry is after the expire timestamp we have to go one
+      // back unless this is the beginning
+      if ((it != mSetReqTimestamps.begin()) && (*it > expire_us)) {
+        --it;
+      }
+    }
+
+    if (it == mSetReqTimestamps.begin()) {
+      if (*it <= expire_us) {
+        (void) mSetReqTimestamps.erase(it);
+      }
+    } else {
+      // Delete all the entries before this iterator i.e. expired ones
+      for (auto it_del = mSetReqTimestamps.begin(); it_del != it; /*no inc*/) {
+        it_del = mSetReqTimestamps.erase(it_del);
+      }
+    }
+  }
+
+  uint64_t new_entry_us {now_us};
+  uint64_t num_requests = mSetReqTimestamps.size();
+  bool has_delay = false;
+
+  if (num_requests >= mRate) {
+    has_delay =  true;
+    // We need to wait for the first entry in the set to expire
+    new_entry_us = *mSetReqTimestamps.begin() + 1000000;
+    auto it = mSetReqTimestamps.lower_bound(new_entry_us);
+
+    // If that slot is taken already then move on to the next one
+    while (it != mSetReqTimestamps.end()) {
+      new_entry_us += mRateIntervalUs;
+      it = mSetReqTimestamps.lower_bound(new_entry_us);
+    }
+  } else {
+    // If the current slot is taken then try the next one
+    while (mSetReqTimestamps.find(new_entry_us) != mSetReqTimestamps.end()) {
+      new_entry_us += mRateIntervalUs;
+    }
+  }
+
+  mSetReqTimestamps.insert(new_entry_us);
+  uint64_t delay {0ull};
+
+  if (has_delay) {
+    delay = new_entry_us - now_us;
+  }
+
+  // This is for testing purposes only
+  if (mLastTimestampUs < new_entry_us) {
+    mLastTimestampUs = new_entry_us;
+  }
+
+  return microseconds(delay);
+}
+
+EOSCOMMONNAMESPACE_END
---
+++ common/RateLimit.hh
@@ -0,0 +1,161 @@
+//------------------------------------------------------------------------------
+//! @file RateLimit.hh
+//! @author Elvin Sindrilaru - CERN
+//------------------------------------------------------------------------------
+
+/************************************************************************
+ * EOS - the CERN Disk Storage System                                   *
+ * Copyright (C) 2019 CERN/Switzerland                                  *
+ *                                                                      *
+ * This program is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ ************************************************************************/
+
+#pragma once
+#include "common/Namespace.hh"
+#include "common/SteadyClock.hh"
+#include <atomic>
+#include <thread>
+#include <set>
+
+EOSCOMMONNAMESPACE_BEGIN
+
+//------------------------------------------------------------------------------
+//! Rate limit interface
+//------------------------------------------------------------------------------
+class IRateLimit
+{
+public:
+  //----------------------------------------------------------------------------
+  //! Constructor
+  //----------------------------------------------------------------------------
+  IRateLimit(bool fake_clock = false):
+    mRate(0ull), mClock(fake_clock)
+  {}
+
+  //----------------------------------------------------------------------------
+  //! Destructor
+  //----------------------------------------------------------------------------
+  virtual ~IRateLimit() = default;
+
+  //----------------------------------------------------------------------------
+  //! Get rate
+  //!
+  //! @return enforced rate
+  //----------------------------------------------------------------------------
+  virtual unsigned long long GetRatePerSecond() const
+  {
+    return mRate;
+  }
+
+  //----------------------------------------------------------------------------
+  //! Set rate
+  //----------------------------------------------------------------------------
+  virtual void SetRatePerSecond(unsigned long long rate)
+  {
+    mRate = rate;
+  }
+
+  //----------------------------------------------------------------------------
+  //! Allow method that might delay the current thread until it can proceed
+  //! processing prmits.
+  //!
+  //! @param permits number of permits requested
+  //!
+  //! @return microseconds that the current thread was delayed
+  //----------------------------------------------------------------------------
+  virtual uint64_t Allow(uint64_t permits) = 0;
+
+  //----------------------------------------------------------------------------
+  //! Get clock reference for testing purposes
+  //----------------------------------------------------------------------------
+  inline eos::common::SteadyClock& GetClock()
+  {
+    return mClock;
+  }
+
+protected:
+  std::atomic<unsigned long long> mRate; ///< Rate per second
+  eos::common::SteadyClock mClock; ///< Clock wrapper also used for testing
+};
+
+
+//------------------------------------------------------------------------------
+//! Requests per second rate limiter
+//------------------------------------------------------------------------------
+class RequestRateLimit: public IRateLimit
+{
+public:
+  //----------------------------------------------------------------------------
+  //! Constructor
+  //----------------------------------------------------------------------------
+  RequestRateLimit(bool fake_clock = false):
+    IRateLimit(fake_clock)
+  {}
+
+  //----------------------------------------------------------------------------
+  //! Destructor
+  //----------------------------------------------------------------------------
+  ~RequestRateLimit() = default;
+
+  //----------------------------------------------------------------------------
+  //! Set rate
+  //----------------------------------------------------------------------------
+  void SetRatePerSecond(unsigned long long rate)
+  {
+    if (rate > 1000000) {
+      throw std::runtime_error("error: RequestRateLimit works only for rates "
+                               "lower than 1MHz");
+    }
+
+    mRate = rate;
+    mRateIntervalUs = 1000000 / rate;
+  }
+
+  //----------------------------------------------------------------------------
+  //! Allow submitting a request
+  //!
+  //! @return milliseconds that the current thread was delayed
+  //----------------------------------------------------------------------------
+  uint64_t Allow(uint64_t permits = 1)
+  {
+    auto wait = GetDelay();
+
+    // When testing skip the actual sleepp
+    if (!mClock.IsFake()) {
+      std::this_thread::sleep_for(wait);
+    }
+
+    return wait.count();
+  }
+
+private:
+#ifdef IN_TEST_HARNESS
+public:
+#endif
+  //----------------------------------------------------------------------------
+  //! Get delay for the current request
+  //!
+  //! @return delay in microseconds for the current requestor
+  //----------------------------------------------------------------------------
+  std::chrono::microseconds GetDelay();
+
+  std::mutex mMutex; ///< Mutex protecting the set of timestamps
+  ///< Set of request timestamps in microsec for each request
+  std::set<uint64_t> mSetReqTimestamps;
+  uint64_t mRateIntervalUs; ///< Interval in microsec corresp. to the given rate
+  ///< Last timestamp of an entry added to the set - only for testing
+  std::atomic<uint64_t> mLastTimestampUs;
+};
+
+EOSCOMMONNAMESPACE_END
