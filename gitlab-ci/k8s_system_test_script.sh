48a7835ca9ddd08e859ab620267ef22c4a7c8b36
Fabio Luchetti <fabio.luchetti@cern.ch>
2019-07-29 18:22:59 +0200

    TEST: Rename k8s jobs to shorten url of Pods. Passing pipeline ID #999999 caused hostname-to-IP translation to truncate the 'IPaddress:Port' last digit in XRootd

--- gitlab-ci/k8s_system_test_script.sh
+++ gitlab-ci/k8s_system_test_script.sh
@@ -79,7 +79,7 @@ kubectl exec --namespace=${NAMESPACE} $(get_podname eos-cli1) \
 kubectl exec --namespace=${NAMESPACE} $(get_podname eos-cli1) \
 	-- su - eos-user -c 'python /eosclient-tests/run.py --workdir="/eos1/dockertest /eos2/dockertest" ci'
 
-if [ "$CI_JOB_NAME" != k8s_ubuntu_system_test ]; then
+if [ "$CI_JOB_NAME" != k8s_ubuntu_system ]; then
 	kubectl exec --namespace=${NAMESPACE} $(get_podname eos-cli1) \
 	-- /bin/bash -c 'eos fuse mount /eos_fuse; eos fuse mount /eos_fuse2;';
 	kubectl exec --namespace=${NAMESPACE} $(get_podname eos-cli1) \
