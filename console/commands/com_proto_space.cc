0866dc621df06b78740308f78f5cb35a526748f3
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2019-08-15 17:55:41 +0200

    CONSOLE: Fix parsing of 'space define <groupmod>' argument. EOS-3678

--- console/commands/com_proto_space.cc
+++ console/commands/com_proto_space.cc
@@ -186,7 +186,7 @@ bool SpaceHelper::ParseCommand(const char* arg)
       if (!tokenizer.NextToken(token)) {
         define->set_groupmod(24);
       } else {
-        define->set_groupsize(std::stoi(token));
+        define->set_groupmod(std::stoi(token));
       }
     }
   } else if (token == "set") {
@@ -381,22 +381,22 @@ void com_space_help()
   std::ostringstream oss;
   oss
       << " usage:\n"
-      << "space ls [-s|-g <depth>] [-m|-l|--io|--fsck] [<space>] : list in all spaces or select only <space>. <space> is a substring match and can be a comma seperated list\n"
+      << "space ls [-s|-g <depth>] [-m|-l|--io|--fsck] [<space>] : list in all spaces or select only <space>. <space> is a substring match and can be a comma separated list\n"
       << "\t      -s : silent mode\n"
       << "\t      -m : monitoring key=value output format\n"
       << "\t      -l : long output - list also file systems after each space\n"
       << "\t      -g : geo output - aggregate space information along the instance geotree down to <depth>\n"
-      << "\t    --io : print IO satistics\n"
+      << "\t    --io : print IO statistics\n"
       << "\t  --fsck : print filesystem check statistics\n"
       << std::endl
       << "space config <space-name> space.nominalsize=<value>                   : configure the nominal size for this space\n"
       << "space config <space-name> space.balancer=on|off                       : enable/disable the space balancer [default=off]\n"
       << "space config <space-name> space.balancer.threshold=<percent>          : configure the used bytes deviation which triggers balancing            [ default=20 (%%)     ] \n"
-      << "space config <space-name> space.balancer.node.rate=<MB/s>             : configure the nominal transfer bandwith per running transfer on a node [ default=25 (MB/s)   ]\n"
+      << "space config <space-name> space.balancer.node.rate=<MB/s>             : configure the nominal transfer bandwidth per running transfer on a node [ default=25 (MB/s)   ]\n"
       << "space config <space-name> space.balancer.node.ntx=<#>                 : configure the number of parallel balancing transfers per node          [ default=2 (streams) ]\n"
       << "space config <space-name> space.converter=on|off                      : enable/disable the space converter [default=off]\n"
       << "space config <space-name> space.converter.ntx=<#>                     : configure the number of parallel conversions per space                 [ default=2 (streams) ]\n"
-      << "space config <space-name> space.drainer.node.rate=<MB/s >             : configure the nominal transfer bandwith per running transfer on a node [ default=25 (MB/s)   ]\n"
+      << "space config <space-name> space.drainer.node.rate=<MB/s >             : configure the nominal transfer bandwidth per running transfer on a node [ default=25 (MB/s)   ]\n"
       << "space config <space-name> space.drainer.node.ntx=<#>                  : configure the number of parallel draining transfers per node           [ default=2 (streams) ]\n"
       << "space config <space-name> space.drainer.node.nfs=<#>                  : configure the number of max draining filesystems per node (Valid only for central drain)  [ default=5 ]\n"
       << "space config <space-name> space.drainer.retries=<#>                   : configure the number of retry for the draining process (Valid only for central drain)     [ default=1 ]\n"
@@ -421,7 +421,7 @@ void com_space_help()
       << std::endl
       << "space define <space-name> [<groupsize> [<groupmod>]] : define how many filesystems can end up in one scheduling group <groupsize> [default=0]\n"
       << "                                                       => <groupsize>=0 means that no groups are built within a space, otherwise it should be the maximum number of nodes in a scheduling group\n"
-      << "                                                       => <groupmod> maximum number of groups in the space, which should be at least equal to the maximun number of filesystems per node\n"
+      << "                                                       => <groupmod> maximum number of groups in the space, which should be at least equal to the maximum number of filesystems per node\n"
       << std::endl
       << "space inspector [--current|-c] [--last|-l] [-m] [-p] [-e] : show namespace inspector output\n"
       << "\t  -c  : show current scan\n"
@@ -430,7 +430,7 @@ void com_space_help()
       << "\t  -p  : combined with -c or -l lists erroneous files\n"
       << "\t  -e  : combined with -c or -l exports erroneous files on the MGM into /var/log/eos/mgm/FileInspector.<date>.list\n"
       << std::endl
-      << "space node-set <space-name> <node.key> <file-name> : store the contents of <file-name> into the node configuration variable <node.key> visibile to all FSTs\n"
+      << "space node-set <space-name> <node.key> <file-name> : store the contents of <file-name> into the node configuration variable <node.key> visible to all FSTs\n"
       << "                                                     => if <file-name> matches file:<path> the file is loaded from the MGM and not from the client\n"
       << "                                                     => local files cannot exceed 512 bytes - MGM files can be arbitrary length\n"
       << "                                                     => the contents gets base64 encoded by default\n"
