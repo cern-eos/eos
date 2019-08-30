2f993eb40344b8c50aca21ca41bf9086912e4ef2
Georgios Bitzes <georgios.bitzes@cern.ch>
2019-08-30 10:38:38 +0200

    COMMON: Switch from std::quick_exit to _exit for Mac OS compat

--- common/Assert.hh
+++ common/Assert.hh
@@ -23,8 +23,8 @@
 
 #pragma once
 
-#include <cstdlib>
+#include <unistd.h>
 
-#define eos_assert(condition) if(!((condition))) { std::cerr << "assertion violation in " << __PRETTY_FUNCTION__ << " at " << __FILE__ << ":" << __LINE__ << ", condition is not true: " << #condition << std::endl; std::quick_exit(1); }
+#define eos_assert(condition) if(!((condition))) { std::cerr << "assertion violation in " << __PRETTY_FUNCTION__ << " at " << __FILE__ << ":" << __LINE__ << ", condition is not true: " << #condition << std::endl; _exit(1); }
 #define SSTR(message) static_cast<std::ostringstream&>(std::ostringstream().flush() << message).str()
 #define DBG(message) std::cerr << __FILE__ << ":" << __LINE__ << " -- " << #message << " = " << message << std::endl
