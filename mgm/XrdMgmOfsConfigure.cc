8d20a87105567dfab762da707320baaa69782c23
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2019-11-01 16:08:07 +0100

    MGM: Add environment variable to enable the Converter engine

--- mgm/XrdMgmOfsConfigure.cc
+++ mgm/XrdMgmOfsConfigure.cc
@@ -2072,6 +2072,13 @@ XrdMgmOfs::Configure(XrdSysError& Eroute)
   // Start the drain engine
   mDrainEngine.Start();
 
+  // Start the Converter driver
+  if (getenv("EOS_CONVERTER_DRIVER") != nullptr) {
+    eos_info("msg=\"starting Converter Engine\"");
+    mConverterDriver.reset(new eos::mgm::ConverterDriver(mQdbContactDetails));
+    mConverterDriver->Start();
+  }
+
   if (mTapeEnabled) {
     try {
       mTapeGc->start(tapeGcSpaces);
