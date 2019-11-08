97a25b592066425e3576123ee045146d458c3808
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2019-11-08 17:54:59 +0100

    MGM: Modify the File command handler to allow scheduling of jobs for the ConverterDriver

--- mgm/proc/user/File.cc
+++ mgm/proc/user/File.cc
@@ -1224,15 +1224,32 @@ ProcCommand::File()
 
                 eos::common::VirtualIdentity rootvid = eos::common::VirtualIdentity::Root();
 
-                if (gOFS->_touch(conversiontagfile, *mError, rootvid, 0)) {
-                  stdErr += "error: unable to create conversion job '";
-                  stdErr += conversiontagfile;
-                  stdErr += "'";
-                  retc = errno;
+                if (gOFS->mConverterDriver) {
+                  // Push conversion job to QuarkDB
+                  std::string conversiontag = conversiontagfile;
+                  conversiontag.erase(0, gOFS->MgmProcConversionPath.length() + 1);
+
+                  if (gOFS->mConverterDriver->ScheduleJob(fmd->getId(), conversiontag)) {
+                    stdOut += "success: pushed conversion job '";
+                    stdOut += conversiontag.c_str();
+                    stdOut += "' to QuarkDB";
+                  } else {
+                    stdErr += "error: unable to push conversion job '";
+                    stdErr += conversiontag.c_str();
+                    stdErr += "' to QuarkDB";
+                  }
                 } else {
-                  stdOut += "success: created conversion job '";
-                  stdOut += conversiontagfile;
-                  stdOut += "'";
+                  // Use file-based conversion scheduling
+                  if (gOFS->_touch(conversiontagfile, *mError, rootvid, 0)) {
+                    stdErr += "error: unable to create conversion job '";
+                    stdErr += conversiontagfile;
+                    stdErr += "'";
+                    retc = errno;
+                  } else {
+                    stdOut += "success: created conversion job '";
+                    stdOut += conversiontagfile;
+                    stdOut += "'";
+                  }
                 }
               }
             }
