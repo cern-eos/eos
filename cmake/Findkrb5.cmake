539879101378130a123010feccc9091b088bab20
Elvin Sindrilaru <esindril@cern.ch>
2019-11-11 23:51:01 +0100

    CMAKE: Update Findkrb5

--- cmake/Findkrb5.cmake
+++ cmake/Findkrb5.cmake
@@ -1,59 +1,47 @@
-# - Try to find Kerberos5 
+#  Try to find Kerberos5
 #  Check for libkrb5.a
 #
-#  KRB5_INCLUDE_DIR - where to find krb5.h, etc.
-#  KRB5_LIBRARIES   - List of libraries when using ....
 #  KRB5_FOUND       - True if Kerberos 5 libraries found.
+#  KRB5_INCLUDE_DIR - where to find krb5.h, etc.
+#  KRB5_LIBRARIES   - List of libraries needed to use krb5
+#
+# and the following imported targets
+#
+# KRB5::KRB5
+
+find_package(PkgConfig)
+pkg_check_modules(PC_krb5 QUIET krb5)
+
+find_path(KRB5_INCLUDE_DIR
+  NAMES krb5/krb5.h
+  HINTS ${KRB5_ROOT} ${PC_krb5_INCLUDEDIR} ${PC_kbr5_INCLUDE_DIRS})
+
+find_library(KRB5_MIT_LIBRARY
+  NAMES k5crypto
+  HINTS ${KRB5_ROOT} ${PC_krb5_LIBDIR} ${PC_krb5_LIBRARY_DIRS})
+
+find_library(KRB5_LIBRARY
+  NAMES krb5
+  HINTS ${KRB5_ROOT} ${PC_krb5_LIBDIR} ${PC_krb5_LIBRARY_DIRS})
+
+find_program(KRB5_INIT NAMES kinit
+  HINTS ${KRB5_ROOT} /usr/bin/ /usr/local/bin)
 
 include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(krb5
+  REQUIRED_VARS KRB5_LIBRARY KRB5_INCLUDE_DIR KRB5_MIT_LIBRARY)
+mark_as_advanced(KRB5_INCLUDE_DIR KRB5_MIT_LIBRARY KRB4_MIT_LIBRARY)
 
-if(KRB5_INCLUDE_DIRS AND KRB5_LIBRARIES)
-  set(KRB5_FIND_QUIETLY TRUE)
-else()
-  find_path(KRB5_INCLUDE_DIR NAMES krb5.h 
-    /usr/kerberos/include
-    /usr/krb5/include
-    /usr/local/kerberos/include
-    /usr/include 
-    /usr/include/kerberosV 
-    /usr/local/include
-    $ENV{KRB5_DIR}/include) 
-
-  set(KRB5_INCLUDE_DIRS ${KRB5_INCLUDE_DIR})
-
-  find_library(KRB5_MIT_LIBRARY NAMES k5crypto
-    /usr/kerberos/lib
-    /usr/krb5/lib 
-    /usr/local/kerberos/lib
-    /usr/lib64 
-    /usr/lib 
-    /usr/local/lib
-    $ENV{KRB5_DIR}/lib)
-
-  set(KRB5_LIBRARIES ${KRB5_LIBRARIES} ${KRB5_MIT_LIBRARY})
- 
-  find_library(KRB5_LIBRARY NAMES krb5
-    /usr/kerberos/lib
-    /usr/krb5/lib 
-    /usr/local/kerberos/lib
-    /usr/lib64 
-    /usr/lib 
-    /usr/local/lib
-    $ENV{KRB5_DIR}/lib)
-
-
-  set(KRB5_LIBRARIES ${KRB5_LIBRARIES} ${KRB5_LIBRARY})
-
-  find_program(KRB5_INIT NAMES kinit
-    /usr/kerberos/bin
-    /usr/krb5/bin 
-    /usr/local/kerberos/bin
-    /usr/bin
-    /usr/local/bin
-    $ENV{KRB5_DIR}/bin)
-  
-  find_package_handle_standard_args(
-    krb5 DEFAULT_MSG KRB5_LIBRARY KRB5_INCLUDE_DIR)
-
-  mark_as_advanced(KRB5_INCLUDE_DIR KRB5_MIT_LIBRARY)
+if (KRB5_FOUND AND NOT TARGET KRB5::KRB5)
+  add_library(KRB5::KRB5 UNKNOWN IMPORTED)
+  set_target_properties(KRB5::KRB5 PROPERTIES
+    IMPORTED_LOCATION "${KRB5_LIBRARY}"
+    INTERFACE_LINK_LIBRARIES "${KRB5_MIT_LIBRARY}"
+    INTERFACE_INCLUDE_DIRECTORIES "${KRB5_INCLUDE_DIR}")
 endif()
+
+set(KRB5_INCLUDE DIRS ${KRB5_INCLUDE_DIR})
+set(KRB5_LIBRARIES ${KRB5_LIBRARY} ${KRB5_MIT_LIBRARY})
+unset(KRB5_INCLUDE_DIR)
+unset(KRB5_LIBRARY)
+unset(KRB5_MIT_LIBRARY)
