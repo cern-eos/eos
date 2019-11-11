63f72c42aa9816dd16569b131c48752c56a4448f
Elvin Sindrilaru <esindril@cern.ch>
2019-11-11 22:17:43 +0100

    CMAKE: Update FindSparseHash

--- cmake/FindSparseHash.cmake
+++ cmake/FindSparseHash.cmake
@@ -3,23 +3,26 @@
 #
 # SPARSEHASH_FOUND        - system has SparseHash
 # SPARSEHASH_INCLUDE_DIRS - SparseHash include directories
+#
+# and the following imported tags
+#
+# GOOGLE::SPARSEHASH
+
+find_path(SPARSEHASH_INCLUDE_DIR
+  NAMES sparsehashtable.h
+  HINTS ${SPARSEHASH_ROOT}
+  PATH_SUFFIXES google/sparsehash/)
 
 include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(SparseHash
+  REQUIRED_VARS SPARSEHASH_INCLUDE_DIR)
+mark_as_advanced(SPARSEHASH_FOUND SPARSEHASH_INCLUDE_DIR)
 
-if(SPARSEHASH_INCLUDE_DIRS)
-  set(SPARSEHASH_FIND_QUIETLY TRUE)
-else()
-  find_path(
-    SPARSEHASH_INCLUDE_DIR
-    NAMES google/sparsehash/sparsehashtable.h
-    HINTS ${SPARSEHASH_ROOT_DIR}
-    PATH_SUFFIXES include)
-
-  set(SPARSEHASH_INCLUDE_DIRS ${SPARSEHASH_INCLUDE_DIR})
-
-  find_package_handle_standard_args(
-    SparseHash
-    DEFAULT_MSG SPARSEHASH_INCLUDE_DIR)
-
-  mark_as_advanced(SPARSEHASH_INCLUDE_DIR)
+if (SPARSEHASH_FOUND AND NOT TARGET GOOGLE::SPARSEHASH)
+  add_library(GOOGLE::SPARSEHASH INTERFACE IMPORTED)
+  set_target_properties(GOOGLE::SPARSEHASH PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${SPARSEHASH_INCLUDE_DIR}")
 endif()
+
+set(SPARSEHASH_INCLUDE_DIRS ${SPARSEHASH_INCLUDE_DIR})
+#unset(SPARSEHASH_INCLUDE_DIR)
