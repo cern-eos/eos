3171e0f27e2f3792833e897631bef05e91109b96
Andreas Joachim Peters <andreas.joachim.peters@cern.ch>
2019-11-18 10:16:51 +0100

    MGM: fix SEGV in FuseServer when not existing things get deleted - fixes EOS-3850

--- mgm/FuseServer/Server.cc
+++ mgm/FuseServer/Server.cc
@@ -1408,7 +1408,6 @@ Server::OpSetDirectory(const std::string& id,
 	throw_mdexception(ENOENT, "No such directory : " << md.md_ino());
       }
 
-
       if (cmd->getParentId() != md.md_pino()) {
         // this indicates a directory move
         {
@@ -2297,6 +2296,11 @@ Server::OpDeleteDirectory(const std::string& id,
                                               md.md_ino()));
     }
 
+    if (!cmd) {
+      // directory does not exist
+      throw_mdexception(ENOENT, "No such directory : " << md.md_ino());
+    }
+
     pcmd->setMTime(mtime);
 
     // check if this directory is empty
@@ -2387,6 +2391,11 @@ Server::OpDeleteFile(const std::string& id,
     } else {
       fmd = gOFS->eosFileService->getFileMD(eos::common::FileId::InodeToFid(
                                               md.md_ino()));
+    }
+
+    if (!fmd) {
+      // file does not exist
+      throw_mdexception(ENOENT, "No such file : " << md.md_ino());
     }
 
     pcmd->setMTime(mtime);
@@ -2556,6 +2565,12 @@ Server::OpDeleteLink(const std::string& id,
       fmd = gOFS->eosFileService->getFileMD(eos::common::FileId::InodeToFid(
                                               md.md_ino()));
     }
+
+    if (!fmd) {
+      // no link 
+      throw_mdexception(ENOENT, "No such link : " << md.md_ino());
+    }
+
 
     pcmd->setMTime(mtime);
     eos_info("ino=%lx delete-link", (long) md.md_ino());
