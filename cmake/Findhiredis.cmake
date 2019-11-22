e445a29937f1922edd5687d9f84c339c6aa44510
Elvin Sindrilaru <esindril@cern.ch>
2019-11-22 14:50:20 +0100

    CMAKE: Update Findhiredis

--- cmake/Findhiredis.cmake
+++ cmake/Findhiredis.cmake
@@ -5,34 +5,33 @@
 # HIREDIS_INCLUDE_DIRS - hiredis include directories
 # HIREDIS_LIBRARIES    - libraries need to use hiredis
 #
-# HIREDIS_ROOT_DIR may be defined as a hint for where to look
+# and the following imported target
+#
+# HIREDIS::HIREDIS
 
-if(HIREDIS_INCLUDE_DIRS AND HIREDIS_LIBRARIES)
-  set(HIREDIS_FIND_QUIETLY TRUE)
-  add_definitions(-DHAVE_HIREDIS)
-else()
-  find_path(
-    HIREDIS_INCLUDE_DIR
-    NAMES hiredis/hiredis.h
-    HINTS ${HIREDIS_ROOT_DIR}
-    PATH_SUFFIXES include)
+find_path(HIREDIS_INCLUDE_DIR
+  NAMES hiredis/hiredis.h
+  HINTS ${HIREDIS_ROOT})
 
-  find_library(
-    HIREDIS_LIBRARY
-    NAMES hiredis
-    HINTS ${HIREDIS_ROOT_DIR}
-    PATH_SUFFIXES ${CMAKE_INSTALL_LIBDIR})
+find_library(HIREDIS_LIBRARY
+  NAMES hiredis
+  HINTS ${HIREDIS_ROOT}
+  PATH_SUFFIXES ${CMAKE_INSTALL_LIBDIR})
 
-  set(HIREDIS_INCLUDE_DIRS ${HIREDIS_INCLUDE_DIR})
-  set(HIREDIS_LIBRARIES ${HIREDIS_LIBRARY})
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(hiredis
+  DEFAULT_MSG HIREDIS_LIBRARY HIREDIS_INCLUDE_DIR)
 
-  include (FindPackageHandleStandardArgs)
-  find_package_handle_standard_args(
-    hiredis DEFAULT_MSG HIREDIS_LIBRARY HIREDIS_INCLUDE_DIR)
-
+if (HIREDIS_FOUND AND NOT TARGET HIREDIS::HIREDIS)
   mark_as_advanced(HIREDIS_LIBRARY HIREDIS_INCLUDE_DIR)
-
-  if (HIREDIS_FOUND)
-    add_definitions(-DHAVE_HIREDIS)
-  endif()
+  add_library(HIREDIS::HIREDIS UNKNOWN IMPORTED)
+  set_target_properties(HIREDIS::HIREDIS PROPERTIES
+    IMPORTED_LOCATION "${HIREDIS_LIBRARY}"
+    INTERFACE_INCLUDE_DIRECTORIES "${HIREDIS_INCLUDE_DIR}"
+    INTERFACE_COMPILE_DEFINITIONS HAVE_HIREDIS)
 endif()
+
+set(HIREDIS_INCLUDE_DIRS ${HIREDIS_INCLUDE_DIR})
+set(HIREDIS_LIBRARIES ${HIREDIS_LIBRARY})
+unset(HIREDIS_INCLUDE_DIR)
+unset(HIREDIS_LIBRARY)
