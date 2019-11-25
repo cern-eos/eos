d2b13d61dee00d4c6f39da69613fab69cf338c55
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2019-11-25 12:12:31 +0100

    CI: Update XDC branch to publish RPMs to citrine-xdc

--- gitlab-ci/export_branch.sh
+++ gitlab-ci/export_branch.sh
@@ -5,7 +5,11 @@
 if [[ "${CI_BUILD_REF_NAME}" == "beryl_aquamarine" ]] || [[ ${CI_BUILD_REF_NAME} == 0.3* ]] ; then
   REFNAME="aquamarine"
 else
-  REFNAME="citrine"
+  if [[ "${CI_JOB_STAGE}" == "publish" ]]; then
+    REFNAME="citrine-xdc"
+  else
+    REFNAME="citrine"
+  fi
 fi
 
 export BRANCH=$REFNAME
