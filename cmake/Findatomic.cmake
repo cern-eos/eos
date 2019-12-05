08941af8b0920bc0c36c58a99e0ec9d564853e62
Elvin Sindrilaru <esindril@cern.ch>
2019-12-05 16:01:44 +0100

    CMAKE: Update Findatomic to add imported target

--- cmake/Findatomic.cmake
+++ cmake/Findatomic.cmake
@@ -3,24 +3,25 @@
 #
 # ATOMIC_FOUND        - system has libatomic
 # ATOMIC_LIBRARIES    - libraries needed to use libatomic
+#
+# and the following imported target
+# ATOMIC::ATOMIC
+
+find_library(ATOMIC_LIBRARY
+  NAMES atomic atomic.so.1 libatomic.so.1
+  HINTS ${ATOMIC_ROOT}
+  PATH_SUFFIXES ${CMAKE_INSTALL_LIBDIR})
 
 include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(atomic
+  DEFAULT_MSG ATOMIC_LIBRARY)
 
-if(ATOMIC_INCLUDE_DIRS AND ATOMIC_LIBRARIES)
-  set(ATOMIC_FIND_QUIETLY TRUE)
-else()
-  find_library(
-    ATOMIC_LIBRARY
-    NAMES atomic atomic.so.1 libatomic.so.1
-    HINTS ${ATOMIC_ROOT_DIR}
-    PATH_SUFFIXES ${LIBRARY_PATH_PREFIX})
-
-  set(ATOMIC_LIBRARIES ${ATOMIC_LIBRARY})
-
-  find_package_handle_standard_args(
-    atomic 
-    DEFAULT_MSG 
-    ATOMIC_LIBRARY)
-
+if (ATOMIC_FOUND AND NOT TARGET ATOMIC::ATOMIC)
   mark_as_advanced(ATOMIC_LIBRARY)
+  add_library(ATOMIC::ATOMIC UNKNOWN IMPORTED)
+  set_target_properties(ATOMIC::ATOMIC PROPERTIES
+    IMPORTED_LOCATION ${ATOMIC_LIBRARY})
 endif()
+
+set(ATOMIC_LIBRARIES ${ATOMIC_LIBRARY})
+unset(ATOMIC_LIBRARY)
