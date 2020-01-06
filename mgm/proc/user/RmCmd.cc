b654847e91174cac1fb6492c9f2cd335db82cd49
Fabio Luchetti <fabio.luchetti@cern.ch>
2020-01-06 02:24:12 +0100

    Revert "MGM: Sanitize rm command against SEGV-on-write - ref EOS-3870"
    This reverts commit f81392d40cd1daa1f54d4997ad3d180175919c91.

--- mgm/proc/user/RmCmd.cc
+++ mgm/proc/user/RmCmd.cc
@@ -131,13 +131,7 @@ eos::mgm::RmCmd::ProcessRequest() noexcept
       // Adding regex anchors for beginning and end of string
       XrdOucString filter_temp = "^";
       // Changing wildcard * into regex syntax
-
-      // @note temporary patch while "xrootd issue 1094" is fixed. @todo check for further usages of XrcOucString::replace method
-      // filter.replace("*", ".*");
-      std::string sfilter {filter.c_str()};
-      sfilter.replace(sfilter.begin(), sfilter.end(),"*", ".*");
-      filter = sfilter.c_str();
-
+      filter.replace("*", ".*");
       filter_temp += filter;
       filter_temp += "$";
       int reg_rc = regcomp(&(regex_filter), filter_temp.c_str(),
