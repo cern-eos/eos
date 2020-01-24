35670c4e99ad9af8f3aa91ef3a0e63fd9b2f514e
Fabio Luchetti <fabio.luchetti@cern.ch>
2020-01-24 19:39:13 +0100

    CI\BUILD: increase ccache cache size

--- gitlab-ci/setup_ccache_deb.sh
+++ gitlab-ci/setup_ccache_deb.sh
@@ -6,7 +6,7 @@ export CCACHE_DIR="`pwd`/ccache"
 export CCACHE_BASEDIR="`pwd`"
 export CCACHE_SLOPPINESS=pch_defines
 export CCACHE_NOHASHDIR=true
-export CCACHE_MAXSIZE=1.5G
+export CCACHE_MAXSIZE=2G
 
 ccache -z
 
--- gitlab-ci/setup_ccache_slc6.sh
+++ gitlab-ci/setup_ccache_slc6.sh
@@ -5,6 +5,7 @@ set -x
 export CCACHE_DIR="`pwd`/ccache"
 export CCACHE_BASEDIR="`rpm --eval %_builddir`/`ls build/SRPMS/*.src.rpm | awk -F '.' 'BEGIN{OFS=".";} {NF-=3;}1' | awk -F '/' '{print $NF}'`"
 export CCACHE_SLOPPINESS=pch_defines
+ccache --max-size=3G
 
 ccache -z
 
