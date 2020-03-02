60e49069936a23dbc54d3226dbea38c1efded83e
Fabio Luchetti <fabio.luchetti@cern.ch>
2020-03-02 14:14:12 +0100

    CI: Run c8 related jobs (tests) also on tags; Use 'krb5 mit' in tests by default

--- gitlab-ci/before_script_docker_test.sh
+++ gitlab-ci/before_script_docker_test.sh
@@ -10,4 +10,4 @@ export CLI_IMAGE_TAG="${BASETAG}${CLI_BASETAG}${CI_COMMIT_TAG}${CI_PIPELINE_ID}"
 docker pull gitlab-registry.cern.ch/dss/eos:${IMAGE_TAG}
 docker pull gitlab-registry.cern.ch/dss/eos:${CLI_IMAGE_TAG}
 
-sudo ./eos-docker/scripts/start_services.sh -q -i gitlab-registry.cern.ch/dss/eos:${IMAGE_TAG} -u gitlab-registry.cern.ch/dss/eos:${CLI_IMAGE_TAG} -k ${KRB5:-"heimdal"} ;
+sudo ./eos-docker/scripts/start_services.sh -q -i gitlab-registry.cern.ch/dss/eos:${IMAGE_TAG} -u gitlab-registry.cern.ch/dss/eos:${CLI_IMAGE_TAG} -k ${KRB5:-"mit"} ;
--- gitlab-ci/before_script_k8s_test.sh
+++ gitlab-ci/before_script_k8s_test.sh
@@ -8,8 +8,4 @@ export K8S_NAMESPACE=$(echo ${CI_JOB_NAME}-${CI_JOB_ID}-${CI_PIPELINE_ID} | tr '
 export IMAGE_TAG="${BASETAG}${CI_COMMIT_TAG}${CI_PIPELINE_ID}";
 export CLI_IMAGE_TAG="${BASETAG}${CLI_BASETAG}${CI_COMMIT_TAG}${CI_PIPELINE_ID}";
 
-if [[ "$CI_JOB_NAME" =~ "InMemory" ]]; then
-    ./eos-on-k8s/create-all.sh -i ${IMAGE_TAG} -u ${CLI_IMAGE_TAG} -n ${K8S_NAMESPACE} -k ${KRB5:-"mit"};
-else
-    ./eos-on-k8s/create-all.sh -i ${IMAGE_TAG} -u ${CLI_IMAGE_TAG} -n ${K8S_NAMESPACE} -q -k ${KRB5:-"mit"};
-fi;
+./eos-on-k8s/create-all.sh -i ${IMAGE_TAG} -u ${CLI_IMAGE_TAG} -n ${K8S_NAMESPACE} -q -k ${KRB5:-"mit"};
