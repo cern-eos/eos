501d21560971a1d6f6e0012ba2263f75f5e6db7e
Fabio Luchetti <fabio.luchetti@cern.ch>
2020-03-09 16:29:11 +0100

    CI: Use right image name in tests for tagged releases

--- gitlab-ci/before_script_docker_test.sh
+++ gitlab-ci/before_script_docker_test.sh
@@ -3,9 +3,14 @@
 sudo ./eos-docker/scripts/shutdown_services.sh
 ./eos-docker/scripts/remove_unused_images.sh
 
-# always only one between $CI_COMMIT_TAG $CI_PIPELINE_ID exists
-export IMAGE_TAG="${BASETAG}${CI_COMMIT_TAG}${CI_PIPELINE_ID}";
-export CLI_IMAGE_TAG="${BASETAG}${CLI_BASETAG}${CI_COMMIT_TAG}${CI_PIPELINE_ID}";
+if [[ -n $CI_COMMIT_TAG ]]; then
+  export IMAGE_TAG="${BASETAG}${CI_COMMIT_TAG}";
+  export CLI_IMAGE_TAG="${BASETAG}${CLI_BASETAG}${CI_COMMIT_TAG}";
+else
+  export IMAGE_TAG="${BASETAG}${CI_PIPELINE_ID}";
+  export CLI_IMAGE_TAG="${BASETAG}${CLI_BASETAG}${CI_PIPELINE_ID}";
+fi
+
 
 docker pull gitlab-registry.cern.ch/dss/eos:${IMAGE_TAG}
 docker pull gitlab-registry.cern.ch/dss/eos:${CLI_IMAGE_TAG}
