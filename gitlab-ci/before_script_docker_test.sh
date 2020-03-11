6b9217725d3ccbd63ae179f835bcd8afa4119ce2
Fabio Luchetti <fabio.luchetti@cern.ch>
2020-03-11 11:35:04 +0100

    CI: Tag eos docker images with CI_COMMIT_SHORT_SHA, not CI_PIPELINE_ID; Slim c8 prebuild image

--- gitlab-ci/before_script_docker_test.sh
+++ gitlab-ci/before_script_docker_test.sh
@@ -3,16 +3,11 @@
 sudo ./eos-docker/scripts/shutdown_services.sh
 ./eos-docker/scripts/remove_unused_images.sh
 
-if [[ -n $CI_COMMIT_TAG ]]; then
-  export IMAGE_TAG="${BASETAG}${CI_COMMIT_TAG}";
-  export CLI_IMAGE_TAG="${BASETAG}${CLI_BASETAG}${CI_COMMIT_TAG}";
-else
-  export IMAGE_TAG="${BASETAG}${CI_PIPELINE_ID}";
-  export CLI_IMAGE_TAG="${BASETAG}${CLI_BASETAG}${CI_PIPELINE_ID}";
-fi
-
+# either $CI_COMMIT_TAG either $CI_COMMIT_SHORT_SHA
+export IMAGE_TAG="${BASETAG}${CI_COMMIT_TAG:-$CI_COMMIT_SHORT_SHA}"
+export CLI_IMAGE_TAG="${BASETAG}${CLI_BASETAG}${CI_COMMIT_TAG:-$CI_COMMIT_SHORT_SHA}"
 
 docker pull gitlab-registry.cern.ch/dss/eos:${IMAGE_TAG}
 docker pull gitlab-registry.cern.ch/dss/eos:${CLI_IMAGE_TAG}
 
-sudo ./eos-docker/scripts/start_services.sh -q -i gitlab-registry.cern.ch/dss/eos:${IMAGE_TAG} -u gitlab-registry.cern.ch/dss/eos:${CLI_IMAGE_TAG} -k ${KRB5:-"mit"} ;
+sudo ./eos-docker/scripts/start_services.sh -q -i gitlab-registry.cern.ch/dss/eos:${IMAGE_TAG} -u gitlab-registry.cern.ch/dss/eos:${CLI_IMAGE_TAG} -k ${KRB5:-"mit"}
--- gitlab-ci/before_script_k8s_test.sh
+++ gitlab-ci/before_script_k8s_test.sh
@@ -4,14 +4,8 @@ source /home/gitlab-runner/coe-cluster-config/st-gitlab-k8s-02/env_st-gitlab-k8s
 git clone https://gitlab.cern.ch/eos/eos-on-k8s.git
 export K8S_NAMESPACE=$(echo ${CI_JOB_NAME}-${CI_JOB_ID}-${CI_PIPELINE_ID} | tr '_' '-' | tr '[:upper:]' '[:lower:]')
 
+# either $CI_COMMIT_TAG either $CI_COMMIT_SHORT_SHA
+export IMAGE_TAG="${BASETAG}${CI_COMMIT_TAG:-$CI_COMMIT_SHORT_SHA}"
+export CLI_IMAGE_TAG="${BASETAG}${CLI_BASETAG}${CI_COMMIT_TAG:-$CI_COMMIT_SHORT_SHA}"
 
-if [[ -n $CI_COMMIT_TAG ]]; then
-  export IMAGE_TAG="${BASETAG}${CI_COMMIT_TAG}";
-  export CLI_IMAGE_TAG="${BASETAG}${CLI_BASETAG}${CI_COMMIT_TAG}";
-else 
-  export IMAGE_TAG="${BASETAG}${CI_PIPELINE_ID}";
-  export CLI_IMAGE_TAG="${BASETAG}${CLI_BASETAG}${CI_PIPELINE_ID}";
-fi
-
-
-./eos-on-k8s/create-all.sh -i ${IMAGE_TAG} -u ${CLI_IMAGE_TAG} -n ${K8S_NAMESPACE} -q -k ${KRB5:-"mit"};
+./eos-on-k8s/create-all.sh -i ${IMAGE_TAG} -u ${CLI_IMAGE_TAG} -n ${K8S_NAMESPACE} -q -k ${KRB5:-"mit"}
