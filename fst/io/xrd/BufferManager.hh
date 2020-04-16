6582693c3148b673eea238e4817eb8d6db89220e
Elvin Sindrilaru <esindril@cern.ch>
2020-04-16 13:52:04 +0200

    FST/UNIT_TEST: Add BufferManager class and corresponding unit tests

---
+++ fst/io/xrd/BufferManager.hh
@@ -0,0 +1,320 @@
+//------------------------------------------------------------------------------
+//! @file BufferManager.hh
+//! @author Elvin-Alin Sindrilaru - CERN
+//------------------------------------------------------------------------------
+
+/************************************************************************
+ * EOS - the CERN Disk Storage System                                   *
+ * Copyright (C) 2020 CERN/Switzerland                                  *
+ *                                                                      *
+ * This program is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ ************************************************************************/
+
+#pragma once
+#include "fst/Namespace.hh"
+#include <memory>
+#include <mutex>
+#include <vector>
+#include <list>
+#include <atomic>
+
+EOSFSTNAMESPACE_BEGIN
+
+//------------------------------------------------------------------------------
+//! Class Buffer
+//------------------------------------------------------------------------------
+class Buffer
+{
+  friend class BufferManager;
+public:
+  //----------------------------------------------------------------------------
+  //! Constructor
+  //----------------------------------------------------------------------------
+  Buffer(uint64_t size):
+    mCapacity(size)
+  {
+    mData.reset(new char(size));
+  }
+
+  //----------------------------------------------------------------------------
+  //! Destructor
+  //----------------------------------------------------------------------------
+  ~Buffer() = default;
+
+  uint64_t mCapacity; ///< Available size of the buffer
+  uint64_t mLength; ///< Length of the useful data
+  std::unique_ptr<char> mData; ///< Buffer holding the data
+};
+
+
+//------------------------------------------------------------------------------
+//! Class BufferSlot
+//------------------------------------------------------------------------------
+class BufferSlot
+{
+  friend class BufferManager;
+public:
+  //----------------------------------------------------------------------------
+  //! Constructor
+  //!
+  //! @param size size of buffers allocated by the current slot
+  //----------------------------------------------------------------------------
+  BufferSlot(uint64_t size):
+    mBuffSize(size), mNumBuffers(0)
+  {}
+
+  //----------------------------------------------------------------------------
+  //! Destructor
+  //----------------------------------------------------------------------------
+  ~BufferSlot()
+  {
+    std::unique_lock<std::mutex> lock(mSlotMutex);
+    mAvailableBuffers.clear();
+  }
+
+  //----------------------------------------------------------------------------
+  //! Move assignment operator
+  //----------------------------------------------------------------------------
+  BufferSlot& operator =(BufferSlot&& other) noexcept
+  {
+    if (this != &other) {
+      mBuffSize = other.mBuffSize;
+      mNumBuffers.store(other.mNumBuffers);
+      mAvailableBuffers = other.mAvailableBuffers;
+      other.mAvailableBuffers.clear();
+    }
+
+    return *this;
+  }
+
+  //----------------------------------------------------------------------------
+  //! Move constructor
+  //----------------------------------------------------------------------------
+  BufferSlot(BufferSlot&& other) noexcept
+  {
+    *this = std::move(other);
+  }
+
+  //----------------------------------------------------------------------------
+  //! Get buffer
+  //----------------------------------------------------------------------------
+  std::shared_ptr<Buffer> GetBuffer()
+  {
+    std::unique_lock<std::mutex> lock(mSlotMutex);
+
+    if (!mAvailableBuffers.empty()) {
+      auto buff =  mAvailableBuffers.front();
+      mAvailableBuffers.pop_front();
+      return buff;
+    }
+
+    ++mNumBuffers;
+    return std::make_shared<Buffer>(mBuffSize);
+  }
+
+  //----------------------------------------------------------------------------
+  //! Recycle buffer object
+  //!
+  //! @param buffer buffer object to be recycled
+  //! @param keep true if buffer is to be saved otherwise false
+  //----------------------------------------------------------------------------
+  void Recycle(std::shared_ptr<Buffer> buffer, bool keep)
+  {
+    if (keep) {
+      std::unique_lock<std::mutex> lock(mSlotMutex);
+      mAvailableBuffers.push_back(buffer);
+    } else {
+      --mNumBuffers;
+    }
+  }
+
+  //----------------------------------------------------------------------------
+  //! Try to pop a buffer from the list of available ones if possible
+  //----------------------------------------------------------------------------
+  void Pop()
+  {
+    std::unique_lock<std::mutex> lock(mSlotMutex);
+
+    if (!mAvailableBuffers.empty()) {
+      mAvailableBuffers.pop_front();
+      --mNumBuffers;
+    }
+  }
+
+private:
+  std::mutex mSlotMutex;
+  std::list<std::shared_ptr<Buffer>> mAvailableBuffers;
+  std::atomic<uint64_t> mNumBuffers;
+  uint64_t mBuffSize;
+};
+
+
+//------------------------------------------------------------------------------
+//! Class BufferManager
+//------------------------------------------------------------------------------
+class BufferManager
+{
+public:
+  //----------------------------------------------------------------------------
+  //! Constructor
+  //!
+  //! @param max_size maximum total size of allocated buffers
+  //! @param slots number of slots for different buffer sizes which are power
+  //!        of 2 and multiple of MB e.g.
+  //!        slot 0 -> 1MB
+  //!        slot 1 -> 2MB
+  //!        slot 2 -> 4MB
+  //----------------------------------------------------------------------------
+  BufferManager(uint64_t max_size = 256 * 1024 * 1024 , uint32_t slots = 2):
+    mMaxSize(max_size), mNumSlots(slots)
+  {
+    for (uint32_t i = 0u; i <= mNumSlots; ++i) {
+      mSlots.emplace_back((1 << i) * 1024 * 1024ull);
+    }
+  }
+
+  //----------------------------------------------------------------------------
+  //! Destructor
+  //----------------------------------------------------------------------------
+  ~BufferManager() = default;
+
+  //----------------------------------------------------------------------------
+  //! Get buffer for the given length
+  //!
+  //! @param size minimum size for requested buffer
+  //!
+  //! @return buffer object
+  //----------------------------------------------------------------------------
+  std::shared_ptr<Buffer> GetBuffer(uint64_t size)
+  {
+    uint32_t slot {UINT32_MAX};
+
+    // Find appropriate slot for the given size
+    for (uint32_t i = 0; i <= mNumSlots; ++i) {
+      if (size <= (1 << (i + 20))) {
+        slot = i;
+        break;
+      }
+    }
+
+    // Can not provide a buffer big enough for the required size
+    if (slot == UINT32_MAX) {
+      return nullptr;
+    }
+
+    return mSlots[slot].GetBuffer();
+  }
+
+  //----------------------------------------------------------------------------
+  //! Recycle buffer object
+  //!
+  //! @param buffer objec to be recycled
+  //----------------------------------------------------------------------------
+  void Recycle(std::shared_ptr<Buffer> buffer)
+  {
+    uint32_t slot {UINT32_MAX};
+
+    // Find appropriate slot for given buffer
+    for (uint32_t i = 0; i <= mNumSlots; ++i) {
+      if (buffer->mCapacity == (1 << (i + 20))) {
+        slot = i;
+        break;
+      }
+    }
+
+    // Not a buffer allocated by us
+    if (slot == UINT32_MAX) {
+      return;
+    }
+
+    uint64_t total_size {0ull};
+    auto sorted_slots = GetSortedSlotSizes(total_size);
+    bool keep = (total_size <= mMaxSize);
+
+    if (!keep) {
+      // Perform clean up for rest of slots depending on their size
+      for (auto it = sorted_slots.rbegin(); it != sorted_slots.rend(); ++it) {
+        if (it->first > slot) {
+          mSlots[it->first].Pop();
+          break;
+        }
+
+        if (it->first < slot) {
+          // Free the equivalent of a block from the current slot
+          int free_blocks = 1 << (slot - it->first);
+
+          while (free_blocks) {
+            mSlots[it->first].Pop();
+            --free_blocks;
+          }
+
+          break;
+        }
+      }
+    }
+
+    mSlots[slot].Recycle(buffer, keep);
+  }
+
+  //----------------------------------------------------------------------------
+  //! Get sorted distribution of slot sizes from smallest to biggest
+  //!
+  //! @param total_size compute the total size allocated so far
+  //!
+  //! @return sorted vector of pairs of slot ids and size of allocated buffers
+  //!         for that corresponding slot
+  //----------------------------------------------------------------------------
+  std::vector< std::pair<uint32_t, uint64_t> >
+  GetSortedSlotSizes(uint64_t& total_size) const
+  {
+    std::vector< std::pair<uint32_t, uint64_t> > elem;
+    total_size = 0ull;
+
+    for (uint32_t i = 0; i <= mNumSlots; ++i) {
+      elem.push_back(std::make_pair(i,
+                                    (mSlots[i].mNumBuffers * (1 << i) * 1024 * 1024)));
+      total_size += elem.rbegin()->second;
+    }
+
+    auto comparator = [](std::pair<uint32_t, uint64_t> a,
+    std::pair<uint32_t, uint64_t> b) {
+      return (a.second < b.second);
+    };
+    std::sort(elem.begin(), elem.end(), comparator);
+    return elem;
+  }
+
+  //----------------------------------------------------------------------------
+  //! Get number of slots handled by the current buffer manager
+  //----------------------------------------------------------------------------
+  uint32_t GetNumSlots() const
+  {
+    return mNumSlots.load();
+  }
+
+  //----------------------------------------------------------------------------
+  //! Get max size of buffers stored by buffer manager
+  //----------------------------------------------------------------------------
+  uint64_t GetMaxSize() const
+  {
+    return mMaxSize.load();
+  }
+
+private:
+  std::atomic<uint64_t> mMaxSize;
+  std::atomic<uint32_t> mNumSlots;
+  std::vector<BufferSlot> mSlots;
+};
+
+EOSFSTNAMESPACE_END
---
+++ unit_tests/fst/BufferManagerTests.cc
@@ -0,0 +1,190 @@
+//------------------------------------------------------------------------------
+//! @file BufferManagerTests.cc
+//! @author Elvin Sindrilaru - CERN
+//------------------------------------------------------------------------------
+
+/************************************************************************
+ * EOS - the CERN Disk Storage System                                   *
+ * Copyright (C) 2020 CERN/Switzerland                                  *
+ *                                                                      *
+ * This program is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ ************************************************************************/
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#define IN_TEST_HARNESS
+#include "fst/io/xrd/BufferManager.hh"
+#undef IN_TEST_HARNESS
+#include "common/StringConversion.hh"
+#include <random>
+#include <thread>
+#include <chrono>
+
+TEST(BufferManager, MatchingSizes)
+{
+  using namespace eos::common;
+  eos::fst::BufferManager buff_mgr(20 * MB);
+  uint64_t buff_sz = 512 * KB;
+  auto buffer = buff_mgr.GetBuffer(buff_sz);
+  ASSERT_NE(buffer, nullptr);
+  ASSERT_EQ(buffer->mCapacity, 1 * MB);
+  buff_sz = 1 * MB;
+  buffer = buff_mgr.GetBuffer(buff_sz);
+  ASSERT_NE(buffer, nullptr);
+  ASSERT_EQ(buffer->mCapacity, 1 * MB);
+  buff_sz = 1;
+  buffer = buff_mgr.GetBuffer(buff_sz);
+  ASSERT_NE(buffer, nullptr);
+  ASSERT_EQ(buffer->mCapacity, 1 * MB);
+  buff_sz = 1 * MB + 22 * KB;
+  buffer = buff_mgr.GetBuffer(buff_sz);
+  ASSERT_NE(buffer, nullptr);
+  ASSERT_EQ(buffer->mCapacity, 2 * MB);
+  buff_sz = 1 * MB + 44 * KB;
+  buffer = buff_mgr.GetBuffer(buff_sz);
+  ASSERT_NE(buffer, nullptr);
+  ASSERT_EQ(buffer->mCapacity, 2 * MB);
+  buff_sz = 3 * MB + 11 * KB;
+  buffer = buff_mgr.GetBuffer(buff_sz);
+  ASSERT_NE(buffer, nullptr);
+  ASSERT_EQ(buffer->mCapacity, 4 * MB);
+  buff_sz = 4 * MB + 33 * KB;
+  buffer = buff_mgr.GetBuffer(buff_sz);
+  ASSERT_EQ(buffer, nullptr);
+  uint64_t total_size {0ull};
+  auto slot_sizes = buff_mgr.GetSortedSlotSizes(total_size);
+  ASSERT_EQ(total_size , 11 * MB);
+  ASSERT_EQ(slot_sizes[0].first, 0);
+  ASSERT_EQ(slot_sizes[0].second, 3 * MB);
+}
+
+TEST(BufferManager, RecycleSingleBuffer)
+{
+  using namespace eos::common;
+  eos::fst::BufferManager buff_mgr(20 * MB);
+
+  for (int i = 0; i < 100; ++i) {
+    auto buffer = buff_mgr.GetBuffer(1 * MB);
+    buff_mgr.Recycle(buffer);
+  }
+
+  uint64_t total_size {0ull};
+  auto sorted_slots = buff_mgr.GetSortedSlotSizes(total_size);
+  ASSERT_EQ(total_size, 1 * MB);
+}
+
+TEST(BufferManager, AdjustCachedSizes)
+{
+  using namespace eos::common;
+  eos::fst::BufferManager buff_mgr(20 * MB);
+  std::list<std::shared_ptr<eos::fst::Buffer>> lst_buffs;
+
+  // Recycle a 1MB blocks
+  for (int i = 0; i < 20; ++i) {
+    lst_buffs.push_back(buff_mgr.GetBuffer(1 * MB));
+    // do some work with the buffer
+    buff_mgr.Recycle(lst_buffs.back());
+    lst_buffs.pop_back();
+  }
+
+  uint64_t total_size {0ull};
+  auto sorted_slots = buff_mgr.GetSortedSlotSizes(total_size);
+  ASSERT_EQ(total_size, 1 * MB);
+
+  // Fill cache with 1MB blocks
+  for (int i = 0; i < 20; ++i) {
+    lst_buffs.push_back(buff_mgr.GetBuffer(1 * MB));
+  }
+
+  for (int i = 0; i < 20; ++i) {
+    buff_mgr.Recycle(lst_buffs.back());
+    lst_buffs.pop_back();
+  }
+
+  sorted_slots = buff_mgr.GetSortedSlotSizes(total_size);
+  ASSERT_EQ(total_size, 20 * MB);
+  auto buffer = buff_mgr.GetBuffer(3 * MB);
+  ASSERT_NE(buffer, nullptr);
+  ASSERT_EQ(buffer->mCapacity, 4 * MB);
+  buff_mgr.Recycle(buffer);
+  sorted_slots = buff_mgr.GetSortedSlotSizes(total_size);
+  ASSERT_EQ(total_size, 16 * MB);
+}
+
+TEST(BufferManager, MultipleThreads)
+{
+  using namespace eos::common;
+  auto work = [](eos::fst::BufferManager & buff_mgr, int num_blocks,
+  float mean, float stddev) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::normal_distribution<> dis(mean, stddev);
+
+    for (int i = 0; i < num_blocks; ++i) {
+      uint64_t max_buff_sz = (1 << buff_mgr.GetNumSlots()) * MB;
+      uint64_t value = std::round(std::abs(dis(gen)));
+
+      // Make sure the generated values is withing the
+      if (value <= 0) {
+        value = 1;
+      } else if (value > max_buff_sz) {
+        value = max_buff_sz;
+      }
+
+      auto buffer = buff_mgr.GetBuffer(value);
+      ASSERT_NE(buffer, nullptr);
+      std::this_thread::sleep_for(std::chrono::milliseconds(50));
+      buff_mgr.Recycle(buffer);
+    }
+  };
+  int num_threads = 32;
+  int num_blocks = 24;
+  std::list<std::thread> lst_threads;
+  std::list<std::pair<float, float>> uniform_dist_params;
+  uniform_dist_params.emplace_back(500 * KB, 200 * KB);
+  uniform_dist_params.emplace_back(1500 * KB, 200 * KB);
+  uniform_dist_params.emplace_back(3500 * KB, 500 * KB);
+
+  for (auto& dis_params : uniform_dist_params) {
+    eos::fst::BufferManager buff_mgr(100 * MB);
+
+    for (int i = 0; i < num_threads; ++i) {
+      lst_threads.emplace_back(work, std::ref(buff_mgr), num_blocks,
+                               dis_params.first, dis_params.second);
+    }
+
+    for (auto& job : lst_threads) {
+      job.join();
+    }
+
+    lst_threads.clear();
+    uint64_t total_size {0ull};
+    auto sorted_slots = buff_mgr.GetSortedSlotSizes(total_size);
+    ASSERT_LE(total_size, buff_mgr.GetMaxSize());
+    // Get the most used slot according to the distribution mean
+    uint32_t slot {UINT32_MAX};
+
+    for (int i = 0; i <= buff_mgr.GetNumSlots(); ++i) {
+      if (dis_params.first <= (1 << (i + 20))) {
+        slot = i;
+      }
+    }
+
+    // The slot with the largest allocated size should be "slot"
+    ASSERT_EQ(sorted_slots.rbegin()->first, slot);
+    // for (auto& elem: sorted_slots) {
+    //   std::cout << "Slot: " << elem.first << " size: " << elem.second << std::endl;
+    // }
+  }
+}
