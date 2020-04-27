76ec644f1278568fb0178cb000137f6402aebd5a
Andreas Joachim Peters <andreas.joachim.peters@cern.ch>
2020-04-27 14:00:49 +0200

    CONSOLE: add new 'status' command file

---
+++ console/commands/com_status.cc
@@ -0,0 +1,36 @@
+// ----------------------------------------------------------------------
+// File: com_status.cc
+// Author: Andreas-Joachim Peters - CERN
+// ----------------------------------------------------------------------
+
+/************************************************************************
+ * EOS - the CERN Disk Storage System                                   *
+ * Copyright (C) 2020 CERN/Switzerland                                  *
+ *                                                                      *
+ * This program is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ ************************************************************************/
+
+/*----------------------------------------------------------------------------*/
+#include "console/ConsoleMain.hh"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+/*----------------------------------------------------------------------------*/
+
+int
+com_status (char*)
+{
+  system("eos-status");
+  return (0);
+}
