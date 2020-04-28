e3033a9879097e482335da4694f5b25dca294489
Fabio Luchetti <fabio.luchetti@cern.ch>
2020-04-28 18:50:12 +0200

    CI: Eliminate hanging problem in eosxd execution and auth eos-user via keytab in cbox_test; Move logs of failing tests to eos-logs-$CI_JOB_ID

--- gitlab-ci/after_script_docker_test.sh
+++ gitlab-ci/after_script_docker_test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -ve
 
-./eos-docker/scripts/collect_logs.sh eos-logs/
+./eos-docker/scripts/collect_logs.sh eos-logs-${CI_JOB_ID}/
 sudo ./eos-docker/scripts/shutdown_services.sh
 
+# remove unused docker resources -> be it on the runner itself, here shutdown_services.sh should be enough
 #docker system prune --all --filter until=30m --force # @note will exploit flag when docker is updated
-docker system prune --force # remove unused docker resources
+#docker system prune --force # remove unused docker resources
--- gitlab-ci/after_script_k8s_test.sh
+++ gitlab-ci/after_script_k8s_test.sh
@@ -2,6 +2,6 @@
 
 source /home/gitlab-runner/coe-cluster-config/st-gitlab-k8s-02/env_st-gitlab-k8s-02.sh # get access configs for the cluster
 export K8S_NAMESPACE=$(echo ${CI_JOB_NAME}-${CI_JOB_ID}-${CI_PIPELINE_ID} | tr '_' '-' | tr '[:upper:]' '[:lower:]')
-./eos-on-k8s/collect_logs.sh ${K8S_NAMESPACE} eos-logs-${K8S_NAMESPACE}/
+./eos-on-k8s/collect_logs.sh ${K8S_NAMESPACE} eos-logs-${CI_JOB_ID}/
 ./eos-on-k8s/delete-all.sh ${K8S_NAMESPACE}
 rm -rf eos-on-k8s/
