c5d2c65cf7a62b7187582945e263137aae1bcdfb
Georgios Bitzes <georgios.bitzes@cern.ch>
2020-05-07 11:38:16 +0000

    TEST: Add executable for tests requiring a running QDB instance

---
+++ unit_tests/with_qdb/Main.cc
@@ -0,0 +1,29 @@
+/************************************************************************
+ * EOS - the CERN Disk Storage System                                   *
+ * Copyright (C) 2020 CERN/Switzerland                                  *
+ *                                                                      *
+ * This program is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ ************************************************************************/
+
+//------------------------------------------------------------------------------
+//! @author Georgios Bitzes <georgios.bitzes@cern.ch>
+//! @brief Main.cc, test initialization
+//------------------------------------------------------------------------------
+
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
