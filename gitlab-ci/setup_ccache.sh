c910eeddf77ab6ffa330954d58bf871c0eb56fd8
Fabio Luchetti <fabio.luchetti@cern.ch>
2020-05-23 04:08:07 +0200

    CI: Push updated prebuild images (stable and latest), caching disabled; Increase a bit the timer in fsck test

--- gitlab-ci/setup_ccache.sh
+++ gitlab-ci/setup_ccache.sh
@@ -6,7 +6,7 @@ export CCACHE_DIR="`pwd`/ccache"
 export CCACHE_BASEDIR="`rpm --eval %_builddir`/`ls build/SRPMS/*.src.rpm | awk -F '.' 'BEGIN{OFS=".";} {NF-=4;}1' | awk -F '/' '{print $NF}'`"
 export CCACHE_SLOPPINESS=pch_defines
 export CCACHE_NOHASHDIR=true
-export CCACHE_MAXSIZE=3G
+export CCACHE_MAXSIZE=4G
 
 ccache -z
 ccache -p
