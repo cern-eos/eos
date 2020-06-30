2e2d0648fa545d454a9087c7d16026a2a5400a7d
Elvin Sindrilaru <esindril@cern.ch>
2020-06-30 23:49:02 +0200

    CONSOLE: Add json output for "who" command. Fixes EOS-4293

--- mgm/proc/user/Who.cc
+++ mgm/proc/user/Who.cc
@@ -24,6 +24,7 @@
 #include "mgm/proc/ProcInterface.hh"
 #include "mgm/XrdMgmOfs.hh"
 #include "mgm/Stat.hh"
+#include <json/json.h>
 
 EOSMGMNAMESPACE_BEGIN
 
@@ -35,15 +36,25 @@ ProcCommand::Who()
   std::map<std::string, int> authcount;
   std::vector<std::string> tokens;
   std::string delimiter = "^";
-  std::string option = (pOpaque->Get("mgm.option")) ? pOpaque->Get("mgm.option") :
-                       "";
+  std::string option;
+  bool json_format = false;
   bool monitoring = false;
-  //      bool translate  = true;
   bool showclients = false;
   bool showall = false;
   bool showauth = false;
   bool showsummary = false;
-  // call the expiration functions
+  Json::Value json;
+
+  if (pOpaque->Get("mgm.option")) {
+    option = pOpaque->Get("mgm.option");
+  }
+
+  if (pOpaque->Get("mgm.format")) {
+    std::string format = pOpaque->Get("mgm.format");
+    json_format = (format == "json");
+  }
+
+  // Call the expiration functions
   eos::common::Mapping::ActiveLock.Lock();
   eos::common::Mapping::ActiveExpire(300, true);
   google::dense_hash_map<std::string, time_t>::const_iterator it;
@@ -89,15 +100,20 @@ ProcCommand::Who()
     for (it = authcount.begin(); it != authcount.end(); it++) {
       char formatline[1024];
 
-      if (!monitoring) {
-        snprintf(formatline, sizeof(formatline) - 1, "auth   : %-24s := %d sessions\n",
-                 it->first.c_str(), it->second);
-      } else {
+      if (monitoring) {
         snprintf(formatline, sizeof(formatline) - 1, "auth=%s nsessions=%d\n",
                  it->first.c_str(), it->second);
+        stdOut += formatline;
+      } else if (json_format) {
+        Json::Value json_auth;
+        json_auth["auth"] = it->first;
+        json_auth["nsessions"] = it->second;
+        json.append(json_auth);
+      } else {
+        snprintf(formatline, sizeof(formatline) - 1, "auth   : %-24s := %d sessions\n",
+                 it->first.c_str(), it->second);
+        stdOut += formatline;
       }
-
-      stdOut += formatline;
     }
   }
 
@@ -109,15 +125,20 @@ ProcCommand::Who()
          ituname++) {
       char formatline[1024];
 
-      if (!monitoring) {
-        snprintf(formatline, sizeof(formatline) - 1, "user   : %-24s := %d sessions\n",
-                 ituname->first.c_str(), ituname->second);
-      } else {
+      if (monitoring) {
         snprintf(formatline, sizeof(formatline) - 1, "uid=%s nsessions=%d\n",
                  ituname->first.c_str(), ituname->second);
+        stdOut += formatline;
+      } else if (json_format) {
+        Json::Value json_user;
+        json_user["uid"] = ituname->first;
+        json_user["nsessions"] = ituname->second;
+        json.append(json_user);
+      } else {
+        snprintf(formatline, sizeof(formatline) - 1, "user   : %-24s := %d sessions\n",
+                 ituname->first.c_str(), ituname->second);
+        stdOut += formatline;
       }
-
-      stdOut += formatline;
     }
   }
 
@@ -138,22 +159,33 @@ ProcCommand::Who()
       char formatline[1024];
       time_t now = time(NULL);
 
-      if (!monitoring) {
+      if (monitoring) {
+        snprintf(formatline, sizeof(formatline) - 1,
+                 "client=%s uid=%s auth=%s idle=%ld gateway=\"%s\" app=%s\n", tokens[1].c_str(),
+                 username.c_str(), tokens[2].c_str(), now - it->second, tokens[3].c_str(),
+                 (tokens.size() > 4) ? tokens[4].c_str() : "XRoot");
+        stdOut += formatline;
+      } else if (json_format) {
+        if (!showsummary) {
+          Json::Value json_client;
+          json_client["client"] = tokens[1];
+          json_client["uid"] = username;
+          json_client["auth"] = tokens[2];
+          json_client["idle"] = (Json::UInt64)(now - it->second);
+          json_client["gateway"] = tokens[3];
+          json_client["app"] = (tokens.size() > 4) ? tokens[4].c_str() : "XRoot";
+          json.append(json_client);
+        }
+      } else {
         snprintf(formatline, sizeof(formatline) - 1,
                  "client : %-10s               := %-40s (%5s) [ %-40s ] { %-8s } %lds idle time \n",
                  username.c_str(), tokens[1].c_str(), tokens[2].c_str(), tokens[3].c_str(),
                  ((tokens.size() > 4) &&
                   tokens[4].length()) ? tokens[4].c_str() : "XRoot", now - it->second);
-      } else {
-        snprintf(formatline, sizeof(formatline) - 1,
-                 "client=%s uid=%s auth=%s idle=%ld gateway=\"%s\" app=%s\n", tokens[1].c_str(),
-                 username.c_str(), tokens[2].c_str(), now - it->second, tokens[3].c_str(),
-                 (tokens.size() > 4) ? tokens[4].c_str() : "XRoot");
-      }
 
-      if (showsummary && (!monitoring)) {
-      } else {
-        stdOut += formatline;
+        if (!showsummary) {
+          stdOut += formatline;
+        }
       }
     }
   }
@@ -163,13 +195,22 @@ ProcCommand::Who()
   if (showsummary) {
     char formatline[1024];
 
-    if (!monitoring) {
-      snprintf(formatline, sizeof(formatline) - 1, "sum(clients) : %llu\n", cnt);
-    } else {
+    if (monitoring) {
       snprintf(formatline, sizeof(formatline) - 1, "nclients=%llu\n", cnt);
+      stdOut += formatline;
+    } else if (json_format) {
+      Json::Value json_count;
+      json_count["nclients"] = Json::UInt64(cnt);
+      json.append(json_count);
+    } else {
+      snprintf(formatline, sizeof(formatline) - 1, "sum(clients) : %llu\n", cnt);
+      stdOut += formatline;
     }
+  }
 
-    stdOut += formatline;
+  if (json_format) {
+    stdOut = "";
+    stdJson += Json::StyledWriter().write(json).c_str();
   }
 
   return SFS_OK;
