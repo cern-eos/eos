c389a79edaf01e8f8b2128796c704591ede3324e
Elvin Sindrilaru <esindril@cern.ch>
2020-09-08 15:13:36 +0200

    MGM: Fix wrong condition due to operator precedence

--- mgm/XrdMgmOfs/fsctl/Commit.cc
+++ mgm/XrdMgmOfs/fsctl/Commit.cc
@@ -163,19 +163,18 @@ XrdMgmOfs::Commit(const char* path,
                     "commit filesize change - file is already removed [EIDRM]", "");
       }
 
-
       // Check if we have this replica in the unlink list or not linked list, if yes, the commit has to be suppressed
       if (option["fusex"] &&
-	  fmd->hasUnlinkedLocation((unsigned int) fsid) ||
-	  (!fmd->hasLocation((unsigned int) fsid)) ) {
-	eos_thread_err("suppressing possible recovery replica for fxid=%08llx "
-		       "on unlinked/not linked fsid=%llu - rejecting replica",
-		       fmd->getId(), fsid);
-	// This happens when a FUSEX recovery has been triggered.
-	// To avoid to reattach replicas, we clean them up here
-	return Emsg(epname, error, EBADE,
-		    "commit replica - file size is wrong [EBADE] "
-		    "- suppressing recovery replica", "");
+          (fmd->hasUnlinkedLocation((unsigned int) fsid) ||
+           (!fmd->hasLocation((unsigned int) fsid)))) {
+        eos_thread_err("suppressing possible recovery replica for fxid=%08llx "
+                       "on unlinked/not linked fsid=%llu - rejecting replica",
+                       fmd->getId(), fsid);
+        // This happens when a FUSEX recovery has been triggered.
+        // To avoid to reattach replicas, we clean them up here
+        return Emsg(epname, error, EBADE,
+                    "commit replica - file size is wrong [EBADE] "
+                    "- suppressing recovery replica", "");
       }
 
       // Check if commit comes from a replication procedure
