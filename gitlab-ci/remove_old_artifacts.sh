be6722e860b0cc68c171b38d7e8d82b4777c4729
Fabio Luchetti <fabio.luchetti@cern.ch>
2020-09-16 16:55:58 +0200

    CI: On clean, delete also commit tarball artifacts older than 10 days

--- gitlab-ci/remove_old_artifacts.sh
+++ gitlab-ci/remove_old_artifacts.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 AGE_DAYS=10
-EOS_DIRS=/eos/project/s/storage-ci/www/eos/citrine/commit
+COMMIT_DIRS=/eos/project/s/storage-ci/www/eos/citrine/commit
+TARBALL_DIR=/eos/project/s/storage-ci/www/eos/citrine/tarball
 
-for commit_dir in ${EOS_DIRS}/*; do
+for commit_dir in ${COMMIT_DIRS}/*; do
   count=$(find ${commit_dir} -regex '.*\(rpm\|dmg\)$' -type f -mtime +${AGE_DAYS} -print -delete | wc -l)
 
   if [[ ${count} -gt 0 ]]; then
@@ -17,4 +18,12 @@ for commit_dir in ${EOS_DIRS}/*; do
       createrepo --update -q ${repo_dir}
     done
   fi
+done
+
+# Delete commit tarballs older than AGE_DAYS; @note Do not delete tag tarballs!
+for tarball in ${TARBALL_DIR}/*; do
+  if [[ $(find $tarball -mtime +${AGE_DAYS}) ]] && [[ $tarball =~ git[a-zA-Z0-9]{7}".tar.gz" ]]; then
+    rm $tarball && echo "tarball deleted: $tarball"
+#    echo "$tarball"
+  fi;
 done
