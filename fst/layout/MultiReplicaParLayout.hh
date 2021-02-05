f9b483a7265249ac4e8a3269061c5375e4b67ba6
Andreas Stoeve <andreas.stoeve@cern.ch>
2021-02-05 15:47:55 +0100

    New file for multi replicas

---
+++ fst/layout/MultiReplicaParLayout.hh
@@ -0,0 +1,22 @@
+
+
+
+#ifndef __EOSFST_MultiReplicaParLayout_HH__
+#define __EOSFST_MultiReplicaParLayout_HH__
+
+#include "fst/layout/Layout.hh"
+
+EOSFSTNAMESPACE_BEGIN
+
+class MultiReplicaParLayout : public Layout
+{
+public:
+
+private:
+
+
+};
+
+EOSFSTNAMESPACE_END
+
+#endif // __EOSFST_MULTIREPLICAPARLAYOUT_HH__
