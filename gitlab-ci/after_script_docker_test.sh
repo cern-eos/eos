0254c1b2597b2851b21624d917653297b92168af
Fabio Luchetti <fabio.luchetti@cern.ch>
2021-02-17 15:35:58 +0100

    CI: no 'sudo' for DinD environment

--- gitlab-ci/after_script_docker_test.sh
+++ gitlab-ci/after_script_docker_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ve
 
 ./eos-docker/scripts/collect_logs.sh eos-logs-${CI_JOB_ID}/
-sudo ./eos-docker/scripts/shutdown_services.sh
+./eos-docker/scripts/shutdown_services.sh
 
 # remove unused docker resources -> be it on the runner itself, here shutdown_services.sh should be enough
 #docker system prune --all --filter until=30m --force # @note will exploit flag when docker is updated
--- gitlab-ci/before_script_docker_test.sh
+++ gitlab-ci/before_script_docker_test.sh
@@ -1,13 +1,10 @@
 #!/bin/bash -ve
 
-sudo ./eos-docker/scripts/shutdown_services.sh
+./eos-docker/scripts/shutdown_services.sh
 ./eos-docker/scripts/remove_unused_images.sh
 
 # either $CI_COMMIT_TAG either $CI_COMMIT_SHORT_SHA
 export IMAGE_TAG="${BASETAG}${CI_COMMIT_TAG:-$CI_COMMIT_SHORT_SHA}"
 export CLI_IMAGE_TAG="${BASETAG}${CLI_BASETAG}${CI_COMMIT_TAG:-$CI_COMMIT_SHORT_SHA}"
 
-docker pull gitlab-registry.cern.ch/dss/eos:${IMAGE_TAG}
-docker pull gitlab-registry.cern.ch/dss/eos:${CLI_IMAGE_TAG}
-
-sudo ./eos-docker/scripts/start_services.sh -q -i gitlab-registry.cern.ch/dss/eos:${IMAGE_TAG} -u gitlab-registry.cern.ch/dss/eos:${CLI_IMAGE_TAG} -k ${KRB5:-"mit"}
+./eos-docker/scripts/start_services.sh -q -i gitlab-registry.cern.ch/dss/eos:${IMAGE_TAG} -u gitlab-registry.cern.ch/dss/eos:${CLI_IMAGE_TAG} -k ${KRB5:-"mit"}
