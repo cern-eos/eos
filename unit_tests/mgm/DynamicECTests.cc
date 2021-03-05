5557ecdb991ffc900fe5747fd454cd0d0316ac5c
Andreas Stoeve <andreas.stoeve@cern.ch>
2021-03-05 10:51:34 +0100

    Commit all

--- unit_tests/mgm/DynamicECTests.cc
+++ unit_tests/mgm/DynamicECTests.cc
@@ -1,27 +1,26 @@
+//------------------------------------------------------------------------------
+// File: DynamicECTests.cc
+// Author: Andreas Stoeve Cern
+//------------------------------------------------------------------------------
 
-	//------------------------------------------------------------------------------
-	// File: DynamicECTests.cc
-	// Author: Andreas Stoeve Cern
-	//------------------------------------------------------------------------------
-
-	/************************************************************************
-	 * EOS - the CERN Disk Storage System                                   *
-	 * Copyright (C) 2018 CERN/Switzerland                                  *
-	 *                                                                      *
-	 * This program is free software: you can redistribute it and/or modify *
-	 * it under the terms of the GNU General Public License as published by *
-	 * the Free Software Foundation, either version 3 of the License, or    *
-	 * (at your option) any later version.                                  *
-	 *                                                                      *
-	 * This program is distributed in the hope that it will be useful,      *
-	 * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
-	 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
-	 * GNU General Public License for more details.                         *
-	 *                                                                      *
-	 * You should have received a copy of the GNU General Public License    *
-	 * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
-	 ************************************************************************/
-	//test
+/************************************************************************
+ * EOS - the CERN Disk Storage System                                   *
+ * Copyright (C) 2018 CERN/Switzerland                                  *
+ *                                                                      *
+ * This program is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ ************************************************************************/
+//test
 #include "gtest/gtest.h"
 #include "mgm/dynamicec/DynamicEC.hh"
 #include "common/LayoutId.hh"
