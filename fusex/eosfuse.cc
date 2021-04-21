6c53c30b29af1e3ee2471cc58199c1142623d2dc
Elvin Sindrilaru <esindril@cern.ch>
2021-04-21 13:05:08 +0200

    FUSE: Fix compilation warning about missing return value

--- fusex/eosfuse.cc
+++ fusex/eosfuse.cc
@@ -1807,6 +1807,7 @@ EosFuse::run(int argc, char* argv[], void* userdata)
   }
 
   eos::common::Logging::GetInstance().shutDown(true);
+  return 0;
 }
 
 /* -------------------------------------------------------------------------- */
@@ -5635,7 +5636,8 @@ EosFuse::readlink(fuse_req_t req, fuse_ino_t ino)
 
   if (!md->id() || md->deleted()) {
     rc = md->deleted() ? ENOENT : md->err();
-    if ( rc == EPERM) {
+
+    if (rc == EPERM) {
       rc = EACCES;
     }
   } else {
