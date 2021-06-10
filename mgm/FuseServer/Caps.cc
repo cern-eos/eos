3edde51f71568356a200bb231a1b50fcb13e310a
Abhishek L <abhishek.l@cern.ch>
2021-06-10 10:10:05 +0200

    mgm: caps: BroadcastMD fix logging when refcap can be potentially null
    Since we use the unsafe variant of Get, refcap could be a nullptr, so check
    before dereferencing in the log message.
    
    Signed-off-by: Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>

--- mgm/FuseServer/Caps.cc
+++ mgm/FuseServer/Caps.cc
@@ -474,6 +474,7 @@ FuseServer::Caps::BroadcastMD(const eos::fusex::md& md,
     }
 
     auto kv = mInodeCaps.find(md_pino);
+
     if (kv == mInodeCaps.end()) {
       EXEC_TIMING_END("Eosxd::int::BcMD");
       return 0; // nothing to process here
