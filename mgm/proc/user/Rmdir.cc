5246b8167bc1c9acea9b6551ecdb3fb633486c4b
Andreas Joachim Peters <andreas.joachim.peters@cern.ch>
2021-06-17 08:43:06 +0200

    MGM: make 'xrdfs ls/rmdir/mkdir' work with tokens and owner-less tokens with CLI - don't allow to delete root directory of a token scoe

--- mgm/proc/user/Rmdir.cc
+++ mgm/proc/user/Rmdir.cc
@@ -34,7 +34,7 @@ ProcCommand::Rmdir()
   XrdOucString spath = pOpaque->Get("mgm.path");
   const char* inpath = spath.c_str();
   NAMESPACEMAP;
-  NAMESPACE_NO_TRALING_SLASH;
+  NAMESPACE_NO_TRAILING_SLASH;
   PROC_BOUNCE_ILLEGAL_NAMES;
   PROC_BOUNCE_NOT_ALLOWED;
   spath = path;
