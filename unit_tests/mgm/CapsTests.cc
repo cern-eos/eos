9c376047b4094d849e5fe9b3ddc994de7b7ae619
Abhishek L <abhishek.l@cern.ch>
2021-06-18 15:14:02 +0200

    unit_tests: Caps: add some basic ImplyCaps tests
    A simple sanity test and a multi threaded test that tries to imply possibly
    deleted caps is added
    
    Signed-off-by: Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>

--- unit_tests/mgm/CapsTests.cc
+++ unit_tests/mgm/CapsTests.cc
@@ -649,7 +649,6 @@ TEST_F(CapsTest, ImplyCaps)
 TEST_F(CapsTest, ImplyCapsMulti)
 {
   int limit = 10000;
-
   for (int i=0; i < limit; i++){
     auto vid = make_vid(i,i);
     mCaps.Store(make_cap(i,"client"+std::to_string(i),"auth"+std::to_string(i)),&vid);
