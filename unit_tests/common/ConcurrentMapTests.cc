d420d69f5be2614922f505ac6762f9bb1d0fc551
Abhishek L <abhishek.l@cern.ch>
2021-06-21 11:40:18 +0200

    unit_tests: use a template for conc_map and test a basic algorithm range method
    Signed-off-by: Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>

--- unit_tests/common/ConcurrentMapTests.cc
+++ unit_tests/common/ConcurrentMapTests.cc
@@ -23,17 +23,36 @@
 #include "gtest/gtest.h"
 #include "common/concurrent_map/concurrent_map_adapter.hh"
 #include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+
+template <class K, class V>
+using std_concurrent_map = eos::common::concurrent_map_adapter<std::unordered_map<K,V>>;
 
 TEST(ConcurrentMap, Basic)
 {
-  eos::common::concurrent_map_adapter<std::unordered_map<int,std::string>> cm;
+  std_concurrent_map<int,std::string> cm;
   for (int i=0;i<100;i++){
     cm.emplace(i,"value"+std::to_string(i));
   }
+  // Test iterator & std::algo basic
+  std::vector <int> expected_keys, actual_keys;
+  expected_keys.resize(100);
+  std::iota(expected_keys.begin(), expected_keys.end(),0);
+  std::transform(cm.begin(),
+                 cm.end(),
+                 std::back_inserter(actual_keys),
+                 [](const auto& kv) { return kv.first; });
+  std::sort(actual_keys.begin(),actual_keys.end());
+
+
+  EXPECT_EQ(expected_keys, actual_keys);
 
   auto kv = cm.find(10);
   EXPECT_EQ(kv->second,"value10");
   EXPECT_TRUE(cm.erase(10));
   const auto k = cm.find(10);
   EXPECT_EQ(k, cm.end());
+
+
 }
