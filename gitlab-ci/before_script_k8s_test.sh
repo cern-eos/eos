ec1acae19dbbb27ef7bf277e446ab11fc50d55bb
Fabio Luchetti <fabio.luchetti@cern.ch>
2021-07-06 17:52:41 +0200

    CI: Add jobs to publish official eos docker images; Make the docker image base repository name configurable and exploit this in the test deploys

--- gitlab-ci/before_script_k8s_test.sh
+++ gitlab-ci/before_script_k8s_test.sh
@@ -4,8 +4,9 @@ export KUBECONFIG=$K8S_CONFIG # get access configs for the cluster
 git clone https://gitlab.cern.ch/eos/eos-on-k8s.git
 export K8S_NAMESPACE=$(echo ${CI_JOB_NAME}-${CI_JOB_ID}-${CI_PIPELINE_ID} | tr '_' '-' | tr '[:upper:]' '[:lower:]')
 
+export IMAGE_REPO="gitlab-registry.cern.ch/dss/eos/eos-ci"
 # either $CI_COMMIT_TAG either $CI_COMMIT_SHORT_SHA
 export IMAGE_TAG="${BASETAG}${CI_COMMIT_TAG:-$CI_COMMIT_SHORT_SHA}"
 export CLI_IMAGE_TAG="${BASETAG}${CLI_BASETAG}${CI_COMMIT_TAG:-$CI_COMMIT_SHORT_SHA}"
 
-./eos-on-k8s/create-all.sh -i ${IMAGE_TAG} -u ${CLI_IMAGE_TAG} -n ${K8S_NAMESPACE} -q -k ${KRB5:-"mit"}
+./eos-on-k8s/create-all.sh -b ${IMAGE_REPO} -i ${IMAGE_TAG} -u ${CLI_IMAGE_TAG} -n ${K8S_NAMESPACE} -q -k ${KRB5:-"mit"}
