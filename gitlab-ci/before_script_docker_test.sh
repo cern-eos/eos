93d6a7469f071ef3cb854b913974bdcb126f6884
Fabio Luchetti <fabio.luchetti@cern.ch>
2021-07-06 17:52:41 +0200

    CI: Add jobs to publish official eos docker images; Make the docker image base repository name configurable and exploit this in the test deploys

--- gitlab-ci/before_script_docker_test.sh
+++ gitlab-ci/before_script_docker_test.sh
@@ -3,8 +3,9 @@
 ./eos-docker/scripts/shutdown_services.sh
 ./eos-docker/scripts/remove_unused_images.sh
 
+export IMAGE_REPO="gitlab-registry.cern.ch/dss/eos/eos-ci"
 # either $CI_COMMIT_TAG either $CI_COMMIT_SHORT_SHA
 export IMAGE_TAG="${BASETAG}${CI_COMMIT_TAG:-$CI_COMMIT_SHORT_SHA}"
 export CLI_IMAGE_TAG="${BASETAG}${CLI_BASETAG}${CI_COMMIT_TAG:-$CI_COMMIT_SHORT_SHA}"
 
-./eos-docker/scripts/start_services.sh -q -i gitlab-registry.cern.ch/dss/eos:${IMAGE_TAG} -u gitlab-registry.cern.ch/dss/eos:${CLI_IMAGE_TAG} -k ${KRB5:-"mit"}
+./eos-docker/scripts/start_services.sh -q -i ${IMAGE_REPO}:${IMAGE_TAG} -u ${IMAGE_REPO}:${CLI_IMAGE_TAG} -k ${KRB5:-"mit"}
