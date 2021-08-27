7ad098cdb4a25e81b32102ebea98a640fcf3b576
Cedric Caffy <cedric.caffy@cern.ch>
2021-08-27 18:05:15 +0200

    [MGM] RestAPI skeleton WIP

---
+++ mgm/http/rest-api/resources/ResourceFactory.hh
@@ -0,0 +1,42 @@
+// ----------------------------------------------------------------------
+// File: ResourceFactory.hh
+// Author: Cedric Caffy - CERN
+// ----------------------------------------------------------------------
+
+/************************************************************************
+ * EOS - the CERN Disk Storage System                                   *
+ * Copyright (C) 2013 CERN/Switzerland                                  *
+ *                                                                      *
+ * This program is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ ************************************************************************/
+
+#ifndef EOS_RESOURCEFACTORY_HH
+#define EOS_RESOURCEFACTORY_HH
+
+#include "mgm/Namespace.hh"
+#include "mgm/http/rest-api/resources/Resource.hh"
+#include <string>
+
+EOSMGMNAMESPACE_BEGIN
+
+class ResourceFactory {
+public:
+  Resource * createResource(const std::string & resourceName);
+private:
+
+};
+
+EOSMGMNAMESPACE_END
+
+#endif // EOS_RESOURCEFACTORY_HH
