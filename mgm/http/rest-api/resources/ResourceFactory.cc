8cdd4ba2c0af2992960108a95fea73c4192b1488
Cedric Caffy <cedric.caffy@cern.ch>
2021-08-27 18:05:15 +0200

    [MGM] RestAPI skeleton WIP

---
+++ mgm/http/rest-api/resources/ResourceFactory.cc
@@ -0,0 +1,32 @@
+// ----------------------------------------------------------------------
+// File: ResourceFactory.cc
+// Author: Cedric Caffy - CERN
+// ----------------------------------------------------------------------
+
+/************************************************************************
+ * EOS - the CERN Disk Storage System                                   *
+ * Copyright (C) 2013 CERN/Switzerland                                  *
+ *                                                                      *
+ * This program is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ ************************************************************************/
+
+#include "ResourceFactory.hh"
+
+EOSMGMNAMESPACE_BEGIN
+
+Resource * ResourceFactory::createResource(const std::string & resourceName){
+ return nullptr;
+}
+
+EOSMGMNAMESPACE_END
