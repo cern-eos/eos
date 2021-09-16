41c6deb62b1e1ea02a23ee69da37ff8aad309506
Elvin Sindrilaru <esindril@cern.ch>
2021-09-16 11:00:25 +0200

    UTILS: Update list of packages for EL7 installation help script

--- utils/el7-packages.sh
+++ utils/el7-packages.sh
@@ -12,6 +12,8 @@ yum install -y cmake3 sparsehash-devel \
     xfsprogs xfsprogs-devel e2fsprogs-devel perl-Time-HiRes json-c json-c-devel \
     jsoncpp jsoncpp-devel libcurl libcurl-devel \
     libevent libevent-devel bzip2-devel bzip2-libs jemalloc jemalloc-devel \
-    eos-rocksdb devtoolset-8 gtest binutils-devel eos-folly libmacaroons libmacaroons-devel
+    eos-rocksdb devtoolset-8 gtest gtest-devel binutils-devel eos-folly \
+    libmacaroons libmacaroons-devel scitokens-cpp scitoeksn-cpp-devel \
+    grpc grpc-devel grpc-plugins libatomic
 
 source /opt/rh/devtoolset-8/enable
