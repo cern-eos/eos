94eaea4a8681ca7846b73aa4e4a35cc632d99f43
Elvin Sindrilaru <esindril@cern.ch>
2021-10-21 23:43:19 +0200

    CMAKE: Use only the eos module to find the correct libprotobuf library

--- cmake/FindProtobuf3.cmake
+++ cmake/FindProtobuf3.cmake
@@ -1,9 +1,9 @@
 # Try to find PROTOBUF3
 # Once done, this will define
 #
-# PROTOBUF_FOUND               - system has PROTOBUF
-# PROTOBUF_INCLUDE_DIRs        - Protobuf include directories
-# PROTOBUF_LIBRARIES           - libraries needed to use Protobuf
+# PROTOBUF3_FOUND               - system has Protobuf3
+# PROTOBUF3_INCLUDE_DIRS        - Protobuf3 include directories
+# PROTOBUF3_LIBRARIES           - libraries needed to use Protobuf3
 #
 # and the following imported targets
 #
@@ -19,8 +19,8 @@ find_program(PROTOBUF_PROTOC_EXECUTABLE
   DOC "Version 3 of The Google Protocol Buffers Compiler"
   NO_DEFAULT_PATH)
 
-if (PROTOBUF_PROTOC_EXECUTABLE)
-  message(STATUS "Found protoc: ${PROTOBUF_PROTOC_EXECUTABLE}")
+if (PROTOBUF3_PROTOC_EXECUTABLE)
+  message(STATUS "Found protoc: ${PROTOBUF3_PROTOC_EXECUTABLE}")
 else()
   message(STATUS "Trying to search for protoc3 instead for protoc")
   unset(PROTOBUF_PROTOC_EXECUTABLE)
@@ -33,7 +33,7 @@ else()
     NO_DEFAULT_PATH)
 endif()
 
-find_path(PROTOBUF_INCLUDE_DIR
+find_path(PROTOBUF3_INCLUDE_DIR
   NAMES google/protobuf/message.h
   HINTS ${PROTOBUF_ROOT}
   PATHS /opt/eos/grpc /opt/eos/include/protobuf3 /usr/include/protobuf3
@@ -41,7 +41,7 @@ find_path(PROTOBUF_INCLUDE_DIR
   PATH_SUFFIXES include
   NO_DEFAULT_PATH)
 
-find_library(PROTOBUF_LIBRARY
+find_library(PROTOBUF3_LIBRARY
   NAME protobuf
   HINTS ${PROTOBUF_ROOT}
   PATHS /opt/eos/grpc /opt/eos/lib64/protobuf3 /usr/lib64/protobuf3 /usr/lib/protobuf3
@@ -49,23 +49,23 @@ find_library(PROTOBUF_LIBRARY
   PATH_SUFFIXES lib64 lib
   NO_DEFAULT_PATH)
 
+# Include Protobuf package from the generation commands like PROTOBUF_GENERATE_CPP
+find_package(Protobuf)
+# This is done to overwrite the variables that might be set by the above call
+set(PROTOBUF_INCLUDE_DIR ${PROTOBUF3_INCLUDE_DIR})
+set(PROTOBUF_LIBRARY ${PROTOBUF3_LIBRARY})
+set(PROTOBUF_INCLUDE_DIRS ${PROTOBUF3_INCLUDE_DIR})
+set(PROTOBUF_LIBRARIES ${PROTOBUF3_LIBRARY})
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Protobuf3
-  REQUIRED_VARS PROTOBUF_INCLUDE_DIR PROTOBUF_LIBRARY PROTOBUF_PROTOC_EXECUTABLE)
-mark_as_advanced(PROOBUF3_FOUND PROTOBUF_INCLUDE_DIR PROTOBUF_LIBRARY
-  PROTOBUF_PROTOC_EXECUTABLE)
+  REQUIRED_VARS PROTOBUF3_LIBRARY PROTOBUF3_INCLUDE_DIR PROTOBUF3_PROTOC_EXECUTABLE)
+mark_as_advanced(PROOBUF3_FOUND PROTOBUF3_INCLUDE_DIR PROTOBUF3_LIBRARY
+  PROTOBUF3_PROTOC_EXECUTABLE)
 
 if (PROTOBUF3_FOUND AND NOT TARGET PROTOBUF::PROTOBUF)
   add_library(PROTOBUF::PROTOBUF UNKNOWN IMPORTED)
   set_target_properties(PROTOBUF::PROTOBUF PROPERTIES
-    IMPORTED_LOCATION "${PROTOBUF_LIBRARY}"
-    INTERFACE_INCLUDE_DIRECTORIES "${PROTOBUF_INCLUDE_DIR}")
+    IMPORTED_LOCATION "${PROTOBUF3_LIBRARY}"
+    INTERFACE_INCLUDE_DIRECTORIES "${PROTOBUF3_INCLUDE_DIR}")
 endif ()
-
-# Include Protobuf package from the generation commands
-find_package(Protobuf)
-
-set(PROTOBUF_INCLUDE_DIRS ${PROTOBUF_INCLUDE_DIR})
-set(PROTOBUF_LIBRARIES ${PROTOBUF_LIBRARY})
-#unset(PROTOBUF_INCLUDE_DIR)
-#unset(PROTOBUF_LIBRARY)
