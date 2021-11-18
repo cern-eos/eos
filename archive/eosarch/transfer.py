39af12377ac4c1d20cd846a7f861855fdba096e3
Elvin Sindrilaru <esindril@cern.ch>
2021-11-18 02:02:39 +0100

    ARCHIVE: Fix wrong variable name

--- archive/eosarch/transfer.py
+++ archive/eosarch/transfer.py
@@ -1047,7 +1047,7 @@ class Transfer(object):
             url = client.URL(dst.encode("utf-8"))
             batch.append(url.path.encode("utf-8"))
 
-            if len(batch) == batch_sz:
+            if len(batch) == batch_size:
                 fs = self.archive.get_fs(dst)
                 prep_stat, __ = fs.prepare(batch, xrd_prepare_evict_flag, 0, timeout)
                 batch.clear()
