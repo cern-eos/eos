6233b576041dd58f4c83ec4aa5c35b6d49fe5815
Abhishek L <abhishek.l@cern.ch>
2021-11-18 18:10:02 +0100

    fst: filemd: Introduce FmdAttrHandler
    This class introduces implements the various filemd functions as file
    xattrs. Internally local io of fst is used for the writer
    implementation. Based on Josef's branch reworked to current changes
    
    Signed-off-by: Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>

---
+++ fst/filemd/FmdAttr.hh
@@ -0,0 +1,54 @@
+#include "FmdHandler.hh"
+
+namespace eos::fst {
+
+class FmdAttrHandler final: public FmdHandler
+{
+public:
+  void LocalDeleteFmd(eos::common::FileId::fileid_t fid,
+                      eos::common::FileSystem::fsid_t fsid) override;
+
+  bool Commit(eos::common::FmdHelper* fmd, bool lockit = true) override;
+
+  virtual std::unique_ptr<eos::common::FmdHelper>
+  LocalGetFmd(eos::common::FileId::fileid_t fid,
+              eos::common::FileSystem::fsid_t fsid,
+              bool force_retrieve = false, bool do_create = false,
+              uid_t uid = 0, gid_t gid = 0,
+              eos::common::LayoutId::layoutid_t layoutid = 0) override;
+
+  std::unique_ptr<eos::common::FmdHelper>
+  LocalGetFmd(std::string filepath,
+              bool force_retrieve = false, bool do_create = false,
+              uid_t uid = 0, gid_t gid = 0,
+              eos::common::LayoutId::layoutid_t layoutid = 0);
+
+  //TODO: Move this to a more appropriate location somewhere higher, this is
+  //nothing related to AttrHandler as such
+  static std::string GetPath(eos::common::FileId::fileid_t fid,
+                             eos::common::FileSystem::fsid_t fsid);
+private:
+  bool LocalPutFmd(eos::common::FileId::fileid_t fid,
+                   eos::common::FileSystem::fsid_t fsid,
+                   const eos::common::FmdHelper& fmd) override;
+
+
+  std::pair<bool,eos::common::FmdHelper>
+  LocalRetrieveFmd(eos::common::FileId::fileid_t fid,
+                   eos::common::FileSystem::fsid_t fsid) override;
+
+  bool LocalPutFmd(const std::string& path,
+                   const eos::common::FmdHelper& fmd);
+
+  std::pair<bool,eos::common::FmdHelper>
+  LocalRetrieveFmd(const std::string& path);
+
+  void LocalDeleteFmd(const std::string& path);
+
+  bool ResetDiskInformation(eos::common::FileSystem::fsid_t fsid) override { return true; };
+  bool ResetMgmInformation(eos::common::FileSystem::fsid_t fsid) override { return true; };
+};
+
+static const std::string gFmdAttrName = "user.eos.fmd";
+
+} // namespace eos::fst
