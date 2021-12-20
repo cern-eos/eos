5e9e5fdd6b2839e68c5c4c789a383dcb486b70e6
Elvin Sindrilaru <esindril@cern.ch>
2021-12-20 14:31:13 +0100

    CI: Create repo for ASAN enabled eos build and drop the prebuild asan stage which is not needed

--- gitlab-ci/store_artifacts.sh
+++ gitlab-ci/store_artifacts.sh
@@ -22,6 +22,7 @@ BUILDMAP[fc-33]=fc-33
 BUILDMAP[fc-34]=fc-34
 BUILDMAP[fc-rawhide]=fc-rawhide
 BUILDMAP[osx]=osx
+BUILDMAP[cc7_asan]=el-7-asan
 
 CODENAME=$1
 BUILD_TYPE=$2
@@ -30,6 +31,7 @@ PATH_PREFIX=$3
 for artifacts_dir in *_artifacts; do
   build=${artifacts_dir%_*}
   repo=${BUILDMAP[${build}]}
+  build_artifacts=${build}_artifacts
 
   # Handle only builds registered in the build map
   [ -z ${repo} ] && continue
@@ -39,22 +41,34 @@ for artifacts_dir in *_artifacts; do
   # Treat OSX artifacts separately
   if [ ${build} == "osx" ]; then
     mkdir -p ${path}/x86_64/
-    cp ${build}_artifacts/* ${path}/x86_64/
+    cp ${build_artifacts}/* ${path}/x86_64/
     continue
   fi
 
   # Upload RPMS
-  mkdir -p ${path}/x86_64/
-  cp ${build}_artifacts/RPMS/* ${path}/x86_64/
-  createrepo --update -q ${path}/x86_64/
+  if [ -d "${build_artifacts}/RPMS" ]; then
+    if [ "$(ls -A ${build_artifacts}/RPMS})" ]; then
+      mkdir -p ${path}/x86_64/
+      cp ${build_artifacts}/RPMS/* ${path}/x86_64/
+      createrepo --update -q ${path}/x86_64/
+    else
+      echo "info: Directory ${build_artifacts}/RPMS is empty!"
+    fi
+  fi
 
   # Upload SRPMS
-  mkdir -p ${path}/SRPMS/
-  cp ${build}_artifacts/SRPMS/* ${path}/SRPMS/
-  createrepo --update -q ${path}/SRPMS/
+  if [ -d "${build_artifacts}/SRPMS" ]; then
+    if [ "$(ls -A ${build_artifacts}/SRPMS})" ]; then
+      mkdir -p ${path}/SRPMS/
+      cp ${build_artifacts}/SRPMS/* ${path}/SRPMS/
+      createrepo --update -q ${path}/SRPMS/
+    else
+      echo "info: Directory ${build_artifacts}/SRPMS is empty!"
+    fi
+  fi
 
   # Upload the tarball if present
-  for tar_file in ${build}_artifacts/eos-*.tar.gz; do
+  for tar_file in ${build_artifacts}/eos-*.tar.gz; do
    if [ -e ${tar_file} ]; then
      tar_path="${PATH_PREFIX}/${CODENAME}/tarball/"
      mkdir -p ${tar_path}
