eec860a6df55ea3762cbb3f00f3206cf1817a587
Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>
2022-02-04 09:00:41 +0100

    fst: FmdAttr: add include guard, drop unused definitions
    Signed-off-by: Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>

--- fst/filemd/FmdAttr.hh
+++ fst/filemd/FmdAttr.hh
@@ -1,3 +1,27 @@
+//------------------------------------------------------------------------------
+//! @file FmdAttr.hh
+//! @author Abhishek Lekshmanan - CERN
+//------------------------------------------------------------------------------
+
+/************************************************************************
+ * EOS - the CERN Disk Storage System                                   *
+ * Copyright (C) 2019 CERN/Switzerland                                  *
+ *                                                                      *
+ * This program is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ ************************************************************************/
+
+#pragma once
 #include "FmdHandler.hh"
 
 namespace eos::fst {
@@ -20,21 +44,15 @@ public:
 
   bool Commit(eos::common::FmdHelper* fmd, bool lockit = true) override;
 
-  virtual std::unique_ptr<eos::common::FmdHelper>
+  std::unique_ptr<eos::common::FmdHelper>
   LocalGetFmd(eos::common::FileId::fileid_t fid,
               eos::common::FileSystem::fsid_t fsid,
               bool force_retrieve = false, bool do_create = false,
               uid_t uid = 0, gid_t gid = 0,
               eos::common::LayoutId::layoutid_t layoutid = 0) override;
 
-  std::unique_ptr<eos::common::FmdHelper>
-  LocalGetFmd(std::string filepath,
-              bool force_retrieve = false, bool do_create = false,
-              uid_t uid = 0, gid_t gid = 0,
-              eos::common::LayoutId::layoutid_t layoutid = 0);
-
-  //TODO: Move this to a more appropriate location somewhere higher, this is
-  //nothing related to AttrHandler as such
+  // TODO(abhi): Move this to a more appropriate location somewhere higher, this is
+  // nothing related to AttrHandler as such
   static std::string GetPath(eos::common::FileId::fileid_t fid,
                              eos::common::FileSystem::fsid_t fsid);
 private:
@@ -59,7 +77,7 @@ private:
   bool ResetDiskInformation(eos::common::FileSystem::fsid_t fsid) override { return true; };
   bool ResetMgmInformation(eos::common::FileSystem::fsid_t fsid) override { return true; };
 
-  void SetSyncStatus(eos::common::FileSystem::fsid_t, bool) {}
+  void SetSyncStatus(eos::common::FileSystem::fsid_t, bool) override {}
 };
 
 static constexpr auto gFmdAttrName = "user.eos.fmd";
