18a8932c1c90a09395b8212466a80aca4092cfb3
Andreas Joachim Peters <andreas.joachim.peters@cern.ch>
2022-03-14 15:59:29 +0100

    MGM: add application specific iorpriority and fix a type in directory based iorpriority

--- mgm/Policy.cc
+++ mgm/Policy.cc
@@ -144,6 +144,19 @@ Policy::GetLayoutAndSpace(const char* path,
 	  iotype = app_iotype;
 	}
       }
+      {
+	// try application specific iopriority setting
+	std::string appkey = "iopriority.";
+	if (env.Get("eos.app")) {
+	  appkey += env.Get("eos.app");
+	} else {
+	  appkey += "default";
+	}
+	std::string app_iopriority = it->second->GetConfigMember(appkey);
+	if (app_iopriority.length()) {
+	  iopriority = app_iopriority;
+	}
+      }
     }
   }
 
@@ -217,6 +230,19 @@ Policy::GetLayoutAndSpace(const char* path,
 	std::string app_iotype = it->second->GetConfigMember(appkey);
 	if (app_iotype.length()) {
 	  iotype = app_iotype;
+	}
+      }
+      {
+	// try application specific iopriority setting
+	std::string appkey = "iopriority.";
+	if (env.Get("eos.app")) {
+	  appkey += env.Get("eos.app");
+	} else {
+	  appkey += "default";
+	}
+	std::string app_iopriority = it->second->GetConfigMember(appkey);
+	if (app_iopriority.length()) {
+	  iopriority = app_iopriority;
 	}
       }
     }
@@ -321,7 +347,7 @@ Policy::GetLayoutAndSpace(const char* path,
     }
 
     if (attrmap.count("sys.forced.iopriority")) {
-      iotype = attrmap["sys.forced.iopriority"];
+      iopriority = attrmap["sys.forced.iopriority"];
       eos_static_debug("sys.forced.iopriority i %s : %s", path, iopriority.c_str());
     }
 
