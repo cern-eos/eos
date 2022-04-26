2913f3e88d168203090de3868f357228e54793e3
Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>
2022-04-26 23:34:48 +0200

    MGM: introduce ConverterUtils for common helpers for drainer/balancer
    Helps reuse common functionity instead of needing to include GroupBalancer for
    the drainer functionality
    
    Signed-off-by: Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>

---
+++ mgm/groupbalancer/ConverterUtils.cc
@@ -0,0 +1,60 @@
+//
+// Created by abhi on 4/26/22.
+//
+
+#include "ConverterUtils.hh"
+#include "common/Logging.hh"
+#include "namespace/Prefetcher.hh"
+#include "mgm/XrdMgmOfs.hh"
+#include "namespace/interface/IView.hh"
+#include "XrdOuc/XrdOucString.hh"
+
+namespace eos::mgm::group_balancer {
+
+std::string
+getFileProcTransferNameAndSize(eos::common::FileId::fileid_t fid,
+                                const string& target_group, uint64_t* size)
+{
+  char fileName[1024];
+  std::shared_ptr<eos::IFileMD> fmd;
+  eos::common::LayoutId::layoutid_t layoutid = 0;
+  eos::common::FileId::fileid_t fileid = 0;
+  {
+    eos::Prefetcher::prefetchFileMDAndWait(gOFS->eosView, fid);
+    eos::common::RWMutexReadLock lock(gOFS->eosViewRWMutex);
+
+    try {
+      fmd = gOFS->eosFileService->getFileMD(fid);
+      layoutid = fmd->getLayoutId();
+      fileid = fmd->getId();
+
+      if (fmd->getContainerId() == 0) {
+        return std::string("");
+      }
+
+      if (size) {
+        *size = fmd->getSize();
+      }
+
+      XrdOucString fileURI = gOFS->eosView->getUri(fmd.get()).c_str();
+
+      if (fileURI.beginswith(gOFS->MgmProcPath.c_str())) {
+        // don't touch files in any ../proc/ directory
+        return std::string("");
+      }
+
+      eos_static_debug("msg=\"found file for transfering\" file=\"%s\"",
+                       fileURI.c_str());
+    } catch (eos::MDException& e) {
+      eos_static_debug("msg=\"exception\" ec=%d emsg=\"%s\"\n", e.getErrno(),
+                       e.getMessage().str().c_str());
+      return std::string("");
+    }
+  }
+  snprintf(fileName, 1024, "%s/%016llx:%s#%08lx",
+           gOFS->MgmProcConversionPath.c_str(), fileid, target_group.c_str(),
+           (unsigned long)layoutid);
+  return std::string(fileName);
+}
+
+} // eos::mgm::group_balancer
---
+++ mgm/groupbalancer/ConverterUtils.hh
@@ -0,0 +1,46 @@
+//------------------------------------------------------------------------------
+// File: ConverterUtils.hh
+// Author: Abhishek Lekshmanan - CERN
+//------------------------------------------------------------------------------
+
+/************************************************************************
+ * EOS - the CERN Disk Storage System                                   *
+ * Copyright (C) 2022 CERN/Switzerland                                  *
+ *                                                                      *
+ * This program is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ ************************************************************************/
+
+#ifndef EOS_CONVERTERUTILS_HH
+#define EOS_CONVERTERUTILS_HH
+
+#include "common/FileId.hh"
+#include <string>
+
+namespace eos::mgm::group_balancer {
+//----------------------------------------------------------------------------
+//! Produces a file conversion path to be placed in the proc directory taking
+//! into account the given group and also returns its size
+//!
+//! @param fid the file ID
+//! @param target_group the group to which the file will be transferred
+//! @param size return address for the size of the file
+//!
+//! @return name of the proc transfer file
+//----------------------------------------------------------------------------
+std::string
+getFileProcTransferNameAndSize(eos::common::FileId::fileid_t fid,
+                               const string& target_group, uint64_t* size);
+
+} // eos::mgm::group_balancer
+#endif // EOS_CONVERTERUTILS_HH
