568fe4cdc8135e0157af112e9f8cdacfefd117fc
Andreas Joachim Peters <andreas.joachim.peters@cern.ch>
2022-06-10 09:18:51 +0200

    FUSEX: remove control sequences at beginning of EnvironmentReader and typo

--- fusex/auth/EnvironmentReader.cc
+++ fusex/auth/EnvironmentReader.cc
@@ -1,5 +1,4 @@
-
-1;3409;0c//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 // File: EnvironmentReader.cc
 // Author: Georgios Bitzes - CERN
 //------------------------------------------------------------------------------
@@ -141,7 +140,7 @@ void EnvironmentReader::worker()
         std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
 
       if (duration.count() > 5) {
-        eos_static_notic("Reading /proc/%d/environ took %dms (uid=%d)", request.pid,
+        eos_static_notice("Reading /proc/%d/environ took %dms (uid=%d)", request.pid,
                            duration.count(), request.uid);
       }
 
