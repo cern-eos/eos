19c2706219c58af6b3854e452a32309ce48edf0c
Cedric Caffy <cedric.caffy@cern.ch>
2022-06-24 11:41:12 +0200

    MGM: Tape REST API - Removed duplicate slashes coming from the client

--- mgm/http/rest-api/utils/URLParser.hh
+++ mgm/http/rest-api/utils/URLParser.hh
@@ -69,6 +69,13 @@ public:
    *    This method will return true and the params map will contain "requestid":"xxx-xxx"
    */
   bool matchesAndExtractParameters(const std::string & urlPattern, std::map<std::string, std::string>& params);
+
+  /**
+   * Removes the duplicate slashes from a path given in parameter
+   * example: /eos//test/////file.txt --> /eos/test/file.txt
+   * @param path the path to remove the duplicate slashes from
+   */
+  static void removeDuplicateSlashes(std::string & path);
 private:
   const std::string & mURL;
   std::vector<std::string> mURLTokens;
