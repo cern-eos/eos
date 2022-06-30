a43be937a4ee2c8f20e5834f907a6f8fa5e01628
Elvin Sindrilaru <esindril@cern.ch>
2022-06-30 10:47:21 +0200

    MGM/CONSOLE: Add fsck configuration option to target a certain error category during the automatic repair process. Fixes EOS-5321

--- mgm/fsck/FsckEntry.cc
+++ mgm/fsck/FsckEntry.cc
@@ -1004,6 +1004,32 @@ FsckErr ConvertToFsckErr(const std::string& serr)
 }
 
 //------------------------------------------------------------------------------
+// Convert to FsckErr type to string
+//------------------------------------------------------------------------------
+std::string ConvertToString(const FsckErr& err)
+{
+  if (err == FsckErr::MgmXsDiff) {
+    return "m_cx_diff";
+  } else if (err == FsckErr::MgmSzDiff) {
+    return "m_mem_sz_diff";
+  } else if (err == FsckErr::FstXsDiff) {
+    return "d_cx_diff";
+  } else if (err == FsckErr::FstSzDiff) {
+    return "d_mem_sz_diff";
+  } else if (err == FsckErr::UnregRepl) {
+    return "unreg_n";
+  } else if (err == FsckErr::DiffRepl) {
+    return "rep_diff_n";
+  } else if (err == FsckErr::MissRepl) {
+    return "rep_missing_n";
+  } else if (err == FsckErr::BlockxsErr) {
+    return "blockxs_err";
+  } else {
+    return "none";
+  }
+}
+
+//------------------------------------------------------------------------------
 // Update MGM stats depending on the final outcome
 //------------------------------------------------------------------------------
 void
