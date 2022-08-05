3710fd3deb8889d49204e67c1aa688cf571aec28
Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>
2022-08-05 16:09:24 +0200

    MGM: utils/AttrHelper: add helper function to get AtomicUpload/versioning
    Also assert the expected behaviour of these
    
    Signed-off-by: Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>

--- unit_tests/mgm/utils/AttrHelperTests.cc
+++ unit_tests/mgm/utils/AttrHelperTests.cc
@@ -52,3 +52,115 @@ TEST(checkStickyDirOwner, dirOwner)
   ASSERT_EQ(vid.uid, dir_uid);
   ASSERT_EQ(vid.gid, dir_gid);
 }
+
+TEST(checkAtomicUpload, EmptyMap)
+{
+  ASSERT_FALSE(attr::checkAtomicUpload({}));
+}
+
+TEST(checkAtomicUpload, sys)
+{
+  eos::IContainerMD::XAttrMap xattrs {{SYS_FORCED_ATOMIC,"1"}};
+  ASSERT_TRUE(attr::checkAtomicUpload(xattrs));
+  xattrs[SYS_FORCED_ATOMIC] = "0";
+  ASSERT_FALSE(attr::checkAtomicUpload(xattrs));
+  xattrs[SYS_FORCED_ATOMIC] = "-1";
+  ASSERT_TRUE(attr::checkAtomicUpload(xattrs));
+  xattrs[SYS_FORCED_ATOMIC] = "garbage";
+  ASSERT_FALSE(attr::checkAtomicUpload(xattrs));
+}
+
+TEST(checkAtomicUpload, user)
+{
+  eos::IContainerMD::XAttrMap xattrs {{USER_FORCED_ATOMIC,"1"}};
+  ASSERT_TRUE(attr::checkAtomicUpload(xattrs));
+  xattrs[USER_FORCED_ATOMIC] = "0";
+  ASSERT_FALSE(attr::checkAtomicUpload(xattrs));
+  xattrs[USER_FORCED_ATOMIC] = "-1";
+  ASSERT_TRUE(attr::checkAtomicUpload(xattrs));
+  xattrs[USER_FORCED_ATOMIC] = "garbage";
+  ASSERT_FALSE(attr::checkAtomicUpload(xattrs));
+}
+
+TEST(checkAtomicUpload, cgi)
+{
+  eos::IContainerMD::XAttrMap xattrs{};
+  ASSERT_TRUE(attr::checkAtomicUpload(xattrs, "foo"));
+
+  // sys overrides everything!
+  xattrs[SYS_FORCED_ATOMIC] = "0";
+  ASSERT_FALSE(attr::checkAtomicUpload(xattrs, "foo"));
+
+  // usr overrides cgi
+  xattrs.clear();
+  xattrs[USER_FORCED_ATOMIC] = "0";
+  ASSERT_FALSE(attr::checkAtomicUpload(xattrs, "foo"));
+}
+
+TEST(checkAtomicUpload, sysoverride)
+{
+  eos::IContainerMD::XAttrMap xattrs;
+  xattrs[SYS_FORCED_ATOMIC] = "0";
+  xattrs[USER_FORCED_ATOMIC] = "1";
+  ASSERT_FALSE(attr::checkAtomicUpload(xattrs));
+  ASSERT_FALSE(attr::checkAtomicUpload(xattrs, "foo"));
+
+}
+
+TEST(getVersioning, cgi)
+{
+  eos::IContainerMD::XAttrMap xattrs;
+  std::string version {"1"};
+  ASSERT_EQ(attr::getVersioning(xattrs, version), 1);
+
+  version = "2";
+  ASSERT_EQ(attr::getVersioning(xattrs, version), 2);
+}
+
+TEST(getVersioning, invalid_cgi)
+{
+  ASSERT_FALSE(attr::getVersioning({},"garbage"));
+
+  eos::IContainerMD::XAttrMap xattrs {{SYS_VERSIONING, "0"},
+                                     {USER_VERSIONING, "1"}};
+
+  ASSERT_FALSE(attr::getVersioning(xattrs, "garbage"));
+
+  xattrs.clear();
+  xattrs[SYS_VERSIONING] = "1";
+  xattrs[USER_VERSIONING] = "0";
+  ASSERT_FALSE(attr::getVersioning(xattrs, "garbage"));
+}
+
+TEST(getVersioning, cgi_overrides)
+{
+  eos::IContainerMD::XAttrMap xattrs {{SYS_VERSIONING, "0"},
+                                     {USER_VERSIONING, "1"}};
+  std::string version {"2"};
+  ASSERT_EQ(attr::getVersioning(xattrs, version), 2);
+}
+
+TEST(getVersioning, sys_overrides)
+{
+  eos::IContainerMD::XAttrMap xattrs;
+  xattrs[SYS_VERSIONING] = "1";
+  xattrs[USER_VERSIONING] = "0";
+  ASSERT_TRUE(attr::getVersioning(xattrs));
+
+  xattrs[SYS_VERSIONING] = "10";
+  ASSERT_EQ(attr::getVersioning(xattrs), 10);
+
+  // sys overrides usr, so a garbage sys value will mean 0 versions!
+  xattrs[SYS_VERSIONING] = "garbage";
+  xattrs[USER_VERSIONING] = "1";
+  ASSERT_FALSE(attr::getVersioning(xattrs));
+}
+
+TEST(getVersioning, user)
+{
+  eos::IContainerMD::XAttrMap xattrs {{USER_VERSIONING, "1"}};
+  ASSERT_TRUE(attr::getVersioning(xattrs));
+
+  xattrs[USER_VERSIONING] = "10";
+  ASSERT_EQ(attr::getVersioning(xattrs), 10);
+}
