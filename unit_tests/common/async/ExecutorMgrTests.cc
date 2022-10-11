4f557f87b7376d47e6e2a8ce39d8ea8942be5c4f
Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>
2022-10-11 23:21:12 +0200

    COMMON: ExecutorMgr: introduce Shutdown & add support for folly CPU executors
    Shutdown routine either finishes all the tasks (like join) or abandons them
    depending on the underlying executor impl.
    
    In addition, add support for folly CPU executors, which are a bit more easier to
    deal with compared to IOThreadPoolExecutor, also they support true cancellation.
    Both IOThreadPoolExecutor & our common::ThreadPool will drain all the tasks on
    shutdown, whereas CPUThreadPoolExecutor will actually abandon tasks.
    
    Also set min common threadpool size as 2, and set the threadsize as the max_threadsize
    similar to the way we do for the folly executors.
    
    Signed-off-by: Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>

--- unit_tests/common/async/ExecutorMgrTests.cc
+++ unit_tests/common/async/ExecutorMgrTests.cc
@@ -17,13 +17,9 @@
 //  ************************************************************************
 //
 
-//
-// Created by Abhishek Lekshmanan on 29/09/2022.
-//
 #include "common/async/ExecutorMgr.hh"
 #include "unit_tests/common/async/FollyExecutorFixture.hh"
 #include <gtest/gtest.h>
-#include <folly/executors/CPUThreadPoolExecutor.h>
 
 
 TEST(ExecutorMgr, Construction)
@@ -45,11 +41,11 @@ TEST(ExecutorMgr, ThreadPool)
 
   for (int i = 0; i < 10; i++) {
     auto future = mgr.PushTask(
-    [] {
-      std::this_thread::sleep_for(std::chrono::milliseconds(20));
-      return std::this_thread::get_id();
-    }
-                  );
+        [] {
+          std::this_thread::sleep_for(std::chrono::milliseconds(20));
+          return std::this_thread::get_id();
+        }
+    );
     futures.emplace_back(std::move(future));
   }
 
@@ -65,17 +61,17 @@ TEST(ExecutorMgr, ThreadPool)
 
 TEST_F(FollyExecutor_F, IOThreadPoolExecutorTests)
 {
-  eos::common::ExecutorMgr mgr(folly_executor);
+  eos::common::ExecutorMgr mgr(folly_io_executor);
   ASSERT_TRUE(mgr.IsFollyExecutor());
   std::vector<eos::common::OpaqueFuture<std::thread::id>> futures;
 
   for (int i = 0; i < 10; i++) {
     auto future = mgr.PushTask(
-    [] {
-      std::this_thread::sleep_for(std::chrono::milliseconds(20));
-      return std::this_thread::get_id();
-    }
-                  );
+        [] {
+          std::this_thread::sleep_for(std::chrono::milliseconds(20));
+          return std::this_thread::get_id();
+        }
+    );
     futures.emplace_back(std::move(future));
   }
 
@@ -88,3 +84,93 @@ TEST_F(FollyExecutor_F, IOThreadPoolExecutorTests)
   // Check if we have exactly 4 different thread ids
   ASSERT_EQ(kNumThreads, threadIds.size());
 }
+
+TEST_F(FollyExecutor_F, CPUThreadPoolExecutorTests)
+{
+  eos::common::ExecutorMgr mgr(folly_cpu_executor);
+  ASSERT_TRUE(mgr.IsFollyExecutor());
+  std::vector<eos::common::OpaqueFuture<std::thread::id>> futures;
+
+  for (int i = 0; i < 10; i++) {
+    auto future = mgr.PushTask(
+        [] {
+          std::this_thread::sleep_for(std::chrono::milliseconds(20));
+          return std::this_thread::get_id();
+        }
+    );
+    futures.emplace_back(std::move(future));
+  }
+
+  std::set<std::thread::id> threadIds;
+
+  for (auto && future : futures) {
+    threadIds.insert(future.getValue());
+  }
+
+  // Check if we have exactly 4 different thread ids
+  ASSERT_EQ(kNumThreads, threadIds.size());
+}
+
+TEST(ExecutorMgr, ThreadPoolShutdown)
+{
+  eos::common::ExecutorMgr mgr("std",2,4);
+  ASSERT_TRUE(mgr.IsThreadPool());
+  std::atomic<int> counter{0};
+
+  for (int i = 0; i < 100; i++) {
+    mgr.PushTask(
+                 [&counter] {
+                   std::this_thread::sleep_for(std::chrono::milliseconds(20));
+                   counter++;
+                 }
+                 );
+  }
+  // currently the tasks shouldn't complete just yet!
+  ASSERT_GT(100, counter);
+  mgr.Shutdown();
+  ASSERT_EQ(100, counter);
+  std::cout << "common::ThreadPool executed " << counter << " tasks" << std::endl;
+}
+
+TEST_F(FollyExecutor_F, IOThreadPoolShutdown)
+{
+  eos::common::ExecutorMgr mgr(folly_io_executor);
+  ASSERT_TRUE(mgr.IsFollyExecutor());
+  std::atomic<int> counter{0};
+
+  for (int i = 0; i < 100; i++) {
+    mgr.PushTask(
+                 [&counter] {
+                   std::this_thread::sleep_for(std::chrono::milliseconds(20));
+                   counter++;
+                 }
+                 );
+  }
+  ASSERT_GT(100, counter);
+  mgr.Shutdown();
+  // There is no stopping the IOThreadPoolExecutor!!!
+  ASSERT_EQ(100, counter); // 100 tasks should have been executed
+  std::cout << "folly::IOThreadPoolExecutor executed " << counter << " tasks" << std::endl;
+}
+
+
+TEST_F(FollyExecutor_F, CPUThreadPoolShutdown)
+{
+  eos::common::ExecutorMgr mgr(folly_cpu_executor);
+  ASSERT_TRUE(mgr.IsFollyExecutor());
+  std::atomic<int> counter{0};
+  for (int i = 0; i < 100; i++) {
+    mgr.PushTask(
+                 [&counter] {
+                   std::this_thread::sleep_for(std::chrono::milliseconds(20));
+                   counter++;
+                 }
+                 );
+  }
+  ASSERT_GT(100, counter);
+  mgr.Shutdown();
+  // CPU ThreadPool supports true cancellation!
+  ASSERT_GT(100, counter);
+  std::cout << "folly::CPUThreadPoolExecutor executed " << counter << " tasks" << std::endl;
+}
+
