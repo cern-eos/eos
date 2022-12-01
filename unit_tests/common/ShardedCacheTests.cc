6cf9e9373cb6754640a5c93e6c8ac0d923f26259
Elvin Sindrilaru <esindril@cern.ch>
2022-12-01 16:27:21 +0100

    TEST: Increase a bit the timeouts to have tests pass on busy systems

--- unit_tests/common/ShardedCacheTests.cc
+++ unit_tests/common/ShardedCacheTests.cc
@@ -34,10 +34,10 @@ TEST(ShardedCache, Construction)
 
 TEST(ShardedCache, CalculateShard)
 {
-  ShardedCache<std::string, int> cache(8,100);
+  ShardedCache<std::string, int> cache(8, 100);
   std::hash<std::string> hasher{};
   ASSERT_GE(cache.calculateShard("hello"), 0);
-  ASSERT_LT(cache.calculateShard("hello"),256);
+  ASSERT_LT(cache.calculateShard("hello"), 256);
   EXPECT_EQ(cache.calculateShard("hello"),
             hasher("hello") % 256);
 }
@@ -50,22 +50,21 @@ TEST(ShardedCache, NoGC)
   EXPECT_EQ(cache.num_entries(), 0);
   std::hash<std::string> hasher{};
   ASSERT_GE(cache.calculateShard("hello"), 0);
-  ASSERT_LT(cache.calculateShard("hello"),256);
+  ASSERT_LT(cache.calculateShard("hello"), 256);
   EXPECT_EQ(cache.calculateShard("hello"),
             hasher("hello") % 256);
 }
 
 TEST(ShardedCache, EmptyRetrieve)
 {
-  ShardedCache<std::string, int> cache(8,100);
+  ShardedCache<std::string, int> cache(8, 100);
   auto result = cache.retrieve("hello");
   EXPECT_EQ(result, nullptr);
-
 }
 
 TEST(ShardedCache, ValueRetrieve)
 {
-  ShardedCache<std::string, int> cache(8,100);
+  ShardedCache<std::string, int> cache(8, 100);
   auto val = std::make_unique<int>(5);
   ASSERT_TRUE(cache.store("hello", std::move(val)));
   auto result = cache.retrieve("hello");
@@ -80,17 +79,14 @@ TEST(ShardedCache, ValueNonExpiry)
   auto result = cache.retrieve("hello");
   ASSERT_NE(result, nullptr);
   EXPECT_EQ(*result, 5);
-
   // Since we hold a valid reference to the entry it never expires
   std::this_thread::sleep_for(std::chrono::milliseconds(30));
   auto result2 = cache.retrieve("hello");
   EXPECT_EQ(result2, result);
-
   // now release ownership. The entry should expire
   result.reset();
   result2.reset();
-
-  std::this_thread::sleep_for(std::chrono::milliseconds(20));
+  std::this_thread::sleep_for(std::chrono::milliseconds(30));
   auto result3 = cache.retrieve("hello");
   EXPECT_EQ(result3, nullptr);
 }
@@ -103,11 +99,10 @@ TEST(ShardedCache, ValueExpiry)
   ASSERT_NE(result, nullptr);
   EXPECT_EQ(*result, 5);
   result.reset();
-
   // The entry takes between 2*ttl and 3*ttl to completely expire
   // as the first round just marks the entry as expired and the second round
   // actually deletes it.
-  std::this_thread::sleep_for(std::chrono::milliseconds(30));
+  std::this_thread::sleep_for(std::chrono::milliseconds(50));
   auto result2 = cache.retrieve("hello");
   EXPECT_EQ(result2, nullptr);
 }
@@ -140,11 +135,13 @@ TEST(ShardedCache, get_shard_except)
 {
   ShardedCache<std::string, int> cache(8, 10);
   std::unordered_map<std::string, int> empty_map;
-  for (int i=0; i < cache.num_shards(); i++) {
+
+  for (int i = 0; i < cache.num_shards(); i++) {
     ASSERT_EQ(cache.get_shard(i), empty_map);
   }
+
   ASSERT_THROW(cache.get_shard(cache.num_shards()), std::out_of_range);
-  ASSERT_THROW(cache.get_shard(cache.num_shards()+1), std::out_of_range);
+  ASSERT_THROW(cache.get_shard(cache.num_shards() + 1), std::out_of_range);
   ASSERT_THROW(cache.get_shard(-1), std::out_of_range);
 }
 
@@ -156,7 +153,7 @@ TEST(ShardedCache, get_shard)
   auto shard_copy = cache.get_shard(cache.calculateShard("hello"));
   ASSERT_EQ(shard_copy.size(), 1);
   ASSERT_EQ(shard_copy["hello"], 5);
-  std::this_thread::sleep_for(std::chrono::milliseconds(30));
+  std::this_thread::sleep_for(std::chrono::milliseconds(50));
   ASSERT_EQ(cache.num_entries(), 0);
 }
 
