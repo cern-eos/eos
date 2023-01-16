b19843813f80d4d4b501d068d5d9f63b78575da4
Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>
2023-01-16 10:28:20 +0100

    MGM: placement: Introduce a simple flat scheduler
    This scheduler basically schedules assuming a simple flat array of storage
    elements and disks supplied as cluster_data. The elements are chosen by a
    PlacementStrategy class, currently a simple Round Robin scheduler is introduced.
    
    The rule to pick elements right now would be a simple array indexed by the
    storage element type and the elements to pick. A -1 value implies pick as many
    replicas as originally requested, this allows for a more storage aware placement
    strategy where one can tweak the numbers, so 2 replicas in site 1 and 1 replica
    in site 2 split across two racks for eg.
    
    For the default case, where we don't expect geo scheduling, we do a kind of DFS
    to go to the end of the tree, since we are only about to pick 1 site, group etc,
    this makes it simple.
    
    The non default case picks the replicas using a classical BFS approach of
    picking elements per storage element
    
    Signed-off-by: Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>

--- mgm/placement/Scheduler.hh
+++ mgm/placement/Scheduler.hh
@@ -25,6 +25,7 @@
 
 #include "mgm/placement/ClusterDataTypes.hh"
 #include "mgm/placement/RRSeed.hh"
+#include <algorithm>
 
 namespace eos::mgm::placement {
 
@@ -39,6 +40,14 @@ struct PlacementResult {
     return ret_code == 0;
   }
 
+
+  bool is_valid_placement(uint8_t n_replicas) const {
+    return ids.size() == n_replicas &&
+    (std::all_of(ids.cbegin(), ids.cend(),[](item_id_t id) {
+      return id > 0;
+    }));
+  }
+
   friend std::ostream& operator<< (std::ostream& os, const PlacementResult r) {
     for (const auto& id: r.ids) {
       os << id << " ";
@@ -49,6 +58,12 @@ struct PlacementResult {
 
 };
 
+enum class PlacementStrategyT {
+  kRoundRobin
+};
+
+
+
 struct PlacementStrategy {
   struct Args {
     item_id_t bucket_id;
@@ -58,15 +73,17 @@ struct PlacementStrategy {
 
     Args(item_id_t bucket_id, uint8_t n_replicas,
          DiskStatus status=DiskStatus::kRO, uint64_t fid=0) :
-      bucket_id(bucket_id), n_replicas(n_replicas), status(status), fid(fid) {}
+      bucket_id(bucket_id), n_replicas(n_replicas),
+      status(status), fid(fid) {}
   };
 
   virtual PlacementResult chooseItems(const ClusterData& cluster_data,
                                       Args args) = 0;
+
+  virtual ~PlacementStrategy() = default;
 };
 
-
-class RoundRobinPlacement : PlacementStrategy {
+class RoundRobinPlacement : public PlacementStrategy {
 public:
   explicit
   RoundRobinPlacement(size_t max_buckets) : mSeed(max_buckets) {}
@@ -77,6 +94,66 @@ private:
   RRSeed<size_t> mSeed;
 };
 
+template <typename... Args>
+std::unique_ptr<PlacementStrategy> makePlacementStrategy(PlacementStrategyT type,
+                                                         Args&&... args) {
+  switch (type) {
+  case PlacementStrategyT::kRoundRobin:
+    return std::make_unique<RoundRobinPlacement>(std::forward<Args>(args)...);
+  default:
+    return nullptr;
+  }
+}
+
+
+
+// We really need a more creative name?
+class FlatScheduler {
+public:
+
+  struct PlacementArguments {
+    item_id_t bucket_id = 0;
+    uint8_t n_replicas;
+    DiskStatus status = DiskStatus::kRW;
+    uint64_t fid;
+    bool default_placement = true;
+    selection_rules_t rules=kDefault2Replica;
+
+
+    PlacementArguments(uint8_t n_replicas, DiskStatus _status): bucket_id(0), n_replicas(n_replicas),
+        status(_status), fid(0), rules(kDefault2Replica), default_placement(true) {}
+
+    PlacementArguments(uint8_t n_replicas) :
+        PlacementArguments(n_replicas, DiskStatus::kRW)
+    {}
+
+    PlacementArguments(item_id_t bucket_id, uint8_t n_replicas,
+                       DiskStatus status, uint64_t fid, selection_rules_t rules):
+      bucket_id(bucket_id), n_replicas(n_replicas), status(status), fid(fid),
+      rules(rules), default_placement(false) {}
+  };
+
+  template <typename... Args>
+  FlatScheduler(PlacementStrategyT strategy, Args&&... args) :
+      mPlacementStrategy(makePlacementStrategy(strategy, std::forward<Args>(args)...)) {}
+
+  PlacementResult schedule(const ClusterData& cluster_data,
+                           PlacementArguments args);
+
+private:
+  PlacementResult scheduleDefault(const ClusterData& cluster_data,
+                                  PlacementArguments args);
+
+
+  std::unique_ptr<PlacementStrategy> mPlacementStrategy;
+
+};
+
+static_assert(sizeof(FlatScheduler) == 8);
 
 } // namespace eos::mgm::placement
+
+
+
+
 
