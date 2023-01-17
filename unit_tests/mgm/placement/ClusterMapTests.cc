12a6cb2a77c577b94ee9a5422441daed2a8fee00
Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>
2023-01-17 16:38:38 +0100

    UNIT_TESTS: placement: Add tests for adding disks in random order
    Signed-off-by: Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>

--- unit_tests/mgm/placement/ClusterMapTests.cc
+++ unit_tests/mgm/placement/ClusterMapTests.cc
@@ -184,3 +184,68 @@ TEST(ClusterMgr, StorageHandlerDiskInOrder)
     EXPECT_EQ(disk.id, i+1);
   }
 }
+
+TEST(ClusterMgr, StorageHandlerDisksOutOfOrder)
+{
+
+  using namespace eos::mgm::placement;
+  eos::mgm::placement::ClusterMgr mgr;
+  {
+    auto sh = mgr.getStorageHandler();
+    ASSERT_TRUE(sh.addBucket(get_bucket_type(StdBucketType::ROOT), 0));
+    ASSERT_TRUE(sh.addBucket(get_bucket_type(StdBucketType::SITE), -1, 0));
+    ASSERT_TRUE(sh.addBucket(get_bucket_type(StdBucketType::SITE), -2, 0));
+    ASSERT_TRUE(sh.addBucket(get_bucket_type(StdBucketType::GROUP), -100, -1));
+    ASSERT_TRUE(sh.addBucket(get_bucket_type(StdBucketType::GROUP), -101, -1));
+    ASSERT_TRUE(sh.addBucket(get_bucket_type(StdBucketType::GROUP), -102, -2));
+
+    ASSERT_TRUE(sh.addDisk(Disk(110), -100));
+    ASSERT_TRUE(sh.addDisk(Disk(100), -100));
+    ASSERT_TRUE(sh.addDisk(Disk(104), -101));
+    ASSERT_TRUE(sh.addDisk(Disk(121), -101));
+    ASSERT_TRUE(sh.addDisk(Disk(150), -102));
+  }
+
+  ASSERT_EQ(mgr.getCurrentEpoch(), 1);
+
+  auto cluster_data = mgr.getClusterData();
+
+  EXPECT_EQ(cluster_data->disks.size(), 150);
+  EXPECT_EQ(cluster_data->buckets.size(), 256);
+
+  auto root_bucket = cluster_data->buckets[0];
+  std::vector<int32_t> root_items{-1, -2};
+  EXPECT_EQ(root_bucket.id, 0);
+  EXPECT_EQ(root_bucket.bucket_type, get_bucket_type(StdBucketType::ROOT));
+  EXPECT_EQ(root_bucket.items, root_items);
+
+  auto site_bucket1 = cluster_data->buckets[1];
+  std::vector<int32_t> site_items1{-100, -101};
+  EXPECT_EQ(site_bucket1.id, -1);
+  EXPECT_EQ(site_bucket1.bucket_type, get_bucket_type(StdBucketType::SITE));
+  EXPECT_EQ(site_bucket1.items, site_items1);
+
+  auto site_bucket2 = cluster_data->buckets[2];
+  std::vector<int32_t> site_items2{-102};
+  EXPECT_EQ(site_bucket2.id, -2);
+  EXPECT_EQ(site_bucket2.bucket_type, get_bucket_type(StdBucketType::SITE));
+  EXPECT_EQ(site_bucket2.items, site_items2);
+
+  auto group_bucket1 = cluster_data->buckets[100];
+  std::vector<int32_t> group_items1{110,100};
+  EXPECT_EQ(group_bucket1.id, -100);
+  EXPECT_EQ(group_bucket1.bucket_type, get_bucket_type(StdBucketType::GROUP));
+  EXPECT_EQ(group_bucket1.items, group_items1);
+
+  auto group_bucket2 = cluster_data->buckets[101];
+  std::vector<int32_t> group_items2{104,121};
+  EXPECT_EQ(group_bucket2.id, -101);
+  EXPECT_EQ(group_bucket2.bucket_type, get_bucket_type(StdBucketType::GROUP));
+  EXPECT_EQ(group_bucket2.items, group_items2);
+
+  auto group_bucket3 = cluster_data->buckets[102];
+  std::vector<int32_t> group_items3{150};
+  EXPECT_EQ(group_bucket3.id, -102);
+  EXPECT_EQ(group_bucket3.bucket_type, get_bucket_type(StdBucketType::GROUP));
+  EXPECT_EQ(group_bucket3.items, group_items3);
+}
