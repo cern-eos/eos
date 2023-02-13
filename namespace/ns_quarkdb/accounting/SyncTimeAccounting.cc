723550be5b5020f9e7a3794a776114813f6fde33
Cedric Caffy <cedric.caffy@cern.ch>
2023-02-13 14:44:16 +0100

    NS: QuarkSyncTimeAccounting - The container to update the mtime is now locked with the new locking mechanism

--- namespace/ns_quarkdb/accounting/SyncTimeAccounting.cc
+++ namespace/ns_quarkdb/accounting/SyncTimeAccounting.cc
@@ -155,6 +155,7 @@ QuarkSyncTimeAccounting::PropagateUpdates(ThreadAssistant* assistant)
 
         try {
           cont = mContainerMDSvc->getContainerMD(id);
+          eos::IContainerMD::IContainerMDWriteLocker locker(cont);
 
           // Only traverse if there there is an attribute saying so
           if (!cont->hasAttribute("sys.mtime.propagation")) {
