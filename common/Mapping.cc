615a6623ac7b0c7911cd2d86df130e7b8b68ad72
Andreas Joachim Peters <andreas.joachim.peters@cern.ch>
2023-02-14 16:59:23 +0100

    MGM: allow http(s) gateways to specify x-forwarded-for headers - fixes EOS-5524 COMMON: add 'gateway' bool to indicated if a client is sitting on a gateway node

--- common/Mapping.cc
+++ common/Mapping.cc
@@ -205,6 +205,7 @@ Mapping::IdMap(const XrdSecEntity* client, const char* env, const char* tident,
   vid.name = client->name;
   vid.tident = tident;
   vid.sudoer = false;
+  vid.gateway = false;
   // first map by alias
   XrdOucString useralias = client->prot;
   useralias += ":";
@@ -577,6 +578,7 @@ Mapping::IdMap(const XrdSecEntity* client, const char* env, const char* tident,
         } else {
           Mapping::getPhysicalIds(client->name, vid);
         }
+	vid.gateway = true;
       }
     } else {
       eos_static_debug("tident uid forced mapping");
@@ -614,6 +616,7 @@ Mapping::IdMap(const XrdSecEntity* client, const char* env, const char* tident,
         vid.allowed_uids.clear();
         vid.allowed_uids.insert(uid);
         vid.allowed_uids.insert(99);
+	vid.gateway = true;
       }
     } else {
       eos_static_debug("tident gid forced mapping");
@@ -667,6 +670,7 @@ Mapping::IdMap(const XrdSecEntity* client, const char* env, const char* tident,
       if (gVirtualUidMap.count(maptident.c_str()) ||
 	  gVirtualUidMap.count(wildcardmaptident.c_str())) {
 	// if this is an allowed gateway, map according to client name or authkey
+	vid.gateway = true;
 	std::string uidkey = "grpc:\"";
 	uidkey += "key:";
 	uidkey += keyname;
@@ -740,6 +744,7 @@ Mapping::IdMap(const XrdSecEntity* client, const char* env, const char* tident,
 
         if (gVirtualUidMap.count(maptident) ||
             gVirtualUidMap.count(wildcardmaptident)) {
+	  vid.gateway = true;
           // if this is an allowed gateway, map according to client name or authkey
           std::string uidkey = "sss:\"";
           uidkey += "key:";
@@ -2128,6 +2133,7 @@ VirtualIdentity Mapping::Someone(uid_t uid, gid_t gid)
   vid.allowed_uids = {uid, 99};
   vid.allowed_gids = {uid, 99};
   vid.sudoer = false;
+  vid.gateway = false;
   vid.uid_string = UidToUserName(uid, errc);
 
   if (!errc) {
