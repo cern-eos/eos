59224f634fb04faec260368b081e3a24d012d0af
Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>
2023-02-28 15:01:59 +0100

    COMMON: Concurrency: Introduce simple macros for alignment
    Introduce the various 64 byte alignment macros
    
    Signed-Off-By: Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>

---
+++ common/concurrency/AlignMacros.hh
@@ -0,0 +1,38 @@
+//------------------------------------------------------------------------------
+// File: AlignMacros.hh
+// Author: Abhishek Lekshmanan - CERN
+//------------------------------------------------------------------------------
+
+/************************************************************************
+ * EOS - the CERN Disk Storage System                                   *
+ * Copyright (C) 2023 CERN/Switzerland                                  *
+ *                                                                      *
+ * This program is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ ************************************************************************/
+
+
+#pragma once
+
+#include <new>
+// Macros to help with alignment, ideally __cpp_lib_hardware_interference_size
+// is a cpp17 feature, but looks like none of the compilers actually implement
+// this!
+#ifdef __cpp_lib_hardware_interference_size
+using std::hardware_constructive_interference_size;
+using std::hardware_destructive_interference_size;
+#else
+// 64 bytes on x86-64 │ L1_CACHE_BYTES │ L1_CACHE_SHIFT │ __cacheline_aligned │ ...
+constexpr std::size_t hardware_constructive_interference_size = 64;
+constexpr std::size_t hardware_destructive_interference_size = 64;
+#endif
