3bbbd71f23ef8520a128c4de76d5aec6c2140e95
Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>
2023-03-21 17:01:56 +0100

    COMMON: Concurrency: Introduce a RCU Like primitive for Atomic Uniq Ptrs
    We introduce a simple Kernel RCU like primitive to track readers in their
    critical sections. The idea is for rarely changing data, that readers
    do not wait upon writers. The readers track when they enter and leave critical
    sections and the writer side does not delete the old data until we're sure that
    no readers access this.
    
    The idea here is that whenever a publisher updates the data we increment the
    epoch of the data, the epoch is incremented post an atomic ptr reset, and this
    ensures that no threads will access data newer than the published epoch, and
    this idea can be used to reclaim the older epochs when the count of threads
    accessing epochs is zero. We introduce 2 types of epoch counter
    
    - A simple epoch counter where every thread gets the current epoch of the data
    and increment their counts, the difference between this approach vs RW Locks is
    that we still keep alive the old version of data while new data is published, so
    readers are never blocked even if a writer comes along. However all threads
    almost read the same memory, so this has high cache contention.
    
    - A thread local epoch counter: here a thread local TID is generated similar to
    folly and CMU's deferred RCU approach. This is done statically, so the first
    time the thread tries to do some locking a thread local TID is generated which
    is indexed onto a static array. The array elements are cache aligned, so for
    readers we should have the ideal case of no sharing as far as the locking
    primitive itself goes, only the writer has to pay the cost of walking through
    this array to determine the epochs
    
    Tests are added to demonstrate the various functionalities, and microbenchmarks
    follow to demonstrate the different gains
    
    Signed-off-by: Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>

---
+++ unit_tests/common/concurrency/RCUTests.cc
@@ -0,0 +1,115 @@
+// /************************************************************************
+//  * EOS - the CERN Disk Storage System                                   *
+//  * Copyright (C) 2023 CERN/Switzerland                           *
+//  *                                                                      *
+//  * This program is free software: you can redistribute it and/or modify *
+//  * it under the terms of the GNU General Public License as published by *
+//  * the Free Software Foundation, either version 3 of the License, or    *
+//  * (at your option) any later version.                                  *
+//  *                                                                      *
+//  * This program is distributed in the hope that it will be useful,      *
+//  * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+//  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+//  * GNU General Public License for more details.                         *
+//  *                                                                      *
+//  * You should have received a copy of the GNU General Public License    *
+//  * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+//  ************************************************************************
+//
+
+#include "common/concurrency/RCULite.hh"
+#include "common/concurrency/AtomicUniquePtr.h"
+#include "gtest/gtest.h"
+#include <shared_mutex>
+
+TEST(RCUTests, Basic)
+{
+  using namespace eos::common;
+
+  // Test that we can create an RCU object
+  RCUDomain<experimental::ThreadEpochCounter,1> rcu_domain;
+  atomic_unique_ptr<int> ptr(new int(0));
+  int sum{0};
+  int i{0};
+  // Test that we can create an RCU read lock
+  auto read_fn = [&rcu_domain,&i, &sum,&ptr](int index) {
+    auto tid = experimental::tlocalID.get();
+    std::cout << "Starting reader at index=" << index << "tid=" << tid
+              <<  std::endl;
+    for (int j=0; j<100; ++j) {
+      RCUReadLock rlock(rcu_domain);
+      ASSERT_TRUE(ptr);
+    }
+    std::cout << "Done with reader at index= " << index << " tid=" << tid << std::endl;
+  };
+
+  std::thread writer([&rcu_domain, &ptr, &i]() {
+    std::cout << "Starting writer";
+
+    for (int j=0; j < 5000; ++j) {
+      int* old_ptr (nullptr);
+      {
+        RCUWriteLock wlock(rcu_domain);
+        old_ptr = ptr.reset(new int(i++));
+      }
+      std::cout << ".";
+      delete old_ptr;
+      std::this_thread::sleep_for(std::chrono::nanoseconds(1));
+    }
+  });
+
+  std::vector<std::thread> readers;
+  for (int k=0; k<100; ++k) {
+    readers.emplace_back(read_fn, k);
+  }
+  for (int i=0; i<100; ++i) {
+    readers[i].join();
+  }
+  writer.join();
+
+}
+
+TEST(RCUTests, BasicVersionCounter)
+{
+  using namespace eos::common;
+
+  // Test that we can create an RCU object
+  VersionedRCUDomain rcu_domain;
+  atomic_unique_ptr<int> ptr(new int(0));
+  int sum{0};
+  int i{0};
+  // Test that we can create an RCU read lock
+  auto read_fn = [&rcu_domain,&i, &sum,&ptr](int index) {
+    auto tid = std::hash<std::thread::id>{}(std::this_thread::get_id()) % 4096;
+    std::cout << "Starting reader at index=" << index << "tid=" << tid
+              <<  std::endl;
+    for (int j=0; j<100; ++j) {
+      RCUReadLock rlock(rcu_domain);
+      ASSERT_TRUE(ptr);
+    }
+    std::cout << "Done with reader at index= " << index << " tid=" << tid << std::endl;
+  };
+
+  std::thread writer([&rcu_domain, &ptr, &i]() {
+    std::cout << "Starting writer";
+
+    for (int j=0; j < 5000; ++j) {
+      rcu_domain.rcu_write_lock();
+      auto old_ptr = ptr.reset(new int(i++));
+      rcu_domain.rcu_synchronize();
+      std::cout << ".";
+      delete old_ptr;
+      std::this_thread::sleep_for(std::chrono::nanoseconds(1));
+    }
+  });
+
+  std::vector<std::thread> readers;
+  for (int k=0; k<100; ++k) {
+    readers.emplace_back(read_fn, k);
+  }
+  for (int i=0; i<100; ++i) {
+    readers[i].join();
+  }
+  writer.join();
+
+} // namespace eos::common
