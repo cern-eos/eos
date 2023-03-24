9ec1bbd6f3c1fada072b0c7d942167bb8d9e62f8
Andreas Joachim Peters <andreas.joachim.peters@cern.ch>
2023-03-24 17:33:48 +0100

    CONSOLE: factor out a NewfindHelper class to have programmtic access to client side find

---
+++ console/commands/helpers/NewfindHelper.hh
@@ -0,0 +1,62 @@
+//------------------------------------------------------------------------------
+//! @file NewfindHelper.hh
+//! @author Andreas-Joachim Peters - CERN
+//------------------------------------------------------------------------------
+
+/************************************************************************
+ * EOS - the CERN Disk Storage System                                   *
+ * Copyright (C) 2023 CERN/Switzerland                                  *
+ *                                                                      *
+ * This program is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ ************************************************************************/
+
+#pragma once
+#include "console/commands/ICmdHelper.hh"
+
+//------------------------------------------------------------------------------
+//! Class NewfindHelper
+//------------------------------------------------------------------------------
+class NewfindHelper: public ICmdHelper
+{
+public:
+  //----------------------------------------------------------------------------
+  //! Constructor
+  //!
+  //! @param opts global options
+  //----------------------------------------------------------------------------
+  NewfindHelper(const GlobalOptions& opts):
+    ICmdHelper(opts)
+  {
+    mIsAdmin = false;
+  }
+
+  //----------------------------------------------------------------------------
+  //! Destructor
+  //----------------------------------------------------------------------------
+  ~NewfindHelper() override = default;
+
+  //----------------------------------------------------------------------------
+  //! Parse command line input
+  //!
+  //! @param arg input
+  //!
+  //! @return true if successful, otherwise false
+  //----------------------------------------------------------------------------
+  bool ParseCommand(const char* arg) override;
+  
+  int FindXroot(std::string path);
+  int FindAs3(std::string path);
+
+  void Silent() { mIsSilent=true; }
+};
