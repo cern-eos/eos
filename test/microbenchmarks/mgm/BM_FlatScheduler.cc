2eba9ffe45428c02478b9b97fd08256c96c08b02
Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>
2023-04-14 16:09:44 +0200

    TEST: Microbenchmarks: Add WeightedRandomScheduler
    Signed-off-by: Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>

--- test/microbenchmarks/mgm/BM_FlatScheduler.cc
+++ test/microbenchmarks/mgm/BM_FlatScheduler.cc
@@ -20,11 +20,12 @@
  * You should have received a copy of the GNU General Public License    *
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
  ************************************************************************/
-
-#include "mgm/placement/PlacementStrategy.hh"
 #include "benchmark/benchmark.h"
+#include "common/utils/ContainerUtils.hh"
+#include "mgm/placement/PlacementStrategy.hh"
 #include "mgm/placement/ClusterMap.hh"
 #include "mgm/placement/FlatScheduler.hh"
+
 
 static void BM_Scheduler(benchmark::State& state) {
   using namespace eos::mgm::placement;
@@ -161,6 +162,47 @@ static void BM_FidScheduler(benchmark::State& state) {
                                                    benchmark::Counter::kIsRate);
 }
 
+static void BM_WeightedRandomScheduler(benchmark::State& state) {
+  using namespace eos::mgm::placement;
+  auto n_groups = state.range(0);
+  auto n_elements = 1024;
+  const int n_disks_per_group = 16;
+  std::vector<int> weights = {4,8,16};
+  ClusterMgr mgr;
+  {
+
+    auto sh = mgr.getStorageHandler(n_elements);
+    sh.addBucket(get_bucket_type(StdBucketType::ROOT), 0);
+    //sh.addBucket(get_bucket_type(StdBucketType::SITE), -1, 0);
+
+    for (int i=0; i< n_groups; ++i) {
+      sh.addBucket(get_bucket_type(StdBucketType::GROUP), -100-i, 0);
+    }
+
+    for (int i=0; i < n_groups*n_disks_per_group; i++) {
+      sh.addDisk(Disk(i+1, ConfigStatus::kRW, ActiveStatus::kOnline,
+                      eos::common::pickIndexRR(weights, i)),
+                 -100 - i/n_disks_per_group);
+    }
+
+  }
+  FlatScheduler flat_scheduler(PlacementStrategyT::kFidRandom, n_elements);
+
+
+  FlatScheduler::PlacementArguments args(state.range(1));
+  args.fid=1;
+  for (auto _: state) {
+    auto cluster_data_ptr = mgr.getClusterData();
+    benchmark::DoNotOptimize(flat_scheduler.schedule(cluster_data_ptr(), args));
+    args.fid++;
+  }
+  state.counters["frequency"] = benchmark::Counter(state.iterations(),
+                                                   benchmark::Counter::kIsRate);
+}
+
+
+
+
 BENCHMARK(BM_Scheduler)->Threads(1)->Threads(8)->Threads(64)->Threads(128)->Threads(256)
     ->ArgsProduct({{32, 64, 128, 256, 512},
                    {2,3,6}})->UseRealTime();
@@ -174,6 +216,10 @@ BENCHMARK(BM_RandomScheduler)->Threads(1)->Threads(8)->Threads(64)->Threads(128)
                    {2,3,6}})->UseRealTime();
 
 BENCHMARK(BM_FidScheduler)->Threads(1)->Threads(8)->Threads(64)->Threads(128)->Threads(256)
+->ArgsProduct({{32, 64, 128, 256, 512},
+               {2,3,6}})->UseRealTime();
+
+BENCHMARK(BM_WeightedRandomScheduler)->Threads(1)->Threads(8)->Threads(64)->Threads(128)->Threads(256)
 ->ArgsProduct({{32, 64, 128, 256, 512},
                {2,3,6}})->UseRealTime();
 
