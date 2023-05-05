97952b504fccf377a89db7a8fdbfcc256a5e52b4
Andreas Joachim Peters <andreas.joachim.peters@cern.ch>
2023-05-05 16:18:51 +0200

    DOC: remove unused sphinx extensions

--- doc/diopside/conf.py
+++ doc/diopside/conf.py
@@ -37,7 +37,7 @@ import solar_theme
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx_markdown_tables','sphinx.ext.todo']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_themes']
