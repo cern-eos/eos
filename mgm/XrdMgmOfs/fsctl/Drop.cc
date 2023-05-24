60ad8e4d249d4fbd843702455ed8134c349ec34a
Elvin Sindrilaru <esindril@cern.ch>
2023-05-24 17:12:57 +0200

    MGM: Do the deletion report publishing outside the namespace write lock. EOS-5680

--- mgm/XrdMgmOfs/fsctl/Drop.cc
+++ mgm/XrdMgmOfs/fsctl/Drop.cc
@@ -69,112 +69,114 @@ XrdMgmOfs::Drop(const char* path,
     eos::Prefetcher::prefetchFilesystemFileListAndWait(gOFS->eosView,
         gOFS->eosFsView, fsid);
     eos::Prefetcher::prefetchFileMDWithParentsAndWait(gOFS->eosView, fid);
-    eos::common::RWMutexWriteLock ns_wr_lock(gOFS->eosViewRWMutex);
-
-    try {
-      fmd = eosFileService->getFileMD(fid);
-    } catch (...) {
-      eos_thread_warning("msg=\"no meta record exists anymore\" fxid=%s", afid);
-      // Nevertheless drop the file identifier from the file system view
-      gOFS->eosFsView->eraseEntry(fsid, fid);
-    }
-
-    if (fmd) {
-      std::string locations;
+    {
+      eos::common::RWMutexWriteLock ns_wr_lock(gOFS->eosViewRWMutex);
 
       try {
-        locations = fmd->getAttribute("sys.fs.tracking");
-      } catch (...) {}
-
-      try {
-        container =
-          gOFS->eosDirectoryService->getContainerMD(fmd->getContainerId());
-      } catch (eos::MDException& e) {}
-
-      if (container) {
-        try {
-          ns_quota = gOFS->eosView->getQuotaNode(container.get());
-        } catch (eos::MDException& e) {
-          ns_quota = nullptr;
-        }
+        fmd = eosFileService->getFileMD(fid);
+      } catch (...) {
+        eos_thread_warning("msg=\"no meta record exists anymore\" fxid=%s", afid);
+        // Nevertheless drop the file identifier from the file system view
+        gOFS->eosFsView->eraseEntry(fsid, fid);
       }
 
-      try {
-        std::vector<unsigned int> drop_fsid;
-        bool updatestore = false;
-        // If mgm.dropall flag is set then it means we got a deleteOnClose
-        // at the gateway node and we need to delete all replicas
-        char* drop_all = env.Get("mgm.dropall");
+      if (fmd) {
+        std::string locations;
 
-        if (drop_all) {
-          for (unsigned int i = 0; i < fmd->getNumLocation(); i++) {
-            drop_fsid.push_back(fmd->getLocation(i));
+        try {
+          locations = fmd->getAttribute("sys.fs.tracking");
+        } catch (...) {}
+
+        try {
+          container =
+            gOFS->eosDirectoryService->getContainerMD(fmd->getContainerId());
+        } catch (eos::MDException& e) {}
+
+        if (container) {
+          try {
+            ns_quota = gOFS->eosView->getQuotaNode(container.get());
+          } catch (eos::MDException& e) {
+            ns_quota = nullptr;
           }
-        } else {
-          drop_fsid.push_back(fsid);
         }
 
-        // Drop the selected replicas
-        for (const auto& id : drop_fsid) {
-          eos_thread_debug("msg=\"remove location\" fxid=%s fsid=%lu", afid, id);
-          updatestore = false;
+        try {
+          std::vector<unsigned int> drop_fsid;
+          bool updatestore = false;
+          // If mgm.dropall flag is set then it means we got a deleteOnClose
+          // at the gateway node and we need to delete all replicas
+          char* drop_all = env.Get("mgm.dropall");
 
-          if (fmd->hasLocation(id)) {
-            fmd->unlinkLocation(id);
-            updatestore = true;
-            locations += "-";
-            locations += std::to_string(id);
-          }
-
-          if (fmd->hasUnlinkedLocation(id)) {
-            fmd->removeLocation(id);
-            updatestore = true;
-            locations += "/";
-            locations += std::to_string(id);
-          }
-
-          if (updatestore) {
-            fmd->setAttribute("sys.fs.tracking",
-                              eos::common::StringConversion::ReduceString(locations).c_str());
-            gOFS->eosView->updateFileStore(fmd.get());
-            // After update we might have to get the new address
-            fmd = eosFileService->getFileMD(eos::common::FileId::Hex2Fid(afid));
+          if (drop_all) {
+            for (unsigned int i = 0; i < fmd->getNumLocation(); i++) {
+              drop_fsid.push_back(fmd->getLocation(i));
+            }
           } else {
-            // The FileSystem view has a reference for this file but the file
-            // has no replicas registered on the current file system - we need
-            // to force delete the entry from the FileSystem view
-            gOFS->eosFsView->eraseEntry(id, fid);
+            drop_fsid.push_back(fsid);
           }
+
+          // Drop the selected replicas
+          for (const auto& id : drop_fsid) {
+            eos_thread_debug("msg=\"remove location\" fxid=%s fsid=%lu", afid, id);
+            updatestore = false;
+
+            if (fmd->hasLocation(id)) {
+              fmd->unlinkLocation(id);
+              updatestore = true;
+              locations += "-";
+              locations += std::to_string(id);
+            }
+
+            if (fmd->hasUnlinkedLocation(id)) {
+              fmd->removeLocation(id);
+              updatestore = true;
+              locations += "/";
+              locations += std::to_string(id);
+            }
+
+            if (updatestore) {
+              fmd->setAttribute("sys.fs.tracking",
+                                eos::common::StringConversion::ReduceString(locations).c_str());
+              gOFS->eosView->updateFileStore(fmd.get());
+              // After update we might have to get the new address
+              fmd = eosFileService->getFileMD(eos::common::FileId::Hex2Fid(afid));
+            } else {
+              // The FileSystem view has a reference for this file but the file
+              // has no replicas registered on the current file system - we need
+              // to force delete the entry from the FileSystem view
+              gOFS->eosFsView->eraseEntry(id, fid);
+            }
+          }
+
+          // Delete the record only if all replicas are dropped
+          if ((!fmd->getNumUnlinkedLocation()) && (!fmd->getNumLocation())
+              && (drop_all || updatestore)) {
+            // However we should only remove the file from the namespace, if
+            // there was indeed a replica to be dropped, otherwise we get
+            // unlinked files if the secondary replica fails to write but
+            // the machine can call the MGM
+            if (ns_quota) {
+              // If we were still attached to a container, we can now detach
+              // and count the file as removed
+              ns_quota->removeFile(fmd.get());
+            }
+
+            gOFS->eosView->removeFile(fmd.get());
+
+            if (container) {
+              container->setMTimeNow();
+              gOFS->eosView->updateContainerStore(container.get());
+              container->notifyMTimeChange(gOFS->eosDirectoryService);
+              eos::ContainerIdentifier container_id = container->getIdentifier();
+              eos::ContainerIdentifier container_pid = container->getParentIdentifier();
+              ns_wr_lock.Release();
+              gOFS->FuseXCastContainer(container_id);
+              gOFS->FuseXCastRefresh(container_id, container_pid);
+            }
+          }
+        } catch (...) {
+          eos_thread_warning("no meta record exists anymore for fxid=%s", afid);
         }
-
-        // Delete the record only if all replicas are dropped
-        if ((!fmd->getNumUnlinkedLocation()) && (!fmd->getNumLocation())
-            && (drop_all || updatestore)) {
-          // However we should only remove the file from the namespace, if
-          // there was indeed a replica to be dropped, otherwise we get
-          // unlinked files if the secondary replica fails to write but
-          // the machine can call the MGM
-          if (ns_quota) {
-            // If we were still attached to a container, we can now detach
-            // and count the file as removed
-            ns_quota->removeFile(fmd.get());
-          }
-
-          gOFS->eosView->removeFile(fmd.get());
-
-          if (container) {
-            container->setMTimeNow();
-            gOFS->eosView->updateContainerStore(container.get());
-            container->notifyMTimeChange(gOFS->eosDirectoryService);
-            eos::ContainerIdentifier container_id = container->getIdentifier();
-            eos::ContainerIdentifier container_pid = container->getParentIdentifier();
-            ns_wr_lock.Release();
-            gOFS->FuseXCastContainer(container_id);
-            gOFS->FuseXCastRefresh(container_id, container_pid);
-          }
-        }
-      } catch (...) {
-        eos_thread_warning("no meta record exists anymore for fxid=%s", afid);
       }
     }
 
