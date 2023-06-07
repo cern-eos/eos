a9824120f8175db70311324859c7e9de2f309d65
Joao Afonso <joao.afonso@cern.ch>
2023-06-07 10:29:31 +0200

    [Cherry-pick from master][bc47a2dc] Fixing a few broken el7-packages.sh dependencies

--- utils/el7-packages.sh
+++ utils/el7-packages.sh
@@ -4,7 +4,7 @@ yum install --disablerepo=epel -y xrootd-server-devel xrootd-private-devel \
 yum install -y centos-release-scl
 yum install -y cmake3 sparsehash-devel \
     ncurses ncurses-devel ncurses-static openssl openssl-devel openssl-static \
-    readline readline-devel libuuid libuuid-devel zeromq3 zeromq3-devel \
+    readline readline-devel libuuid libuuid-devel zeromq zeromq-devel \
     eos-protobuf3 eos-protobuf3-devel eos-protobuf3-compiler \
     eos-protobuf3-debuginfo leveldb leveldb-devel cppunit cppunit-devel \
     fuse-libs fuse fuse-devel libattr libattr-devel openldap openldap-devel \
@@ -13,7 +13,7 @@ yum install -y cmake3 sparsehash-devel \
     jsoncpp jsoncpp-devel libcurl libcurl-devel \
     libevent libevent-devel bzip2-devel bzip2-libs jemalloc jemalloc-devel \
     eos-rocksdb devtoolset-8 gtest gtest-devel binutils-devel eos-folly \
-    libmacaroons libmacaroons-devel scitokens-cpp scitoeksn-cpp-devel \
-    grpc grpc-devel grpc-plugins libatomic
+    libmacaroons libmacaroons-devel scitokens-cpp scitokens-cpp-devel \
+    eos-grpc eos-grpc-devel eos-grpc-plugins libatomic
 
 source /opt/rh/devtoolset-8/enable
