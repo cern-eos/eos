5de9b73203b8230a3c3fb726d63e47590c6e94ed
Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>
2023-09-05 14:51:11 +0200

    common/RandUtils: declare the thread local variables at the function itself

--- common/utils/RandUtils.hh
+++ common/utils/RandUtils.hh
@@ -4,17 +4,12 @@
 
 namespace eos::common {
 
-static auto get_local_generator() {
-  thread_local std::random_device tlrd;
-  thread_local std::mt19937 generator(tlrd());
-  return generator();
-}
-
-
-
 template <typename IntType=uint32_t>
 auto getRandom(IntType start, IntType end) -> IntType
 {
+  thread_local std::random_device tlrd;
+  thread_local std::mt19937 generator(tlrd());
+
   std::uniform_int_distribution<IntType> distrib(start, end);
   return distrib(get_local_generator());
 }
