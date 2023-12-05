61e53d6a7343c90cf7152d75231190c1baf09ad5
Cedric Caffy <cedric.caffy@cern.ch>
2023-12-05 16:43:53 +0100

    Test: Cosmetic change for std::this_thread::sleep_for() usage in getFileWhileBeingWriteLocked and getFileAfterBeingRenamed tests

--- namespace/ns_quarkdb/tests/HierarchicalViewTest.cc
+++ namespace/ns_quarkdb/tests/HierarchicalViewTest.cc
@@ -1212,7 +1212,7 @@ TEST_F(HierarchicalViewF, getFileWhileBeingWriteLocked)
   auto threadWriteLockingFile = std::thread([&file, &fileLocked, sleepSeconds]() {
     eos::IFileMD::IFileMDWriteLocker fileLocker(file);
     fileLocked = true;
-    std::this_thread::sleep_for(std::chrono::duration<double>(sleepSeconds + 0.1));
+    std::this_thread::sleep_for(std::chrono::seconds(sleepSeconds) + 0.1s);
   });
   std::chrono::time_point<std::chrono::steady_clock> start;
   std::chrono::time_point<std::chrono::steady_clock> stop;
@@ -1248,7 +1248,7 @@ TEST_F(HierarchicalViewF, getFileAfterBeingRenamed)
     view()->renameFile(file.get(), "file2");
     view()->updateContainerStore(root.get());
     fileRenamed = true;
-    std::this_thread::sleep_for(std::chrono::seconds(sleepSeconds));
+    std::this_thread::sleep_for(std::chrono::seconds(sleepSeconds) + 0.1s);
   });
   std::chrono::time_point<std::chrono::steady_clock> start;
   std::chrono::time_point<std::chrono::steady_clock> stop;
