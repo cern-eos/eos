0361dbe775b12d26d4fee443e20df4185af7582f
Guilherme Amadio <amadio@cern.ch>
2024-01-29 10:32:59 +0100

    CMAKE: Drop requirement of librocksdb_tools.so
    When linking with the static libraries, some symbols are missing
    in librocksdb.a, so librocksdb_tools.a needs to be linked as well.
    When using shared libraries, all symbols can be found in the main
    librocksdb.so, so it's not necessary to link the tools library.
    Moreover, the tools library is not usualy installed as part of most
    rocksdb packages in distributions.

--- cmake/FindRocksDB.cmake
+++ cmake/FindRocksDB.cmake
@@ -30,7 +30,7 @@ find_library(ROCKSDB_TOOLS_LIBRARY
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(RocksDB
-  REQUIRE_VARS ROCKSDB_LIBRARY ROCKSDB_TOOLS_LIBRARY ROCKSDB_INCLUDE_DIR)
+  REQUIRED_VARS ROCKSDB_LIBRARY ROCKSDB_INCLUDE_DIR)
 mark_as_advanced(ROCKSDB_FOUND ROCKSDB_LIBRARY ROCKSDB_INCLUDE_DIR)
 
 if (ROCKSDB_FOUND AND NOT TARGET ROCKSDB::ROCKSDB)
@@ -39,16 +39,22 @@ if (ROCKSDB_FOUND AND NOT TARGET ROCKSDB::ROCKSDB)
   find_package(BZip2 REQUIRED)
   find_package(Snappy REQUIRED)
 
+  set(ROCKSDB_LIBRARIES "ZSTD::ZSTD;LZ4::LZ4;BZip2::BZip2;Snappy::snappy")
+
+  #@note: The ROCKSDB_LIBRARY must be specified again after
+  # the ROCKSDB_TOOLS_LIBRARY since the latter has a symbol
+  # that only the former provides and since these are both
+  # static libraries the linker searches from left to right
+  # and notes unresolved symbols as it goes!!!
+  if (ROCKSDB_TOOLS_LIBRARY)
+    set(ROCKSDB_LIBRARIES "${ROCKSDB_LIBRARIES};${ROCKSDB_TOOLS_LIBRARY};${ROCKSDB_LIBRARY}")
+  endif()
+
   add_library(ROCKSDB::ROCKSDB UNKNOWN IMPORTED)
   set_target_properties(ROCKSDB::ROCKSDB PROPERTIES
     IMPORTED_LOCATION "${ROCKSDB_LIBRARY}"
     INTERFACE_INCLUDE_DIRECTORIES "${ROCKSDB_INCLUDE_DIR}"
-    #@note: The ROCKSDB_LIBRARY must be specified again after
-    # the ROCKSDB_TOOLS_LIBRARY since the latter has a symbol
-    # that only the former provides and since these are both
-    # static libraries the linker searches from left to right
-    # and notes unresolved symbols as it goes!!!
-    INTERFACE_LINK_LIBRARIES "ZSTD::ZSTD;LZ4::LZ4;BZip2::BZip2;Snappy::snappy;${ROCKSDB_TOOLS_LIBRARY};${ROCKSDB_LIBRARY}"
+    INTERFACE_LINK_LIBRARIES "${ROCKSDB_LIBRARIES}"
     INTERFACE_COMPILE_DEFINITIONS "HAVE_ROCKSDB=1")
 else ()
   message(WARNING "Notice: rocksdb not found, no rocksdb support")
