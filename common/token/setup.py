c09038ca3f9e9ced7220413fb63c69798f9d1f11
Elvin Sindrilaru <esindril@cern.ch>
2024-11-28 00:35:28 +0100

    COMMON: Avoid polluting the source tree with python build artifacts from the eosscitokens module.

--- common/token/setup.py
+++ common/token/setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, Extension
+import distutils.command.build
 import os
 
 # Set the name of your library without the "lib" prefix or extension
@@ -6,14 +7,26 @@ import os
 library_name = "EosCommon"
 
 destdir = os.environ.get('DESTDIR', '')
+custom_build_base = os.environ.get('BUILD_BASE', '')
+custom_source_base = os.environ.get("SOURCE_BASE", "")
+source = "".join([custom_source_base, "eosscitokenmodule.c"])
+
+print("info: Using custom_build_base=\"{}\"".format(custom_build_base))
+print("info: Using custom_source_base=\"{}\"".format(custom_source_base))
+
+# Override build command to set a custom build_base as destination for the
+# build artifacts.
+class BuildCommand(distutils.command.build.build):
+    def initialize_options(self):
+        distutils.command.build.build.initialize_options(self)
+        self.build_base = custom_build_base
 
 # Set the path to the directory containing the shared library, if it's not in a standard location
 library_dir = f"{destdir}/usr/lib64/"
 
-
 module = Extension(
     'eosscitoken',
-    sources=['eosscitokenmodule.c'],  # Only the wrapper C extension file
+    sources=[source],               # Only the wrapper C extension file
     libraries=[library_name],       # Link against the shared library
     library_dirs=[library_dir],     # Directory where the library is located
 )
@@ -23,4 +36,5 @@ setup(
     version='1.0',
     description='Python interface for EOS SciToken C library',
     ext_modules=[module],
+    cmdclass={"build": BuildCommand},
 )
