b769327c36a291fc14ca1c321608b209762d784b
Guilherme Amadio <amadio@cern.ch>
2024-11-27 11:21:16 +0100

    CMAKE: Add script to build/test EOS with CTest
    The test.cmake script is meant to automate some of the standard
    configure, build, test, install cycle during development, but also
    for continuous integration. This script was originally started in
    VecCore, then improved for XRootD, and now EOS. The script can be
    generically called as
    
    ctest -VV -S test.cmake
    
    from the top directory of the repository. This will configure, build,
    and try to run all tests with ctest. A build directory will be created
    at the top level of the source directory automatically. The script has
    several options to customize the build, the main ones are:
    
     -DCDASH=1            Enables submission to CDash dashboard
     -DCOVERAGE=1         Enables test coverage analysis with gcov
     -DMEMCHECK=1         Enables memory checking with valgrind
     -DSTATIC_ANALYSIS=1  Enables static analysis with clang-tidy
     -DINSTALL=1          Enables an extra step to call make install
    
    When enabling coverage, a report is generated by default in the html/
    directory inside the build directory. The results can be viewed by
    opening the file html/coverage_details.html. This step can be disabled
    by passing -DGCOVR=0 to ctest. It is recommended to use a debug build
    to generate the coverage analysis.
    
    The configuration can also be specified directly on the command line
    via the -C option. For example, to run a coverage build in debug mode,
    with less verbose output, and showing test output when a test failure
    happens, one can run:
    
    ctest -V --output-on-failure -C Debug -DCOVERAGE=1 -S test.cmake
    
    Some environment variables can also influence the behavior of the
    script, like CC, CXX, CMAKE_GENERATOR, CTEST_CONFIGURATION_TYPE,
    CMAKE_BUILD_PARALLEL_LEVEL, CTEST_PARALLEL_LEVEL, and CMAKE_ARGS.
    These are mostly self-explanatory and can be used to override the
    provided defaults. For example, to build with the clang compiler
    and use Ninja as CMake generator, one can run:
    
    env CC=clang CXX=clang++ CMAKE_GENERATOR=Ninja ctest -V -S test.cmake
    
    Finally, the script tries to load configuration files from the .ci
    subdirectory in the source directory. The default configuration is
    used if no specific configuration is found for the detected OS. For
    example, on Ubuntu, a file named ubuntu.cmake will be used if present
    instead of config.cmake. The script also tries to detect a version,
    so, for example, on Alma, one could use almalinux8.cmake which would
    have higher precedence than almalinux.cmake.

---
+++ .ci/config.cmake
@@ -0,0 +1,5 @@
+# This file is meant to contain set commands for options
+# you'd normally set at configuration when calling CMake.
+
+# For example, to compile with C++20, uncomment the line below
+# set(CMAKE_CXX_STANDARD 20 CACHE STRING "C++Standard")
