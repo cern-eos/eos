803de646f708a9ca96550ddafff45a5928f17ba2
Elvin Sindrilaru <esindril@cern.ch>
2024-12-09 15:08:53 +0100

    AUTH: Fix length used when doing the base64 encoding

--- auth_plugin/ProtoUtils.cc
+++ auth_plugin/ProtoUtils.cc
@@ -353,7 +353,7 @@ utils::ComputeHMAC(RequestProto*& req)
   std::string hmac = eos::common::SymKey::HmacSha1(smsg);
   XrdOucString base64hmac;
   bool do_encoding = eos::common::SymKey::Base64Encode((char*)hmac.c_str(),
-                     SHA_DIGEST_LENGTH, base64hmac);
+                     hmac.length(), base64hmac);
 
   if (!do_encoding) {
     eos_static_err("unable to do base64encoding on HMAC");
