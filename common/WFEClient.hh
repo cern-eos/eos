ac0b00479d1cb11c11c2bf8b80261cc900344f66
Konstantina Skovola <konstantina.skovola@cern.ch>
2025-05-19 18:20:36 +0200

    Rename path to ca.crt

--- common/WFEClient.hh
+++ common/WFEClient.hh
@@ -66,7 +66,7 @@ class WFEGrpcClient : public WFEClient {
 public:
   WFEGrpcClient(std::string endpoint_str) {
     endpoint = endpoint_str;
-    constexpr char RootCertificate[] = "/shared/sslCerts/ca-cert.pem";
+    constexpr char RootCertificate[] = "/shared/sslCerts/ca.crt";
     grpc::SslCredentialsOptions ssl_options;
     ssl_options.pem_root_certs = file2string(RootCertificate);
     // Create a channel with SSL credentials
