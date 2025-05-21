29cc190bcf3905fdd17f10d34d8a69db3ed4e820
Gianmaria Del Monte <gianmaria.del.monte@cern.ch>
2025-05-21 10:58:58 +0200

    eos-adler32: Add --offset option EOS-6413
    When the offset option is passed, the checksum is computed
    from the given offset.

--- fst/tools/Adler32.cc
+++ fst/tools/Adler32.cc
@@ -24,29 +24,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "fst/checksum/ChecksumPlugins.hh"
+#include "common/CLI11.hpp"
 
 int
 main(int argc, char* argv[])
 {
-  if (argc != 2) {
-    fprintf(stderr, "error: you have to provide a path name\n");
-    exit(-1);
+  size_t offset{0};
+  std::string path{""};
+  CLI::App app("Compute adler32 checksum on a file");
+  app.add_option("--offset", offset, "Offset");
+  app.add_option("path", path, "Path")->required();
+
+  try {
+    app.parse(argc, argv);
+  } catch (const CLI::ParseError& e) {
+    return app.exit(e);
   }
 
   std::unique_ptr<eos::fst::CheckSum> normalXS =
     eos::fst::ChecksumPlugins::GetChecksumObject(eos::common::LayoutId::kAdler);
 
   if (normalXS) {
-    XrdOucString path = (argv[1]) ? argv[1] : "";
     unsigned long long scansize;
     float scantime;
 
-    if (!normalXS->ScanFile(path.c_str(), scansize, scantime)) {
-      fprintf(stderr, "error: unable to scan file path=%s\n", argv[1]);
+    if (!normalXS->ScanFile(path.c_str(), scansize, scantime, 0, offset)) {
+      fprintf(stderr, "error: unable to scan file path=%s\n", path.c_str());
       exit(-1);
     } else {
-      fprintf(stdout, "path=%s size=%llu time=%.02f adler32=%s\n", argv[1], scansize,
-              scantime, normalXS->GetHexChecksum());
+      fprintf(stdout, "path=%s size=%llu time=%.02f adler32=%s\n", path.c_str(),
+              scansize, scantime, normalXS->GetHexChecksum());
       exit(0);
     }
   }
