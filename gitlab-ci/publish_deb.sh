e6afbaefb61ca5d4260ef5f9a0c0232147ede721
Elvin Sindrilaru <esindril@cern.ch>
2025-06-16 17:50:54 +0200

    DEBIAN: Make adjustments for Ubuntu builds

--- gitlab-ci/publish_deb.sh
+++ gitlab-ci/publish_deb.sh
@@ -25,44 +25,26 @@ fi
 EOS_CODENAME="diopside"
 STCI_ROOT_PATH="/eos/project/s/storage-ci/www/debian"
 
-# rerepro can not handle the case when a debian package with the same name
-# already exists in the repo but with a different contents and will throw
-# an error such as:
-# ERROR: './ubuntu-jammy/eos-testkeytab_5.2.24-20240726170436git30a72cf26_amd64.deb'
-#        cannot be included as 'pool/jammy/commit/e/eos/eos-testkeytab_5.2.24-20240726170436git30a72cf26_amd64.deb'.
-# Already existing files can only be included again, if they are the same, but:
-# md5 expected: bf50a1c0a6f7d22cdd3e91f1a0095ded, got: 434617b59e9fdf63ca196b833e8244de
-# sha1 expected: 605132874877eaa79093ce4b593a489abc33c5b5, got: 4c9707ad3ce7f5511d595a53374a4ea1f2fa4997
-#
-# Therefore we check upfront if any of the files are already in the repo and we
-# skip the publishing step if this is the case.
-
-for RELEASE in "jammy" "noble"; do
+for RELEASE in "jammy" "noble" "noble-arm"; do
   if [ -d ./ubuntu-${RELEASE} ]; then
     EXPORT_REPO="${STCI_ROOT_PATH}/eos/${EOS_CODENAME}"
     mkdir -p ${EXPORT_REPO} || true
-    echo "info: Check if packages exist already"
-    found=false
-
-    for DEB_PKG in ./ubuntu-${RELEASE}/*.deb; do
-      if [ -f ${DEB_PKG} ]; then
-        FN=$(basename ${DEB_PKG})
-        # Check that find output is not empty  
-        if [[ -n "$(find ${EXPORT_REPO}/pool/${RELEASE} -name ${FN} -type f)" ]]; then  
-          echo "notice: file already exists ${DEB_PKG}"
-          found=true
-          break
-        fi
-      fi
-    done
-
-    if [[ "${found}" = true ]]; then
-      echo "notice: skip publising for already existing packages"
-      continue
-    fi
-
     echo "info: Publishing for: ${RELEASE} in location: ${EXPORT_REPO}"
-    reprepro -C ${RELEASE}/${BUILD_TYPE} -Vb ${EXPORT_REPO} includedeb ${RELEASE} ./ubuntu-${RELEASE}/*.deb
+    RELEASE_LTS="$(echo ${RELEASE} | cut -d '-' -f1)"
+
+    if ! reprepro -C ${RELEASE_LTS}/${BUILD_TYPE} -Vb ${EXPORT_REPO} includedeb ${RELEASE_LTS} ./ubuntu-${RELEASE}/*.deb; then
+        # Delete offending package and retry
+        for file in ./ubuntu-${RELEASE}/*.deb; do
+            file=$(basename "$file")
+            PKG_NAME=$(echo -n "$file" | cut -d '_' -f1)
+            PKG_VERSION=$(echo -n "$file" | cut -d '_' -f2)
+            PKG_ARCH=$(echo -n "$file" | cut -d '_' -f3 | cut -d '.' -f 1)
+            reprepro -Vb ${EXPORT_REPO} removefilter ${RELEASE_LTS} \
+                     'Package (=='${PKG_NAME}'), $Version (=='${PKG_VERSION}'), $Architecture (=='${PKG_ARCH}')'
+        done
+        # Retry
+        reprepro -C ${RELEASE_LTS}/${BUILD_TYPE} -Vb ${EXPORT_REPO} includedeb ${RELEASE_LTS} ./ubuntu-${RELEASE}/*.deb
+    fi
   fi
 done
 
