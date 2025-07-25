1bb43de6afb01fa3df9207df0ab4a1bf6f061402
Cedric Caffy <cedric.caffy@cern.ch>
2025-07-25 14:53:10 +0200

    Revert "MGM,FST: Users can specify a checksum type alongside with the checksum of the file to be transferred"
    This reverts commit 432e2ffd8047b6828cb66743525ec715b2aaf9d1.

--- mgm/XrdMgmOfsFile.cc
+++ mgm/XrdMgmOfsFile.cc
@@ -3381,12 +3381,6 @@ XrdMgmOfsFile::open(eos::common::VirtualIdentity* invid,
     if (openOpaque->Get("eos.checksum") || openOpaque->Get("eos.cloneid")) {
       redirectionhost += "&mgm.checksum=";
       redirectionhost += openOpaque->Get("eos.checksum");
-
-      if(openOpaque->Get("eos.checksumtype")) {
-        // User sets the checksum type corresponding to the eos.checksum specified
-        redirectionhost += "&mgm.checksumtypereq=";
-        redirectionhost += openOpaque->Get("eos.checksumtype");
-      }
     }
 
     if (openOpaque->Get("eos.mtime")) {
