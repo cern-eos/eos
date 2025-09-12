b9b5040a6d43d6f29098a1a16ba6a153ab580bd5
Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>
2025-09-12 15:07:14 +0200

    COMMON: Move ThreadEpochCounter out of experimental
    Also move the unused VersionEpochCounter to experimental, this will enable
    us to move a std::shared_lock/unique_lock kind of pattern which is
    more easier to handle and change to classical RWLocks when necessary. Currently
    VersionedEpochCounter makes this harder to move because of its inherent
    state, however we don't use this anywhere, so deprecate and move to a
    more saner API that'll allow switching
    
    Signed-off-by: Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>

--- unit_tests/common/concurrency/ThreadEpochCounterTests.cc
+++ unit_tests/common/concurrency/ThreadEpochCounterTests.cc
@@ -3,7 +3,7 @@
 
 TEST(ThreadEpochCounter, Basic)
 {
-  eos::common::experimental::ThreadEpochCounter counter;
+  eos::common::ThreadEpochCounter counter;
   ASSERT_FALSE(counter.epochHasReaders(0));
   int epoch=1;
   auto tid = counter.increment(epoch, 1);
@@ -15,8 +15,8 @@ TEST(ThreadEpochCounter, Basic)
 
 TEST(ThreadEpochCounter, HashCollision)
 {
-  eos::common::experimental::ThreadEpochCounter counter;
-  std::cout << "My local TID=" << eos::common::experimental::tlocalID.get() << "\n";
+  eos::common::ThreadEpochCounter counter;
+  std::cout << "My local TID=" << eos::common::tlocalID.get() << "\n";
   ASSERT_FALSE(counter.epochHasReaders(0));
   std::array<std::atomic<int>, 4096> epoch_counter {0};
   std::vector<std::thread> threads;
@@ -30,7 +30,7 @@ TEST(ThreadEpochCounter, HashCollision)
       std::this_thread::sleep_for(std::chrono::milliseconds(10));
 
       std::cout << "Got TID=" << tid << " local tid= "
-                << eos::common::experimental::tlocalID.get() << "\n";
+                << eos::common::tlocalID.get() << "\n";
       epoch_counter[tid]++;
       ASSERT_EQ(counter.getReaders(tid), epoch_counter[tid]);
     });
@@ -44,8 +44,8 @@ TEST(ThreadEpochCounter, HashCollision)
 
 TEST(ThreadEpochCounter, HashCollision2)
 {
-  eos::common::experimental::ThreadEpochCounter counter;
-  std::cout << "My local TID=" << eos::common::experimental::tlocalID.get() << "\n";
+  eos::common::ThreadEpochCounter counter;
+  std::cout << "My local TID=" << eos::common::tlocalID.get() << "\n";
   ASSERT_FALSE(counter.epochHasReaders(0));
   std::array<std::atomic<int>, 4096> epoch_counter {0};
   std::vector<std::thread> threads;
@@ -59,7 +59,7 @@ TEST(ThreadEpochCounter, HashCollision2)
       std::this_thread::sleep_for(std::chrono::milliseconds(10));
 
       std::cout << "Got TID=" << tid << " local tid= "
-                << eos::common::experimental::tlocalID.get() << "\n";
+                << eos::common::tlocalID.get() << "\n";
       epoch_counter[tid]++;
       ASSERT_EQ(counter.getReaders(tid), epoch_counter[tid]);
     });
@@ -74,7 +74,7 @@ TEST(ThreadEpochCounter, HashCollision2)
 
 TEST(VersionEpochCounter, Basic)
 {
-  eos::common::VersionEpochCounter counter;
+  eos::common::experimental::VersionEpochCounter counter;
   ASSERT_FALSE(counter.epochHasReaders(0));
   int epoch=1;
   auto tid = counter.increment(epoch, 1);
@@ -86,7 +86,7 @@ TEST(VersionEpochCounter, Basic)
 
 TEST(VersionEpochCounter, MultiThreaded)
 {
-  eos::common::VersionEpochCounter<2> counter;
+  eos::common::experimental::VersionEpochCounter<2> counter;
   ASSERT_FALSE(counter.epochHasReaders(0));
   std::array<std::atomic<int>, 2> epoch_counter = {0,0};
   std::vector<std::thread> threads;
