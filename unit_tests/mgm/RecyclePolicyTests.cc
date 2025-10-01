3204b8fc5a76a53561b04d275a84c73bd8fcffd2
Elvin Sindrilaru <esindril@cern.ch>
2025-10-01 14:43:55 +0200

    MGM: Redesign the Recycle implementation to avoid frequent find traversals and rely on the structure of the recycle bin to decide which entries need to be deleted. Fixes EOS-5973 Fixes EOS-6367

--- unit_tests/mgm/RecyclePolicyTests.cc
+++ unit_tests/mgm/RecyclePolicyTests.cc
@@ -1,11 +1,11 @@
 //------------------------------------------------------------------------------
 // File: RecyclePolicyTests.cc
-// Author: Elvin-Alin Sindrilaru <esindril at cern dot ch>
+// Author: Elvin Sindrilaru - CERN
 //------------------------------------------------------------------------------
 
 /************************************************************************
  * EOS - the CERN Disk Storage System                                   *
- * Copyright (C) 2018 CERN/Switzerland                                  *
+ * Copyright (C) 2025 CERN/Switzerland                                  *
  *                                                                      *
  * This program is free software: you can redistribute it and/or modify *
  * it under the terms of the GNU General Public License as published by *
