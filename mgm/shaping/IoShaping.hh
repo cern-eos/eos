d0f5450fff60704fbbf42911b1a4484da39c1ff6
Yanar ilkay <Ilkay.yanar71@proton.me>
2025-10-08 17:32:36 +0200

    TEST: adding files for cleanup repo and sync everything

---
+++ mgm/shaping/IoShaping.hh
@@ -0,0 +1,41 @@
+//  File: IoShaping.hh
+//  Author: Ilkay Yanar - 42Lausanne / CERN
+//  ----------------------------------------------------------------------
+
+/*************************************************************************
+ *  EOS - the CERN Disk Storage System                                   *
+ *  Copyright (C) 2025 CERN/Switzerland                                  *
+ *                                                                       *
+ *  This program is free software: you can redistribute it and/or modify *
+ *  it under the terms of the GNU General Public License as published by *
+ *  the Free Software Foundation, either version 3 of the License, or    *
+ *  (at your option) any later version.                                  *
+ *                                                                       *
+ *  This program is distributed in the hope that it will be useful,      *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ *  GNU General Public License for more details.                         *
+ *                                                                       *
+ *  You should have received a copy of the GNU General Public License    *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ *************************************************************************/
+
+#pragma once
+
+#include "mgm/Namespace.hh"
+#include "common/ioMonitor/include/IoAggregate.hh"
+
+EOSMGMNAMESPACE_BEGIN
+
+#include "common/AssistedThread.hh"
+
+class IoShaping {
+	private:
+		AssistedThread _mReceivingThread;
+		AssistedThread _mPublishingThread;
+		AssistedThread _mShapingThread;
+		std::atomic<bool> _running;
+	public:
+};
+
+EOSMGMNAMESPACE_END
