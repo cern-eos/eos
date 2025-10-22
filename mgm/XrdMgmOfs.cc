456718d9b388c5cf1db196b0d7bcc5800c24a3c0
Yanar ilkay <Ilkay.yanar71@proton.me>
2025-10-22 17:03:22 +0200

    MGM: add thread receving IoShaping

--- mgm/XrdMgmOfs.cc
+++ mgm/XrdMgmOfs.cc
@@ -628,6 +628,13 @@ XrdMgmOfs::OrderlyShutdown()
   eos_warning("msg=\"finished orderly shutdown in %llu seconds\"",
               std::chrono::duration_cast<std::chrono::seconds>
               (end_ts - start_ts).count());
+
+  mIoShaper.stopShaping();
+  eos_warning("%s", "msg=\"stopping IoShaping shaping thread\"");
+  mIoShaper.stopPublishing();
+  eos_warning("%s", "msg=\"stopping IoShaping publishing thread\"");
+  mIoShaper.stopReceiving();
+  eos_warning("%s", "msg=\"stopping IoShaping receiving thread\"");
 }
 
 //------------------------------------------------------------------------------
