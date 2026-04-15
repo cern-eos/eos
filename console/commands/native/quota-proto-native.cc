825dc9f9167dbee5392da593267a322ec3819332
Andreas Joachim Peters <andreas.joachim.peters@cern.ch>
2026-04-15 11:42:50 +0200

    CONSOLE: re-add help details for acl,df,find,group,quota commands

--- console/commands/native/quota-proto-native.cc
+++ console/commands/native/quota-proto-native.cc
@@ -7,24 +7,53 @@
 #include <CLI/CLI.hpp>
 #include "console/ConsoleMain.hh"
 #include "console/commands/helpers/ICmdHelper.hh"
+#include <iomanip>
 #include <memory>
 #include <sstream>
 
 namespace {
 std::string MakeQuotaHelp()
 {
-  return "Usage: quota [<path>] | ls|set|rm|rmnode [OPTIONS]\n\n"
-         "  [<path>]           show personal quota for path\n"
-         "  ls [-n] [-m] [-u <uid>] [-g <gid>] [[-p|-x|-q] <path>]\n"
-         "    list configured quota and quota node(s)\n"
-         "  set -u <uid>|-g <gid> [-v <bytes>] [-i <inodes>] [[-p] <path>]\n"
-         "    set volume and/or inode quota\n"
-         "  rm -u <uid>|-g <gid> [-v] [-i] [[-p] <path>]\n"
-         "    remove configured quota type(s)\n"
-         "  rmnode [-p] <path> [--really-want]\n"
-         "    remove quota node and every defined quota on that node\n\n"
-         "Options: -m (monitoring), -n (numeric ids), -u/--uid, -g/--gid,\n"
-         "  -p/--path, -v/--volume, -i/--inodes\n";
+  std::ostringstream oss;
+  oss << "Usage:\n"
+      << "    quota [<path>]                    : show personal quota for all or "
+         "only the quota node responsible for <path>\n"
+      << "    quota ls [-n] [-m] [-u <uid>] [-g <gid>] [[-p|x|q] <path>] : list "
+         "configured quota and quota node(s)\n"
+      << "                                                                       -p : "
+         "find closest matching quotanode\n"
+      << "                                                                       -x : "
+         "as -p but <path> has to exist\n"
+      << "                                                                       -q : "
+         "as -p but <path> has to be a quotanode\n"
+      << "    quota set -u <uid>|-g <gid> [-v <bytes>] [-i <inodes>] [[-p] <path>] : set "
+         "volume and/or inode quota by uid or gid\n"
+      << "    quota rm -u <uid>|-g <gid> [-v] [-i] [[-p] <path>] : remove configured "
+         "quota type(s) for uid/gid in path\n"
+      << "    quota rmnode [-p] <path> [--really-want] : remove quota node and every "
+         "defined quota on that node\n"
+      << std::endl
+      << "  General options:\n"
+      << "    -m : print information in monitoring <key>=<value> format\n"
+      << "    -n : don't translate ids, print uid and gid number\n"
+      << "    -u/--uid <uid> : print information only for uid <uid>\n"
+      << "    -g/--gid <gid> : print information only for gid <gid>\n"
+      << "    -p/--path <path> : print information only for path <path> - this can also "
+         "be given without -p or --path\n"
+      << "    -v/--volume <bytes> : refer to volume limit in <bytes>\n"
+      << "    -i/--inodes <inodes> : refer to inode limit in number of <inodes>\n"
+      << std::endl
+      << "  Notes:\n"
+      << "    => you have to specify either the user or the group identified by the "
+         "unix id or the user/group name\n"
+      << "    => the space argument is by default assumed as 'default'\n"
+      << "    => you have to specify at least a volume or an inode limit to set quota\n"
+      << "    => for convenience all commands can just use <path> as last argument "
+         "omitting the -p|--path e.g. quota ls /eos/ ...\n"
+      << "    => if <path> is not terminated with a '/' it is assumed to be a file so "
+         "it won't match the quota node with <path>/ !\n"
+      << "    => rmnode requires confirmation unless --really-want is passed\n";
+  return oss.str();
 }
 
 void ConfigureQuotaApp(CLI::App& app)
@@ -254,13 +283,16 @@ public:
     return !wants_help(args.c_str());
   }
   int
-  run(const std::vector<std::string>& args, CommandContext&) override
+  run(const std::vector<std::string>& args, CommandContext& ctx) override
   {
     std::ostringstream oss;
     for (size_t i = 0; i < args.size(); ++i) {
       if (i)
         oss << ' ';
-      oss << args[i];
+      if (args[i].find(' ') != std::string::npos)
+        oss << std::quoted(args[i]);
+      else
+        oss << args[i];
     }
     std::string joined = oss.str();
     if (wants_help(joined.c_str())) {
@@ -268,7 +300,7 @@ public:
       global_retc = EINVAL;
       return 0;
     }
-    QuotaHelper helper(gGlobalOpts);
+    QuotaHelper helper(*ctx.globalOpts);
     if (!helper.ParseCommand(joined.c_str())) {
       printHelp();
       global_retc = EINVAL;
