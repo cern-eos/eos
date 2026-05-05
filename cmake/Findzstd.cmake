0987042a122d4a26233739a03f4ea1c71cb53a40
Luis Antonio Obis Aparicio <luis.obis@cern.ch>
2026-05-05 11:30:25 +0000

    CMAKE: Improve macOS build support

--- cmake/Findzstd.cmake
+++ cmake/Findzstd.cmake
@@ -8,19 +8,26 @@
 #
 # ZSTD::ZSTD
 
+find_path(ZSTD_INCLUDE_DIR
+  NAMES zstd.h
+  HINTS ${ZSTD_ROOT}
+  PATH_SUFFIXES include)
+
 find_library(ZSTD_LIBRARY
   NAMES zstd
   HINTS ${ZSTD_ROOT}
   PATH_SUFFIXES ${CMAKE_INSTALL_LIBDIR})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(zstd DEFAULT_MSG ZSTD_LIBRARY)
+find_package_handle_standard_args(zstd DEFAULT_MSG ZSTD_LIBRARY ZSTD_INCLUDE_DIR)
 
 if (ZSTD_FOUND AND NOT TARGET ZSTD::ZSTD)
-  mark_as_advanced(ZSTD_FOUND ZSTD_LIBRARY)
+  mark_as_advanced(ZSTD_FOUND ZSTD_LIBRARY ZSTD_INCLUDE_DIR)
   add_library(ZSTD::ZSTD UNKNOWN IMPORTED)
   set_target_properties(ZSTD::ZSTD PROPERTIES
-    IMPORTED_LOCATION "${ZSTD_LIBRARY}")
+    IMPORTED_LOCATION "${ZSTD_LIBRARY}"
+    INTERFACE_INCLUDE_DIRECTORIES "${ZSTD_INCLUDE_DIR}")
 endif()
 
 unset(ZSTD_LIBRARY)
+unset(ZSTD_INCLUDE_DIR)
