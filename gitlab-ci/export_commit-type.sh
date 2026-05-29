e0ee0abd3f6566819aaf4c87c999cb4e30193042
Elvin Sindrilaru <esindril@cern.ch>
2026-05-29 12:47:43 +0200

    CI: Drop doc-skip stage and handle this using the workflow rules mechanism

--- gitlab-ci/export_commit-type.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ -n "$CI_COMMIT_TAG" ]]; then
-    export COMMIT_TYPE=tag
-else
-    export COMMIT_TYPE=commit # @note schedules will work as commit
-fi
-
-# GitLab CI/CD predefined environment variables reference https://docs.gitlab.com/ee/ci/variables/predefined_variables.html
