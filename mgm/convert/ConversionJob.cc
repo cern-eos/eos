6b9500037d5079af4d71c9c01a62e2e84dd6cc32
Octavian-Mihai Matei <octavian-mihai.matei@cern.ch>
2026-08-21 14:58:03 +0200

    MGM: Fix the conversion of obfuscated files.
    The solution is to ask the source for the raw representation when the file
    carries an obfuscation key, so a conversion copies the bytes as they are
    stored.
    
    Fixes EOS-6633

--- mgm/convert/ConversionJob.cc
+++ mgm/convert/ConversionJob.cc
@@ -34,6 +34,7 @@
 #include "namespace/interface/IView.hh"
 #include "namespace/ns_quarkdb/NamespaceGroup.hh"
 #include "namespace/ns_quarkdb/flusher/MetadataFlusher.hh"
+#include "namespace/utils/Attributes.hh"
 #include "namespace/utils/Checksum.hh"
 
 //------------------------------------------------------------------------------
@@ -148,6 +149,7 @@ void ConversionJob::DoIt() noexcept
   std::string source_xs_postconversion;
   bool overwrite_checksum;
   uint64_t source_size;
+  bool source_obfuscated = false;
   eos::IFileMD::LocationVector src_locations;
   eos::IFileMD::LocationVector src_unlink_loc;
   gOFS->MgmStats.Add("ConversionJobStarted", 0, 0, 1);
@@ -170,6 +172,7 @@ void ConversionJob::DoIt() noexcept
     source_size = fmd->getSize();
     src_locations = fmd->getLocations();
     src_unlink_loc = fmd->getUnlinkedLocations();
+    source_obfuscated = fmd->hasAttribute(eos::kAttrObfuscateKey);
     eos::appendChecksumOnStringAsHex(fmd.get(), source_xs);
     // Check if conversion requests a checksum rewrite
     std::string file_checksum = LayoutId::GetChecksumString(fmd->getLayoutId());
@@ -226,6 +229,12 @@ void ConversionJob::DoIt() noexcept
              << "&eos.encodepath=curl"
              << "&eos.app=" + app_tag << "&" << eos::common::kSchedClassKey << "="
              << eos::common::kSchedClassInternal;
+
+  if (source_obfuscated) {
+    // An obfuscated file has to be copied exactly as it is stored on disk.
+    url_params << "&eos.obfuscate=0";
+  }
+
   url_src.SetParams(url_params.str());
   url_src.SetPath(eos::common::StringConversion::curl_escaped(mSourcePath));
   XrdCl::URL url_dst = NewUrl();
